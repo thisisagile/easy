4e86522c520508917107836798164756b8728cb8
aahoogendoorn <aahoogendoorn@gmail.com>
2020-11-05 13:55:25 +0100

    Minor Sonar issue

--- src/domain/Record.ts
+++ src/domain/Record.ts
@@ -2,7 +2,7 @@ import {Validatable} from "../types";
 
 export abstract class Record implements Validatable {
 
-    get isValid(): boolean { return true };
+    get isValid(): boolean { return true }
 
     constructor(protected state: any) {}
 }
