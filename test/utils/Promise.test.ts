5a7455d21c1f22f35adcd26a5960d5e2ba0fb413
aahoogendoorn <aahoogendoorn@gmail.com>
2020-11-07 19:09:14 +0100

    test(Testing resolve() and reject()) Added test for shortcuts resolve() and reject().

---
+++ test/utils/Promise.test.ts
@@ -0,0 +1,16 @@
+import { reject, resolve } from '../../src/utils';
+import { Dev } from '../ref/Dev';
+import { results, Results } from '../../src/types';
+
+describe('Promise', () => {
+
+  test('resolve', () => {
+    return expect(resolve(Dev.Sander)).resolves.toMatchObject(Dev.Sander);
+  });
+
+  test('reject', async () => {
+    await expect(reject("Wrong")).rejects.toBeInstanceOf(Results);
+    await expect(reject(results("Very wrong"))).rejects.toBeInstanceOf(Results);
+    return expect(reject(new Error("Also wrong"))).rejects.toBeInstanceOf(Error);
+  });
+});
