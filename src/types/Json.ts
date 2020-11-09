022ea7baef9c33c2b13ca887212af057f1d53271
aahoogendoorn <aahoogendoorn@gmail.com>
2020-11-09 20:07:18 +0100

    fix(Added jsonify() utility): Added jsonify type to serialize object, and remove undefined props.

--- src/types/Json.ts
+++ src/types/Json.ts
@@ -1,2 +1,5 @@
 export type JsonValue = string | number | boolean | null | Json | JsonValue[];
 export type Json = { [key: string]: JsonValue };
+
+export const jsonify = (subject: unknown = {}, additional?: Json): Json =>
+  ({ ...(JSON.parse(JSON.stringify(subject))), ...additional });
---
+++ test/types/Json.test.ts
@@ -0,0 +1,35 @@
+import { jsonify } from '../../src/types';
+import { Dev } from '../ref/Dev';
+
+describe('Json', () => {
+
+  test("jsonify nothing", () => {
+    const json = jsonify();
+    expect(json).toMatchObject({});
+  })
+
+  test("jsonify empty", () => {
+    const json = jsonify({});
+    expect(json).toMatchObject({});
+  })
+
+  test("jsonify undefined", () => {
+    const json = jsonify(undefined);
+    expect(json).toMatchObject({});
+  })
+
+  test("jsonify undefined", () => {
+    const json = jsonify(null);
+    expect(json).toMatchObject({});
+  })
+
+  test("jsonify simple", () => {
+    const json = jsonify({name: "Sander"});
+    expect(json).toMatchObject({name: "Sander"});
+  })
+
+  test("jsonify entity", () => {
+    const json = jsonify(Dev.Wouter);
+    expect(json).toMatchObject({name: "Wouter", language: "TypeScript", level: 3});
+  })
+});
