f610c78c32a78d6bd996a3ea0955ccfb5330427a
aahoogendoorn <aahoogendoorn@gmail.com>
2020-11-10 16:08:42 +0100

    fix(Added isInstance and isObject): Added functions for checking objects and instances (using Constructor.ts).

--- src/domain/Record.ts
+++ src/domain/Record.ts
@@ -2,7 +2,7 @@ import { Json, jsonify, Validatable } from '../types';
 import { validate } from '../validation';
 
 export abstract class Record implements Validatable {
-    constructor(protected readonly state: any) {}
+    constructor(protected readonly state: any = {}) {}
 
     get isValid(): boolean { return validate(this).isValid; }
 
