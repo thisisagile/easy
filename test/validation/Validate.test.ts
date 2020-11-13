92067838054361afcfef4b80d09f537f36332b61
aahoogendoorn <aahoogendoorn@gmail.com>
2020-11-13 13:29:03 +0100

    test(Added tests for validating inherited classes): Check if validate work with inherited classes.

--- test/validation/Validate.test.ts
+++ test/validation/Validate.test.ts
@@ -1,33 +1,46 @@
-import { Dev } from "../ref/Dev";
-import { validate, validateReject } from "../../src/validation";
+import { Dev } from '../ref/Dev';
+import { includes, validate, validateReject } from '../../src/validation';
 import { Results } from '../../src/types';
 
-describe("validate", () => {
+class Top {
+  @includes('a') readonly name: string = 'Top';
+}
 
-  test("Works on empty objects", () => {
+class Below extends Top {
+  @includes('b') readonly city: string = 'Amsterdam';
+}
+
+describe('validate', () => {
+
+  test('Works on empty objects', () => {
     expect(validate()).toHaveLength(1);
     expect(validate(null)).toHaveLength(1);
   });
 
-  test("Works on invalid objects", () => {
-    const dev = new Dev({level: 1});
+  test('Works on invalid objects', () => {
+    const dev = new Dev({ level: 1 });
     expect(validate(dev)).toHaveLength(2);
   });
 
-  test("Works on valid objects", () => {
+  test('Works on valid objects', () => {
     const dev = Dev.Sander;
     expect(validate(dev)).toHaveLength(0);
   });
+
+  test('Works on inherited objects', () => {
+    const b = new Below();
+    expect(validate(b)).toHaveLength(2);
+  });
 });
 
-describe("validateReject", () => {
+describe('validateReject', () => {
 
-  test("Resolves when ok", () => {
+  test('Resolves when ok', () => {
     expect(validate(Dev.Sander).isValid).toBeTruthy();
     expect(validateReject(Dev.Sander)).resolves.toBe(Dev.Sander);
-  })
+  });
 
-  test("Rejects when fails", () => {
-    expect(validateReject(new Dev({level: 1}))).rejects.toBeInstanceOf(Results);
-  })
+  test('Rejects when fails', () => {
+    expect(validateReject(new Dev({ level: 1 }))).rejects.toBeInstanceOf(Results);
+  });
 });
