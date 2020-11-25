36be6420794b55bc9f0faca6f661c4aaad46fe92
aahoogendoorn <aahoogendoorn@gmail.com>
2020-11-25 11:01:11 +0100

    refactor(Added default): Added default for domain in result().

--- src/types/Result.ts
+++ src/types/Result.ts
@@ -3,6 +3,6 @@ import { Text } from './Text';
 
 export type Result = { message: Text, domain?: string, location?: string };
 
-export const result = (message: Text, domain?: string, location?: string) => ({ message, domain, location });
+export const result = (message: Text, domain: string = "easy", location?: string) => ({ message, domain, location });
 
 export const isResult = (r?: unknown): r is Result => isA<Result>(r, "message");
