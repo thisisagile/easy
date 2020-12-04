e11c3e8f975cec395f07c1322dad8d0cba6e7631
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-04 03:50:05 +0100

    test(Updated tests for Repo): Updated tests on Repo

--- test/domain/Repo.test.ts
+++ test/domain/Repo.test.ts
@@ -1,8 +1,9 @@
-import { DevGateway, DevRepo } from '../ref';
+import { Dev, DevGateway, DevRepo } from '../ref';
 import { mock } from '@thisisagile/easy-test';
 
 describe('Repo', () => {
 
+  const devs = [Dev.Sander.toJSON(), Dev.Jeroen.toJSON(), Dev.Naoufal.toJSON()];
   let gateway: DevGateway;
   let repo: DevRepo;
 
@@ -12,8 +13,17 @@ describe('Repo', () => {
   });
 
   test('all triggers gateway', async () => {
-    gateway.all = mock.resolve([]);
-    await repo.all();
+    gateway.all = mock.resolve(devs);
+    const ds = await repo.all();
     expect(gateway.all).toHaveBeenCalled();
+    expect(ds[0]).toBeInstanceOf(Dev);
+  });
+
+  test('byId triggers gateway', async () => {
+    gateway.byId = mock.resolve(devs[0]);
+    const d = await repo.byId(42);
+    expect(gateway.byId).toHaveBeenCalledWith(42);
+    expect(d).toBeInstanceOf(Dev);
+    expect(d).toMatchObject(devs[0]);
   });
 });
