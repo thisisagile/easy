a074c53e2493d5e352d7bbb023863f2874386bb6
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-08 16:16:00 +0100

    refactor(Minor change): Changed use of quotes.

--- test/ref/DevRepo.ts
+++ test/ref/DevRepo.ts
@@ -6,5 +6,5 @@ export class DevRepo extends Repo<Dev> {
   constructor(readonly devs = new DevGateway()) {super(Dev, devs); }
 
   validate = (dev: Dev): Promise<Dev> =>
-    dev.name.includes("e") ? resolve(dev) : reject("Name should include 'e'.");
+    dev.name.includes('e') ? resolve(dev) : reject('Name should include \'e\'.');
 }
