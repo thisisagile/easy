c215cbccf748e6cf8f5ec31adbd3186f00302994
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-12 00:38:59 +0100

    refactor(Refactored EasyRequest): Update DevResource.ts.

--- test/ref/DevResource.ts
+++ test/ref/DevResource.ts
@@ -1,4 +1,4 @@
-import { del, EasyRequest, get, HttpStatus, isDefined, list, List, patch, post, put, route } from '../../src';
+import { del, Req, get, HttpStatus, isDefined, list, List, patch, post, put, route } from '../../src';
 import { DevUri } from './DevUri';
 import { Dev } from './Dev';
 
@@ -6,14 +6,14 @@ export class Resource<T> {}
 
 @route(DevUri.Developers)
 export class DevsResource {
-  @get() all = (req: EasyRequest): List<Dev> => list(new Dev(req.id));
-  @post() insert = (req: EasyRequest): Dev => new Dev(req.id);
+  @get() all = (req: Req): List<Dev> => list(new Dev(req.id));
+  @post() insert = (req: Req): Dev => new Dev(req.id);
 }
 
 @route(DevUri.Developer)
 export class DevResource extends Resource<Dev> {
-  @get(HttpStatus.Ok, HttpStatus.NoContent) byId = (req: EasyRequest): Dev => new Dev(req.id);
-  @put() update = (req: EasyRequest): Dev => new Dev(req.id);
-  @patch() upsert = (req: EasyRequest): Dev => new Dev(req.id);
-  @del() delete = (req: EasyRequest): boolean => isDefined(req.id);
+  @get(HttpStatus.Ok, HttpStatus.NoContent) byId = (req: Req): Dev => new Dev(req.id);
+  @put() update = (req: Req): Dev => new Dev(req.id);
+  @patch() upsert = (req: Req): Dev => new Dev(req.id);
+  @del() delete = (req: Req): boolean => isDefined(req.id);
 }
