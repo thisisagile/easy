7c0771f3b05b67beaa8b673c20c4b9a8cb3b613e
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-12 12:18:14 +0100

    fix(Update Route): Route and Endpoint are now generic. Funciotn router() now takes a constructor.

--- src/services/Route.ts
+++ src/services/Route.ts
@@ -1,22 +1,22 @@
-import { List, meta, Uri } from '../types';
+import { Constructor, List, meta, Uri } from '../types';
 import { HttpVerb } from './HttpVerb';
 import { Verb } from './Verb';
 import { Req } from './Req';
 
-export type Endpoint = (re: Req) => Promise<unknown | List<unknown>>;
-
 export const route = (uri: Uri): ClassDecorator =>
   (subject: Function): void => { meta(subject).set('route', uri); };
 
-class Router {
+export type Endpoint<T> = (re: Req) => Promise<T | List<T>>;
+
+class Router<T> {
   constructor(readonly resource: unknown) {}
 
   get route(): Uri { return meta(this.resource).get('route'); }
 
-  get endpoints(): List<{ verb: HttpVerb, f: Endpoint }> {
+  get endpoints(): List<{ verb: HttpVerb, f: Endpoint<T> }> {
     return meta(this.resource).properties('verb')
       .map(p => ({ verb: p.get<Verb>('verb').verb, f: (this.resource as any)[p.property] }));
   }
 }
 
-export const router = (resource: unknown) => new Router(resource);
+export const router = <T>(resource: Constructor<T>) => new Router<T>(new resource());
--- test/services/Route.test.ts
+++ test/services/Route.test.ts
@@ -12,14 +12,8 @@ describe('Route', () => {
 
 describe('Router', () => {
 
-  test('Router works on a non-valid object', () => {
-    const { route, endpoints } = router({});
-    expect(route).toBeUndefined();
-    expect(endpoints).toHaveLength(0);
-  });
-
   test('Router works on a valid class', () => {
-    const { route, endpoints } = router(new DevsResource());
+    const { route, endpoints } = router(DevsResource);
     expect(route).toBeInstanceOf(DevUri);
     expect(route.route.toString()).toBe('/developers');
     expect(endpoints).toHaveLength(2);
