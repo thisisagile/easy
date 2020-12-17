d34b0380920673499f473ac30ba7d8629903b43f
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-17 19:51:52 +0100

    fix(Added toJson): Update to Json functionality to support easier updates on entities.

--- src/types/Json.ts
+++ src/types/Json.ts
@@ -1,4 +1,13 @@
+import { isA } from './IsA';
+
 export type JsonValue = string | number | boolean | null | Json | JsonValue[];
 export type Json = { [key: string]: JsonValue };
 
-export const jsonify = (subject: unknown = {}, additional?: Json): Json => ({ ...JSON.parse(JSON.stringify(subject)), ...additional });
+export const isJson = (subject?: unknown): subject is { toJSON: () => Json } => isA<{ toJSON: () => Json }>(subject, 'toJSON');
+
+export const jsonify = (subject: unknown = {}): Json => ({ ...JSON.parse(JSON.stringify(subject)) });
+
+export const toJson = (subject: unknown = {}, add: unknown = {}): Json => ({
+  ...(isJson(subject) ? subject.toJSON() : jsonify(subject)),
+  ...(isJson(add) ? add.toJSON() : jsonify(add)),
+});
--- test/types/Json.test.ts
+++ test/types/Json.test.ts
@@ -1,7 +1,7 @@
-import { jsonify } from '../../src';
-import { Dev } from '../ref';
+import { isJson, jsonify, toJson } from '../../src';
+import { Dev, DevsResource } from '../ref';
 
-describe('Json', () => {
+describe('jsonify', () => {
   test('jsonify nothing', () => {
     const json = jsonify();
     expect(json).toMatchObject({});
@@ -35,5 +35,91 @@ describe('Json', () => {
   test('jsonify removes undefined', () => {
     const json = jsonify({ name: 'Wouter', language: 'TypeScript', level: undefined });
     expect(json?.level).toBeUndefined();
+  });
+
+  test('jsonify removes undefined and adds', () => {
+    const json = jsonify({ name: 'Wouter', language: 'TypeScript' });
+    expect(json).toMatchObject({ name: 'Wouter', language: 'TypeScript' });
+  });
+
+  test('jsonify object and adds', () => {
+    const json = jsonify(Dev.Wouter);
+    expect(json).toMatchObject({ name: 'Wouter', language: 'TypeScript', level: 3 });
+  });
+});
+
+describe('isJson', () => {
+  test('isJson true', () => {
+    expect(
+      isJson({
+        toJSON: () => {
+          'Kim';
+        },
+      })
+    ).toBeTruthy();
+    expect(isJson(Dev.Sander)).toBeTruthy();
+  });
+
+  test('isJson false', () => {
+    expect(isJson()).toBeFalsy();
+    expect(isJson({})).toBeFalsy();
+    expect(isJson(new DevsResource())).toBeFalsy();
+  });
+});
+
+describe('toJson', () => {
+  test('toJson nothing', () => {
+    const json = toJson();
+    expect(json).toMatchObject({});
+  });
+
+  test('toJson empty', () => {
+    const json = toJson({});
+    expect(json).toMatchObject({});
+  });
+
+  test('toJson undefined', () => {
+    const json = toJson(undefined);
+    expect(json).toMatchObject({});
+  });
+
+  test('toJson null', () => {
+    const json = toJson(null);
+    expect(json).toMatchObject({});
+  });
+
+  test('toJson simple', () => {
+    const json = toJson({ name: 'Sander' });
+    expect(json).toMatchObject({ name: 'Sander' });
+  });
+
+  test('toJson entity', () => {
+    const json = toJson(Dev.Wouter);
+    expect(json).toMatchObject({ name: 'Wouter', language: 'TypeScript', level: 3 });
+  });
+
+  test('toJson removes undefined', () => {
+    const json = toJson({ name: 'Wouter', language: 'TypeScript', level: undefined });
+    expect(json?.level).toBeUndefined();
+  });
+
+  test('toJson removes undefined and adds', () => {
+    const json = toJson({ name: 'Wouter', language: 'TypeScript' }, { level: 3 });
+    expect(json).toMatchObject({ name: 'Wouter', language: 'TypeScript', level: 3 });
+  });
+
+  test('toJson object and adds', () => {
+    const json = toJson(Dev.Wouter, { level: 4 });
+    expect(json).toMatchObject({ name: 'Wouter', language: 'TypeScript', level: 4 });
+  });
+
+  test('toJson object and adds object', () => {
+    const json = toJson(Dev.Wouter, Dev.Naoufal);
+    expect(json).toMatchObject({ id: 2, name: 'Naoufal', language: 'TypeScript', level: 3 });
+  });
+
+  test('update works', () => {
+    const dev = Dev.Sander.update({ level: 2 });
+    expect(dev).toMatchObject({ name: 'Sander', level: 2 });
   });
 });
