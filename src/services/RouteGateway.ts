6415cb66e1f4de65aa4b123db6136d2cc543fc18
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-23 10:49:10 +0100

    fix(Update to RouteGateway.ts): Switch properties to functions.

--- src/services/RouteGateway.ts
+++ src/services/RouteGateway.ts
@@ -7,17 +7,31 @@ export class RouteGateway implements Gateway {
 
   constructor(readonly api: Api = new Api()) {}
 
-  all = (): Promise<List<Json>> => this.api.get(this.route).then(r => r.data.items);
+  all(): Promise<List<Json>> {
+    return this.api.get(this.route).then(r => r.data.items);
+  }
 
-  byId = (id: Id): Promise<Json> => this.api.get(this.routeId.id(id)).then(r => r.data.items.first());
+  byId(id: Id): Promise<Json> {
+    return this.api.get(this.routeId.id(id)).then(r => r.data.items.first());
+  }
 
-  search = (q: JsonValue): Promise<List<Json>> => this.api.get(this.route.query(q)).then(r => r.data.items);
+  search(q: JsonValue): Promise<List<Json>> {
+    return this.api.get(this.route.query(q)).then(r => r.data.items);
+  }
 
-  exists = (id: Id): Promise<boolean> => this.api.get(this.routeId.id(id)).then(r => r.data.items.length === 1);
+  exists(id: Id): Promise<boolean> {
+    return this.api.get(this.routeId.id(id)).then(r => r.data.items.length === 1);
+  }
 
-  add = (item: Json): Promise<Json> => this.api.post(this.route, item).then(r => r.data.items.first());
+  add(item: Json): Promise<Json> {
+    return this.api.post(this.route, item).then(r => r.data.items.first());
+  }
 
-  update = (item: Json): Promise<Json> => this.api.patch(this.routeId.id(item.id), item).then(r => r.data.items.first());
+  update(item: Json): Promise<Json> {
+    return this.api.patch(this.routeId.id(item.id), item).then(r => r.data.items.first());
+  }
 
-  remove = (id: Id): Promise<boolean> => this.api.delete(this.routeId.id(id)).then(r => r.data.items.length === 0);
+  remove(id: Id): Promise<boolean> {
+    return this.api.delete(this.routeId.id(id)).then(r => r.data.items.length === 0);
+  }
 }
