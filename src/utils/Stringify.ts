9a1db0c0a0b33e8eaf206fb4709c125c65a296b4
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-28 17:59:03 +0100

    fix(Added toPascal() and toCamel() to Stringify.ts): Added pascal case and camel case.

--- src/utils/Stringify.ts
+++ src/utils/Stringify.ts
@@ -3,7 +3,15 @@ class Stringify {
 
   toCap = (): string => this.subject.charAt(0).toUpperCase() + this.subject.slice(1);
 
-  toTitle = (): string => this.subject.split(' ').map(w => stringify(w).toCap()).join(' ');
+  toTitle = (): string =>
+    this.subject
+      .split(' ')
+      .map(w => stringify(w).toCap())
+      .join(' ');
+
+  toPascal = (): string => this.toTitle().replace(/ /g, '');
+
+  toCamel = (): string => this.toPascal().charAt(0).toLowerCase() + this.toPascal().slice(1);
 
   toKebab = (): string => this.subject.replace(/ /g, '-').toLowerCase();
 }
@@ -12,4 +20,3 @@ export const stringify = (subject?: unknown): Stringify => {
   const s = subject?.toString() ?? '';
   return new Stringify(s !== '[object Object]' ? s : '');
 };
-
--- test/utils/Stringify.test.ts
+++ test/utils/Stringify.test.ts
@@ -6,27 +6,43 @@ describe('Stringify', () => {
   const wouter = stringify('Wouter');
   const kim = stringify('Kim van Wilgen');
 
-  test('toKebab works', () => {
-    expect(empty.toKebab()).toBe('');
-    expect(stringify({}).toKebab()).toBe('');
-    expect(stringify(Dev.Sander).toKebab()).toBe('sander');
-    expect(wouter.toKebab()).toBe('wouter');
-    expect(kim.toKebab()).toBe('kim-van-wilgen');
-  });
-
   test('toCap works', () => {
     expect(empty.toCap()).toBe('');
     expect(stringify({}).toCap()).toBe('');
-    expect(stringify(Dev.Sander).toCap()).toBe('Sander');
     expect(wouter.toCap()).toBe('Wouter');
+    expect(stringify(Dev.Sander).toCap()).toBe('Sander');
     expect(kim.toCap()).toBe('Kim van Wilgen');
   });
 
   test('toTitle works', () => {
     expect(empty.toTitle()).toBe('');
     expect(stringify({}).toTitle()).toBe('');
-    expect(stringify(Dev.Sander).toTitle()).toBe('Sander');
     expect(wouter.toTitle()).toBe('Wouter');
+    expect(stringify(Dev.Sander).toTitle()).toBe('Sander');
     expect(kim.toTitle()).toBe('Kim Van Wilgen');
+  });
+
+  test('toPascal works', () => {
+    expect(empty.toPascal()).toBe('');
+    expect(stringify({}).toPascal()).toBe('');
+    expect(wouter.toPascal()).toBe('Wouter');
+    expect(stringify(Dev.Sander).toPascal()).toBe('Sander');
+    expect(kim.toPascal()).toBe('KimVanWilgen');
+  });
+
+  test('toCamel works', () => {
+    expect(empty.toCamel()).toBe('');
+    expect(stringify({}).toCamel()).toBe('');
+    expect(wouter.toCamel()).toBe('wouter');
+    expect(stringify(Dev.Sander).toCamel()).toBe('sander');
+    expect(kim.toCamel()).toBe('kimVanWilgen');
+  });
+
+  test('toKebab works', () => {
+    expect(empty.toKebab()).toBe('');
+    expect(stringify({}).toKebab()).toBe('');
+    expect(wouter.toKebab()).toBe('wouter');
+    expect(stringify(Dev.Sander).toKebab()).toBe('sander');
+    expect(kim.toKebab()).toBe('kim-van-wilgen');
   });
 });
