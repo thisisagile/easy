aecf26102f2c73452582b21ab8de620e1394326c
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-04 17:33:48 +0100

    fix(Update to Req): Made path and query into any's

--- src/services/Req.ts
+++ src/services/Req.ts
@@ -1,11 +1,11 @@
 import { Id, Json, JsonValue } from '../types';
 
-export type Req = { path?: Json; id?: Id; query?: Json; q?: JsonValue; body?: Json };
+export type Req = { path?: any; id?: Id; query?: any; q?: JsonValue; body?: Json };
 
 export const toReq = (req: { params?: { id?: unknown }; query?: { q?: unknown }; body?: unknown }): Req => ({
-  path: req.params as Json,
+  path: req.params,
   id: req.params?.id as Id,
-  query: req.query as Json,
+  query: req.query,
   q: req.query?.q as Json,
   body: req.body as Json,
 });
