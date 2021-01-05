6eea4022bc83d21c18e1bb398d2493402e6f2d47
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-05 13:24:25 +0100

    fix(Added return type on ErrorHandler.ts): Added return type to error function.

--- src/express/ErrorHandler.ts
+++ src/express/ErrorHandler.ts
@@ -2,7 +2,7 @@ import { isResults, Results } from '../types';
 import { NextFunction, Request, Response } from 'express';
 import { HttpStatus, toRestResult } from '../services';
 
-export const error = (e: string | Error | Results, req: Request, res: Response, _next: NextFunction) => {
+export const error = (e: string | Error | Results, req: Request, res: Response, _next: NextFunction): void => {
   res.set('Connection', 'close');
   res.status(isResults(e) ? HttpStatus.BadRequest.status : HttpStatus.InternalServerError.status).json(toRestResult(e));
 };
