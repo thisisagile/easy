837ff15e17f65153d21a2fed99f6eaea25119a69
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-11 20:22:17 +0100

    fix(Renamed to Search.ts): Manage inherits from Search.

--- src/process/Manage.ts
+++ src/process/Manage.ts
@@ -1,8 +1,8 @@
-import { Select } from './Select';
+import { Search } from './Search';
 import { Record } from '../domain';
 import { Id, Json } from '../types';
 
-export class Manage<T extends Record> extends Select<T> {
+export class Manage<T extends Record> extends Search<T> {
   add = (json: Json): Promise<T> => this.repo.add(json);
   update = (json: Json): Promise<T> => this.repo.update(json);
   remove = (id: Id): Promise<boolean> => this.repo.remove(id);
