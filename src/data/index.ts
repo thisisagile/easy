2ee862227d32e08819ae870ccb9fc0b81df0a442
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-12 15:36:02 +0100

    refactor(Lift and shift data): Altered indexes.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,7 @@
+export * from './Clause';
+export * from './Column';
 export * from './Database';
+export * from './Query';
+export * from './Select';
+export * from './Table';
+export * from './TableGateway';
--- src/sql/index.ts
+++ src/sql/index.ts
@@ -1,5 +1,1 @@
-export * from './Clause';
-export * from './Column';
-export * from './Query';
-export * from './Select';
-export * from './Table';
+export * from './SqlServerProvider';
