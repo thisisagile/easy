8d7f2d120cbbe93dc4cf34748a9751b858d164dc
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-12 16:03:09 +0100

    fix(Added DataProvider to Database): Added provider to database enum, easier to use in TableGateway.ts.

--- src/data/Database.ts
+++ src/data/Database.ts
@@ -1,5 +1,7 @@
 import { Enum } from '../types';
+import { DataProvider } from './DataProvider';
 
 export class Database extends Enum {
+  constructor(name: string, readonly provider?: DataProvider) { super(name); }
   static readonly Main = new Database('Main');
 }
