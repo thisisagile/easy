5ee9e40d2349ece5b61d06d126c1e06339c32cc4
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-16 20:28:33 +0100

    fix(Adding in() and out() to Table.ts): Adding conversion functions in() and out().

--- src/data/Table.ts
+++ src/data/Table.ts
@@ -1,22 +1,26 @@
-import { Property, PropertyOptions } from '../utils';
+import { clone, PropertyOptions } from '../utils';
 import { Database } from './index';
 import { Column } from './Column';
-import { list, List, meta } from '../types';
+import { Json, list, List, meta } from '../types';
 import { Select } from './Select';
 import { Delete } from './Delete';
 
 export class Table {
   readonly db = Database.Main;
 
-  get columns(): List<[string, Property]> {
+  get columns(): List<[string, Column]> {
     return meta(this)
-      .entries<Property>()
+      .entries<Column>()
       .filter(([, v]) => v instanceof Column);
   }
 
   col = (name: string, options?: PropertyOptions): Column => new Column(this, name, options);
 
   readonly id = this.col('id');
+
+  in = (from?: Json): Json => this.columns.reduce((a: any, [k, v]: [string, Column]) => clone(a, k, v.name, v.options.def, v.options.convert.to), { ...from });
+
+  out = (to?: Json): Json => this.columns.reduce((a: any, [k, v]: [string, Column]) => clone(a, v.name, k, undefined, v.options.convert.from), { ...to });
 
   toString(): string {
     return this.constructor.name;
