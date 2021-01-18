139ef06f61018302c834f2305578c0b20ef962c2
aahoogendoorn <aahoogendoorn@gmail.com>
2021-01-18 11:51:41 +0100

    fix(Removed test from Insert.test.ts): Removed test from Insert.test.ts

--- test/data/Insert.test.ts
+++ test/data/Insert.test.ts
@@ -4,11 +4,7 @@ import '@thisisagile/easy-test';
 describe('Insert', () => {
   const devs = new DevTable();
 
-  test('Simple', () => {
-    expect(devs.insert({ id: 3 })).toMatchText('INSERT INTO DevTable (Id) OUTPUT INSERTED.* VALUES (3)');
-  });
-
-  test('Full insert with converters', () => {
+  test('Create insert', () => {
     expect(devs.insert(Dev.Naoufal.toJSON())).toMatchText(
       "INSERT INTO DevTable (Id, Name, Language, CodingLevel) OUTPUT INSERTED.* VALUES (2, 'Naoufal', 'TypeScript', '3')"
     );
