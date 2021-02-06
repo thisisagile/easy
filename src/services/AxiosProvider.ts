af8f0e98462cd3411c4aea3e6e219c5aeb81b2b5
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-06 15:29:39 +0100

    fix: renamed result() to toResult() to unify naming.

--- src/services/AxiosProvider.ts
+++ src/services/AxiosProvider.ts
@@ -2,21 +2,21 @@ import axios, { AxiosError, Method } from 'axios';
 import { Request, RequestProvider } from './RequestProvider';
 import { RequestOptions } from './RequestOptions';
 import { RestResult, toRestResult } from './RestResult';
-import { isDefined, result, Result, Uri } from '../types';
+import { isDefined, toResult, Result, Uri } from '../types';
 import { choose } from '../utils';
 import { HttpVerb } from './HttpVerb';
 
-const toResult = (uri: Uri, verb: HttpVerb, error: AxiosError): Result =>
+const asResult = (uri: Uri, verb: HttpVerb, error: AxiosError): Result =>
   choose<Result, AxiosError>(error)
     .case(
       e => isDefined(e.response),
-      e => result(e.response.statusText, verb.toString(), uri.toString())
+      e => toResult(e.response.statusText, verb, uri)
     )
     .case(
       e => isDefined(e.request),
-      e => result(e.request.statusText, verb.toString(), uri.toString())
+      e => toResult(e.request.statusText, verb, uri)
     )
-    .else(e => result(e.message, verb.toString(), uri.toString()));
+    .else(e => toResult(e.message, verb, uri));
 
 export class AxiosProvider implements RequestProvider {
   execute = ({ uri, verb, body, transform = (r: any) => r, options = RequestOptions.Json }: Request): Promise<RestResult> =>
@@ -28,6 +28,6 @@ export class AxiosProvider implements RequestProvider {
         data: body,
       })
       .then(r => transform(r))
-      .catch(e => toResult(uri, verb, e))
+      .catch(e => asResult(uri, verb, e))
       .then(r => toRestResult(r));
 }
