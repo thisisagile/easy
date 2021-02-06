447eb5fbc3b9098dee3a8929f4fa8d8f9f097a64
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-06 15:31:26 +0100

    fix(Constraints): improved parsing functionality for texts based on objects.

--- src/validation/Contraints.ts
+++ src/validation/Contraints.ts
@@ -1,5 +1,5 @@
-import { inFuture, inPast, isDefined, isIn, isString, list, List, meta, Text } from '../types';
-import { validate, Validator } from './Validate';
+import { inFuture, inPast, isDefined, isIn, isString, isValidatable, list, List, meta, Text } from '../types';
+import { Validator } from './Validate';
 
 export type Constraint = (value: unknown) => boolean;
 
@@ -7,31 +7,33 @@ export const constraint = <T>(c: Constraint, message: Text): PropertyDecorator =
   const cs = meta(subject).property(property).get<List<Validator>>('constraint') ?? list<Validator>();
   meta(subject)
     .property(property)
-    .set('constraint', cs.add({ property, constraint: c, message }));
+    .set('constraint', cs.add({ property, constraint: c, text: message }));
 };
 
-export const defined = (message?: Text): PropertyDecorator => constraint(v => isDefined(v), message ?? '$property must be defined.');
+export const defined = (message?: Text): PropertyDecorator => constraint(v => isDefined(v), message ?? 'Property {property} must be defined.');
 
-export const required = (message?: Text): PropertyDecorator => constraint(v => isDefined(v), message ?? '$property is required.');
+export const required = (message?: Text): PropertyDecorator => constraint(v => isDefined(v), message ?? 'Property {property} is required.');
 
-export const valid = (message?: Text): PropertyDecorator => constraint(v => validate(v).isValid, message ?? '$property must be valid.');
+export const valid = (message?: Text): PropertyDecorator => constraint(v => isValidatable(v) && v.isValid, message ?? 'Property {property} must be valid.');
 
 export const includes = (sub: string, message?: string): PropertyDecorator =>
-  constraint(s => isDefined(s) && isString(s) && s.includes(sub), message ?? `$actual must include '${sub}'.`);
+  constraint(s => isDefined(s) && isString(s) && s.includes(sub), message ?? `Value {actual} must include '${sub}'.`);
 
 export const inList = (values: unknown[], message?: Text): PropertyDecorator =>
-  constraint(v => isDefined(v) && isIn(v, values), message ?? '$actual must appear in list.');
+  constraint(v => isDefined(v) && isIn(v, values), message ?? 'Value {actual} must appear in list.');
 
-export const gt = (limit: number, message?: Text): PropertyDecorator => constraint(v => v > limit, message ?? `$actual must be larger than '${limit}'.`);
+export const gt = (limit: number, message?: Text): PropertyDecorator => constraint(v => v > limit, message ?? `Value {actual} must be larger than '${limit}'.`);
 
 export const gte = (limit: number, message?: Text): PropertyDecorator =>
-  constraint(v => v >= limit, message ?? `$actual must be larger than or equal to ${limit}.`);
+  constraint(v => v >= limit, message ?? `Value {actual} must be larger than or equal to ${limit}.`);
 
-export const lt = (limit: number, message?: Text): PropertyDecorator => constraint(v => v < limit, message ?? `$actual must be smaller than ${limit}.`);
+export const lt = (limit: number, message?: Text): PropertyDecorator => constraint(v => v < limit, message ?? `Value {actual} must be smaller than ${limit}.`);
 
 export const lte = (limit: number, message?: Text): PropertyDecorator =>
-  constraint(v => v <= limit, message ?? `$actual must be smaller than or equal to ${limit}.`);
+  constraint(v => v <= limit, message ?? `Value {actual} must be smaller than or equal to ${limit}.`);
 
-export const past = (message?: Text): PropertyDecorator => constraint(v => inPast(v), message ?? '$actual must lay in the past.');
+export const past = (message?: Text): PropertyDecorator => constraint(v => inPast(v), message ?? 'Value {actual} must lay in the past.');
 
-export const future = (message?: Text): PropertyDecorator => constraint(v => inFuture(v), message ?? '$actual must lay in the future.');
+export const future = (message?: Text): PropertyDecorator => constraint(v => inFuture(v), message ?? 'Value {actual} must lay in the future.');
+
+export const yes = (message?: Text): PropertyDecorator => constraint(v => !!v, message ?? `Value {actual} must be true`);
