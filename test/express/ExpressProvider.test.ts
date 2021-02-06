cc9578856254f6c005340065d5c9f96e303c6f21
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-06 15:32:26 +0100

    chore: line separators.

--- test/express/ExpressProvider.test.ts
+++ test/express/ExpressProvider.test.ts
@@ -4,7 +4,7 @@ import { ExpressProvider, ExpressVerb, Handler, HttpStatus } from '../../src';
 import { DevsResource, DevUri } from '../ref';
 
 type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<any>;
-type Endpoint = {path?: string, handler?: AsyncHandler};
+type Endpoint = { path?: string; handler?: AsyncHandler };
 
 describe('ExpressProvider', () => {
   const app = ({ listen: mock.return(), use: mock.return() } as unknown) as Express;
@@ -26,10 +26,10 @@ describe('ExpressProvider', () => {
   });
 
   function mockRouterMethodOnce(router: express.Router, method: ExpressVerb, cb: (endpoint: Endpoint) => any) {
-    jest.spyOn(router, method).mockImplementationOnce((path: string, handler: RequestHandler) => cb({path, handler: handler as AsyncHandler}));
+    jest.spyOn(router, method).mockImplementationOnce((path: string, handler: RequestHandler) => cb({ path, handler: handler as AsyncHandler }));
   }
 
-  test('route',  () => {
+  test('route', () => {
     const router = express.Router();
     jest.spyOn(express, 'Router').mockReturnValueOnce(router);
     provider.route(DevsResource);
@@ -38,14 +38,13 @@ describe('ExpressProvider', () => {
     expect(router['post']).toEqual(fits.type(Function));
   });
 
-
   test('check if handler works', async () => {
     const router = express.Router();
     const resource = new DevsResource();
     let endpoint: Endpoint = {};
-    const res: any = { status: mock.return({json: mock.return()}) };
+    const res: any = { status: mock.return({ json: mock.return() }) };
 
-    mockRouterMethodOnce(router, 'post', e => endpoint = e);
+    mockRouterMethodOnce(router, 'post', e => (endpoint = e));
     jest.spyOn(express, 'Router').mockReturnValueOnce(router);
     resource.insert = mock.resolve();
 
@@ -55,16 +54,15 @@ describe('ExpressProvider', () => {
     expect(endpoint.path).toBe(DevUri.Developers.path);
     expect(resource.insert).toHaveBeenCalled();
     expect(res.status).toHaveBeenCalledWith(HttpStatus.Created.status);
-
   });
 
   test('use options from decorator', async () => {
     const router = express.Router();
     const resource = new DevsResource();
     let endpoint: Endpoint = {};
-    const res: any = { status: mock.return({json: mock.return()}) };
+    const res: any = { status: mock.return({ json: mock.return() }) };
 
-    mockRouterMethodOnce(router, 'get', e => endpoint = e);
+    mockRouterMethodOnce(router, 'get', e => (endpoint = e));
     jest.spyOn(express, 'Router').mockReturnValueOnce(router);
     resource.all = mock.resolve();
 
@@ -72,6 +70,5 @@ describe('ExpressProvider', () => {
     await endpoint.handler({} as Request, res as Response, jest.fn());
 
     expect(res.status).toHaveBeenCalledWith(HttpStatus.NoContent.status);
-
   });
 });
