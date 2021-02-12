998fb2310bc1663ec79f457c68900f7ef010345a
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-12 16:37:21 +0100

    fix(HealthUri): corrected route and added tests.

--- src/health/HealthUri.ts
+++ src/health/HealthUri.ts
@@ -1,5 +1,6 @@
-import { EasyUri } from '../types';
+import { EasyUri, uri } from '../types';
 
 export class HealthUri extends EasyUri {
-  static readonly Health = new HealthUri([]);
+  static readonly health = uri.segment('health');
+  static readonly Health = new HealthUri([HealthUri.health]);
 }
---
+++ test/health/HealthUri.test.ts
@@ -0,0 +1,10 @@
+import { HealthUri } from '../../src';
+import '@thisisagile/easy-test';
+
+describe('HealthUri', () => {
+
+  test('return route', () => {
+    expect(HealthUri.Health).toMatchRoute('/health');
+    expect(HealthUri.Health.route('dev')).toMatchRoute('/dev/health');
+  });
+});
