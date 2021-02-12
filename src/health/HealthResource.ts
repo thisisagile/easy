ab1f4ca4df3ce8c6344270ecc897abf2a9bec69a
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-12 17:04:53 +0100

    fix(healthresource): returns a Json.

--- src/health/HealthResource.ts
+++ src/health/HealthResource.ts
@@ -1,10 +1,10 @@
 import { get, Resource, route } from '../services';
 import { resolve } from '../utils';
 import { HealthUri } from './HealthUri';
-import { Result, toResult } from '../types';
+import { Json } from '../types';
 
 @route(HealthUri.Health)
 export class HealthResource implements Resource {
   @get()
-  ok = (): Promise<Result> => resolve(toResult('Endpoint is healthy'));
+  ok = (): Promise<Json> => resolve({ message: 'Endpoint is healthy' });
 }
