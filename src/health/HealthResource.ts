698dd06a2cc5ba3aaca68627f45795bec3f141f3
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-12 16:18:11 +0100

    fix(HealthResource): added route to HealthResource.ts

--- src/health/HealthResource.ts
+++ src/health/HealthResource.ts
@@ -1,6 +1,8 @@
-import { get, Resource } from '../services';
+import { get, Resource, route } from '../services';
 import { resolve } from '../utils';
+import { HealthUri } from './HealthUri';
 
+@route(HealthUri.Health)
 export class HealthResource implements Resource {
   @get()
   ok = (): Promise<string> => resolve('Endpoint is healthy');
