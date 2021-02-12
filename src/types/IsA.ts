f0de307dba95aa41fdb023eb1a1f348cfd6ab2ff
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-12 14:54:11 +0100

    fix(isA): altered implementation not calling getters anymore.

--- src/types/IsA.ts
+++ src/types/IsA.ts
@@ -1,6 +1,6 @@
-import { isDefined } from './Is';
+import { isPrimitive } from './Is';
 
-export const isA = <T>(t?: unknown, ...properties: (keyof T)[]): t is T => isDefined(t) && properties.every(p => isDefined((t as T)[p]));
+export const isA = <T>(t?: unknown, ...properties: (keyof T)[]): t is T => !isPrimitive(t) && properties.every(p => p.toString() in (t as any));
 export const isAn = isA;
 
 export const isError = (e: unknown): e is Error => isAn<Error>(e, 'name', 'message');
