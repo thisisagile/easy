6b87bb7b8bcc4aaffa36703005cd0304f03f1111
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-12 16:53:51 +0100

    fix(healthresource): returns a result, not a string.

--- src/health/HealthResource.ts
+++ src/health/HealthResource.ts
@@ -1,9 +1,10 @@
 import { get, Resource, route } from '../services';
 import { resolve } from '../utils';
 import { HealthUri } from './HealthUri';
+import { Result, toResult } from '../types';
 
 @route(HealthUri.Health)
 export class HealthResource implements Resource {
   @get()
-  ok = (): Promise<string> => resolve('Endpoint is healthy');
+  ok = (): Promise<Result> => resolve(toResult('Endpoint is healthy'));
 }
--- test/health/HealthUri.test.ts
+++ test/health/HealthUri.test.ts
@@ -2,7 +2,6 @@ import { HealthUri } from '../../src';
 import '@thisisagile/easy-test';
 
 describe('HealthUri', () => {
-
   test('return route', () => {
     expect(HealthUri.Health).toMatchRoute('/health');
     expect(HealthUri.Health.route('dev')).toMatchRoute('/dev/health');
