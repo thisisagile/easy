00af3c2d2cbfefeb7e583bfa8ada96d1e69c4dad
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-13 20:22:51 +0100

    fix(Validate): optimized code.

--- src/validation/Validate.ts
+++ src/validation/Validate.ts
@@ -1,65 +1,43 @@
-import {
-  Enum,
-  isDefined,
-  isEnum,
-  isResults,
-  isValidatable,
-  isValue,
-  list,
-  List,
-  meta,
-  Result,
-  results,
-  Results,
-  Text,
-  toName,
-  toResult,
-  Value,
-} from '../types';
+import { Enum, isDefined, isEnum, isResults, isValidatable, isValue, list, List, meta, results, Results, Text, toResult, Value } from '../types';
 import { Constraint } from './Contraints';
 import { when } from './When';
-import { asText, choose } from '../utils';
+import { choose, ParseOptions, toText } from '../utils';
 
 export type Validator = { property: string; constraint: Constraint; text: Text; actual?: unknown };
 
-const asResult = (subject?: unknown, message?: Text): Result => toResult(message, toName(subject));
+export const asResults = (subject: unknown, template: Text, options: ParseOptions = {}): Results => results(toResult(toText(subject, template, options)));
 
 const validators = (subject: unknown): List<Validator> =>
   meta(subject)
     .keys<List<Validator>>('constraint')
     .reduce((list, vs) => list.add(vs), list<Validator>());
 
-const run = (v: Validator, subject?: unknown): Results => {
-  console.log('run', toName(subject), v.property);
+const runValidator = (v: Validator, subject?: unknown): Results => {
   v.actual = (subject as any)[v.property];
   const res = v.constraint(v.actual);
-  return isResults(res) ? res : !res ? results(asResult(subject, asText(subject, v.text, v))) : undefined;
+  return isResults(res) ? res : !res ? asResults(subject, v.text, v) : undefined;
 };
 
-const val = (subject?: unknown): Results => {
-  console.log('val', toName(subject));
-  const vals = validators(subject);
-  const res = vals.mapDefined(v => run(v, subject));
-  return res.reduce((rs, r) => rs.add(...r.results), results());
-};
+const constraints = (subject?: unknown): Results =>
+  validators(subject)
+    .mapDefined(v => runValidator(v, subject))
+    .reduce((rs, r) => rs.add(...r.results), results());
 
-export const validate = (subject?: unknown): Results => {
-  console.log('validate', toName(subject));
-  return choose<Results, unknown>(subject)
+export const validate = (subject?: unknown): Results =>
+  choose<Results, unknown>(subject)
     .case(s => !isDefined(s), results('Subject is not defined.'))
     .case(
       s => isEnum(s),
-      (e: Enum) => (e.isValid ? results() : results(asText(e, 'This is not a valid {type.name}.')))
+      (e: Enum) => (e.isValid ? results() : asResults(e, 'This is not a valid {type.name}.'))
     )
     .case(
       s => isValue(s),
-      (v: Value) => (v.isValid ? results() : results(asText(v, 'This is not a valid {type.name}.')))
+      (v: Value) => (v.isValid ? results() : asResults(v, 'This is not a valid {type.name}.'))
     )
     .case(
       s => isValidatable(s),
-      v => val(v)
+      v => constraints(v)
     )
     .else(results());
-};
 
-export const validateReject = <T>(subject: T): Promise<T> => when(subject).not.isValid.reject();
+export const validateReject = <T>(subject?: T): Promise<T> => when(subject).not.isValid.reject();
