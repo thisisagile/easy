c061179273c8eb6dc6800bfdd78402ba56a918dc
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-14 12:17:57 +0100

    fix(reject): adjusted reject() function to

--- src/utils/Promise.ts
+++ src/utils/Promise.ts
@@ -1,4 +1,4 @@
-import { isError, isText, results, Results, Text } from '../types';
+import { isError, isResults, results, Results, Text } from '../types';
 
 export const resolve = <S>(subject: S | PromiseLike<S>): Promise<S> => Promise.resolve(subject);
-export const reject = <S>(e: Text | Error | Results): Promise<S> => Promise.reject(isError(e) ? e : isText(e) ? results(e) : e);
+export const reject = <S>(e: Text | Error | Results): Promise<S> => Promise.reject(isError(e) ? e : isResults(e) ? e : results(e));
