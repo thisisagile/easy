92ae2ea80a9ba0f3cdd6488be3ca94d03adfa5ca
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-15 08:52:47 +0100

    fix(Service): change const to function, to make inheritance and overriding functions of Service.ts class.

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -13,22 +13,22 @@ export class Service extends Enum {
   pre = (): Handler[] => [];
   post = (): Handler[] => [];
 
-  with = (...resources: Constructor<Resource>[]): this => {
+  with(...resources: Constructor<Resource>[]): this {
     this.resources.add(resources.map(r => new r()));
     return this;
-  };
+  }
 
-  atPort = (port: number): this => {
+  atPort(port: number): this {
     this.port = port;
     return this;
-  };
+  }
 
-  start = (message = `Service ${this.name} listening on port ${this.port} with ${this.resources.length} resources.`): void => {
+  start(message = `Service ${this.name} listening on port ${this.port} with ${this.resources.length} resources.`): void {
     this.pre().forEach(h => this.app.use(h));
     this.resources.forEach(r => this.app.route(this, r));
     this.post().forEach(h => this.app.use(h));
     this.app.listen(this.port, message);
-  };
+  }
 }
 
 export const service = (name: string): Service => new Service(name);
