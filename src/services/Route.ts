8dd4b40930de782d9d51a105cf6dfb681d0ffe7a
Jeroen Plug <jeroen.plug@gmail.com>
2021-02-16 11:55:38 +0100

    feat(Route): add requires from endpoint metadata

--- src/services/Route.ts
+++ src/services/Route.ts
@@ -2,13 +2,15 @@ import { List, meta, Uri } from '../types';
 import { Verb } from './Verb';
 import { Req } from './Req';
 import { Resource } from './Resource';
+import { Scope, UseCase } from '../process';
 
 export const route = (uri: Uri): ClassDecorator => (subject: unknown): void => {
   meta(subject).set('route', uri);
 };
 
 export type Endpoint<T = unknown> = (re: Req) => Promise<T | List<T>>;
-export type Route = { verb: Verb; endpoint: Endpoint };
+export type RouteRequires = { token: boolean; scope?: Scope; uc?: UseCase };
+export type Route = { verb: Verb; endpoint: Endpoint; requires: RouteRequires };
 export type Routes = { route: Uri; endpoints: List<Route> };
 
 class Router implements Routes {
@@ -21,7 +23,15 @@ class Router implements Routes {
   get endpoints(): List<Route> {
     return meta(this.resource)
       .properties('verb')
-      .map(p => ({ verb: p.get<Verb>('verb'), endpoint: this.resource[p.property] }));
+      .map(p => ({
+        verb: p.get<Verb>('verb'),
+        endpoint: this.resource[p.property],
+        requires: {
+          token: p.get<boolean>('token') ?? false,
+          scope: p.get<Scope>('scope'),
+          uc: p.get<UseCase>('uc'),
+        },
+      }));
   }
 }
 
--- test/services/Route.test.ts
+++ test/services/Route.test.ts
@@ -1,5 +1,5 @@
-import { HttpVerb, meta, route, routes, Uri } from '../../src';
-import { DevsResource, DevUri } from '../ref';
+import { HttpVerb, meta, route, routes, Scope, Uri, UseCase } from '../../src';
+import { DevResource, DevsResource, DevUri } from '../ref';
 import '@thisisagile/easy-test';
 
 describe('Route', () => {
@@ -17,5 +17,12 @@ describe('Router', () => {
     expect(route.route('dev')).toMatchText('/dev/developers');
     expect(endpoints).toHaveLength(2);
     expect(endpoints[0].verb.verb).toBe(HttpVerb.Get);
+    expect(endpoints[0].requires.token).toBeTruthy();
+  });
+
+  test('Router works on a secured class', () => {
+    const { endpoints } = routes(new DevResource());
+    expect(endpoints[0].requires.scope).toBe(Scope.Basic);
+    expect(endpoints[1].requires.uc).toBe(UseCase.ChangePassword);
   });
 });
