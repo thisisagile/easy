7ce083c53c66329a7e70611ef10d0c6c2eba7b5d
woutervanbakel <wouter.vb@gmail.com>
2021-02-17 15:11:17 +0100

    test(types): Additional tests for Context and minor refactoring

--- test/types/Context.test.ts
+++ test/types/Context.test.ts
@@ -1,32 +1,46 @@
-import { ctx, isUuid, NamespaceContext, toUuid } from '../../src';
+import { Context, ctx, NamespaceContext } from '../../src';
 import { host, port } from '../init';
 
-describe('Ctx', () => {
-  test('env context', () => {
+describe('Environment Context', () => {
+  test('default environment context', () => {
     expect(ctx.env.host).toBe(host);
     expect(ctx.env.port).toBe(port);
     expect(ctx.env.domain).toBe('easy');
+  });
+
+  test('override default environment context', () => {
+    process.env.DOMAIN = 'domain';
+    process.env.PORT = '42';
+    const c = new Context()
+    expect(c.env.domain).toBe('domain');
+    expect(c.env.port).toBe(42);
+  });
+
+  test('invalid env port results in default port', () => {
+    process.env.PORT = '';
+    const c = new Context()
+    expect(c.env.port).toBe(8080);
+  });
+
+  test('undefined environment variable', () => {
+    expect(ctx.env.get('')).toBeUndefined();
     expect(ctx.env.get('doesNotExist')).toBeUndefined();
+  });
+
+  test('defined environment variables', () => {
     process.env.VAR_EXISTS = 'value';
     expect(ctx.env.get('varExists')).toBe('value');
   });
+});
 
-  test('request context', () => {
-    ctx.request.create(() => {
-      ctx.request.token = '42';
-      ctx.request.correlationId = toUuid();
-      expect(ctx.request.token).toBe('42');
-      expect(isUuid(ctx.request.correlationId)).toBeTruthy();
-    });
-  });
-
-  test('other context', () => {
+describe('Other Context', () => {
+  test('can store and retrieve from other context', () => {
     ctx.other.id = 42;
     expect(ctx.other.id).toBe(42);
   });
 });
 
-describe('NamespaceRequestContext', () => {
+describe('NamespaceContext', () => {
   let context: NamespaceContext;
 
   beforeEach(() => {
