3e2f43f27e71959a17aba50554bbe115ec0ecd2a
woutervanbakel <wouter.vb@gmail.com>
2021-02-17 12:02:37 +0100

    chore(express): Add RequestContextHandler and SecurityHandler to index

--- src/express/index.ts
+++ src/express/index.ts
@@ -2,3 +2,5 @@ export * from './CorrelationHandler';
 export * from './ErrorHandler';
 export * from './ExpressProvider';
 export * from './NotFoundHandler';
+export * from './RequestContextHandler';
+export * from './SecurityHandler';
