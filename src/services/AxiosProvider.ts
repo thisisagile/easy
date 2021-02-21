01157e63f2697451f28fd0d09ce92cdba0833a45
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-21 12:28:25 +0100

    chore: extracted Response.ts

--- src/services/AxiosProvider.ts
+++ src/services/AxiosProvider.ts
@@ -1,25 +1,33 @@
 import axios, { AxiosError, Method } from 'axios';
-import { Request, Response, RequestProvider, toResponse } from './RequestProvider';
+import { RequestProvider } from './RequestProvider';
 import { RequestOptions } from './RequestOptions';
 import { isDefined, toResult, Uri } from '../types';
 import { choose } from '../utils';
 import { HttpVerb } from './HttpVerb';
 import { HttpStatus } from './HttpStatus';
+import { Response, toResponse } from './Response';
+import { Request } from './Request';
 
 const asResponse = (uri: Uri, verb: HttpVerb, error: AxiosError): Response =>
   choose<Response, AxiosError>(error)
     .case(
       e => isDefined(e.response),
-      e => toResponse(e.response.status, e.response.headers, toResult(e.response.statusText, verb, uri))
+      e => toResponse(e.response.status, e.response.headers, toResult(e.response.statusText, verb, uri)),
     )
     .case(
       e => isDefined(e.request),
-      e => toResponse(e.request.status, e.request.headers, toResult(e.request.statusText, verb, uri))
+      e => toResponse(e.request.status, e.request.headers, toResult(e.request.statusText, verb, uri)),
     )
     .else(e => toResponse(HttpStatus.InternalServerError.status, {}, toResult(e.message, verb, uri)));
 
 export class AxiosProvider implements RequestProvider {
-  execute = ({ uri, verb, body, transform = (r: any) => r, options = RequestOptions.Json }: Request): Promise<Response> =>
+  execute = ({
+               uri,
+               verb,
+               body,
+               transform = (r: any) => r,
+               options = RequestOptions.Json,
+             }: Request): Promise<Response> =>
     axios
       .request({
         url: uri.toString(),
