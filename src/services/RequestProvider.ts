ce54cb8af6caf0d9280d4b850fafd9496dfc9eab
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-21 12:21:52 +0100

    fix(response): extracted Response.ts

--- src/services/RequestProvider.ts
+++ src/services/RequestProvider.ts
@@ -1,30 +1,5 @@
-import { isA, Uri } from '../types';
-import { HttpVerb } from './HttpVerb';
-import { RequestOptions } from './RequestOptions';
-import { RestResult, toRestResult } from './RestResult';
-import { HttpStatus } from './HttpStatus';
-
-export type Request = {
-  uri: Uri;
-  verb: HttpVerb;
-  body?: unknown;
-  transform?: (r: any) => any;
-  options?: RequestOptions;
-};
-
-export type Response = {
-  status: HttpStatus;
-  headers?: { [key: string]: any };
-  body: RestResult;
-};
-
-export const toResponse = (status: number, headers?: { [key: string]: any }, body?: unknown): Response => ({
-  status: HttpStatus.byId(status),
-  headers,
-  body: toRestResult(body),
-});
-
-export const isResponse = (r?: unknown): r is Response => isA<Response>(r, 'status', 'body');
+import { Response } from './Response';
+import { Request } from './Request';
 
 export interface RequestProvider {
   execute: (request: Request) => Promise<Response>;
