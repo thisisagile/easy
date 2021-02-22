7d33af86b03fdd13a9aa1d97ff2d91c495580f4a
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-22 09:14:27 +0100

    fix(api): api now uses unknown body instead of Json, to allow for other types of bodies than Json.

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,20 +1,21 @@
-import { Json, Uri } from '../types';
+import { Uri } from '../types';
 import { HttpVerb, RequestOptions, RequestProvider, Response } from '../http';
 import { AxiosProvider } from './AxiosProvider';
 
 export class Api {
-  constructor(readonly provider: RequestProvider = new AxiosProvider()) {}
+  constructor(readonly provider: RequestProvider = new AxiosProvider()) {
+  }
 
   get = (uri: Uri, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
     this.provider.execute({ uri, verb: HttpVerb.Get, transform, options });
 
-  post = (uri: Uri, body?: Json, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
+  post = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
     this.provider.execute({ uri, verb: HttpVerb.Post, body, transform, options });
 
-  put = (uri: Uri, body?: Json, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
+  put = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
     this.provider.execute({ uri, verb: HttpVerb.Put, body, transform, options });
 
-  patch = (uri: Uri, body?: Json, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
+  patch = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
     this.provider.execute({ uri, verb: HttpVerb.Patch, body, transform, options });
 
   delete = (uri: Uri, options: RequestOptions = RequestOptions.Json, transform?: (b: any) => any): Promise<Response> =>
