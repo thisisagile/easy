22989321afbb3445e3cab644c415bce5d8a5cdfa
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-23 18:29:31 +0100

    fix(ifText): minor update, now returns string.

--- src/types/Text.ts
+++ src/types/Text.ts
@@ -5,4 +5,4 @@ export type Text = { toString(): string };
 
 export const isText = (t?: unknown): t is Text => isDefined(t?.toString) && isFunction(t.toString);
 
-export const ifText = (t?: unknown, alt: Get<Text> = '') => isText(t) ? t.toString() : ofGet(alt);
+export const ifText = (t?: unknown, alt: Get<string> = ''): string => isText(t) ? t.toString() : ofGet(alt);
