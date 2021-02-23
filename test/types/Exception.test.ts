bdfaf16df90d9e27736bdef14f2ae7edeffda51b
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-23 14:51:57 +0100

    fix: added Exception.ts tests.

--- test/types/Exception.test.ts
+++ test/types/Exception.test.ts
@@ -1,9 +1,10 @@
 import {Exception, isException} from '../../src/types/Exception';
-import {Dev} from '../ref';
+import {Dev, Language} from '../ref';
 
 describe('Exception', () => {
 
   const exc = new Exception('This is wrong');
+  const exc2 = new Exception('This is wrong');
 
   test('name and id are set properly', () => {
     expect(exc.message).toBe('This is wrong');
@@ -15,7 +16,7 @@ describe('Exception', () => {
     expect(isException()).toBeFalsy();
     expect(isException(exc)).toBeTruthy();
     expect(isException(Dev.Naoufal)).toBeFalsy();
-  })
+  });
 
   test('isException with text', () => {
     expect(isException(exc, '')).toBeFalsy();
@@ -23,6 +24,14 @@ describe('Exception', () => {
     expect(isException(exc, 'ThisIsWrong')).toBeTruthy();
     expect(isException(exc, Dev.Naoufal)).toBeFalsy();
     expect(isException(exc, Exception.DoesNotExist)).toBeFalsy();
-    expect(isException(exc, exc)).toBeTruthy();
-  })
+    expect(isException(exc, exc2)).toBeTruthy();
+  });
+
+  test('equals', () => {
+    expect(exc.equals('')).toBeFalsy();
+    expect(exc.equals('Does not exist')).toBeFalsy();
+    expect(exc.equals(Language.Java)).toBeFalsy();
+    expect(exc.equals(Exception.DoesNotExist)).toBeFalsy();
+    expect(exc.equals(exc2)).toBeTruthy();
+  });
 });
