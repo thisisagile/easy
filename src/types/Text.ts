cf66fa62b2eeea45834b3e75bd2cccadf5c54c6d
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-24 10:24:25 +0100

    fix(toString): renamed ifText() to toString().

--- src/types/Text.ts
+++ src/types/Text.ts
@@ -1,8 +1,8 @@
-import {isDefined, isFunction} from './Is';
-import {Get, ofGet} from './Constructor';
+import { isDefined, isFunction } from './Is';
+import { Get, ofGet } from './Constructor';
 
 export type Text = { toString(): string };
 
 export const isText = (t?: unknown): t is Text => isDefined(t?.toString) && isFunction(t.toString);
 
-export const ifText = (t?: unknown, alt: Get<string> = ''): string => isText(t) ? t.toString() : ofGet(alt);
+export const toString = (t?: unknown, alt: Get<Text> = ''): string => (isText(t) ? t : ofGet(alt)).toString();
--- test/types/Text.test.ts
+++ test/types/Text.test.ts
@@ -1,8 +1,7 @@
-import {ifText, isText} from '../../src';
-import {Dev} from '../ref';
+import { toString, isText } from '../../src';
+import { Dev } from '../ref';
 
 describe('isText', () => {
-
   const has = { toString: 3 };
 
   test('correct', () => {
@@ -15,16 +14,16 @@ describe('isText', () => {
   });
 
   test('ifText without alt', () => {
-    expect(ifText()).toBe('');
-    expect(ifText('hallo')).toBe('hallo');
-    expect(ifText(Dev.Jeroen)).toBe('Jeroen');
+    expect(toString()).toBe('');
+    expect(toString('hallo')).toBe('hallo');
+    expect(toString(Dev.Jeroen)).toBe('Jeroen');
   });
 
   test('ifText with alt', () => {
-    expect(ifText(undefined, 'alt')).toBe('alt');
-    expect(ifText(undefined, () => 'alt')).toBe('alt');
-    expect(ifText(has, () => 'alt')).toBe('alt');
-    expect(ifText('hallo', 'alt')).toBe('hallo');
-    expect(ifText(Dev.Jeroen, Dev.Naoufal)).toBe('Jeroen');
+    expect(toString(undefined, 'alt')).toBe('alt');
+    expect(toString(undefined, () => 'alt')).toBe('alt');
+    expect(toString(has, () => 'alt')).toBe('alt');
+    expect(toString('hallo', 'alt')).toBe('hallo');
+    expect(toString(Dev.Jeroen, Dev.Naoufal)).toBe('Jeroen');
   });
 });
