8f0c2bea376624d4f50c6ba5f6ac808e0edcc45d
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-24 21:20:48 +0100

    fix(Promise): simplified code.

--- src/utils/Promise.ts
+++ src/utils/Promise.ts
@@ -1,4 +1,4 @@
-import { isError, isResults, results, Results, Text } from '../types';
+import { ErrorOrigin } from '../types';
 
 export const resolve = <S>(subject: S | PromiseLike<S>): Promise<S> => Promise.resolve(subject);
-export const reject = <S>(e: Text | Error | Results): Promise<S> => Promise.reject(isError(e) ? e : isResults(e) ? e : results(e));
+export const reject = <S>(e: ErrorOrigin): Promise<S> => Promise.reject(e);
