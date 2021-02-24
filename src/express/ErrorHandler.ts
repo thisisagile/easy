3bf730efa85a9e257e47f3eebc101d5edc670c41
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-24 21:08:39 +0100

    fix(errorhandler): from isString to isText.

--- src/express/ErrorHandler.ts
+++ src/express/ErrorHandler.ts
@@ -1,11 +1,9 @@
-import { ErrorOrigin, Exception, isError, isResults, isString, Result, Results, toResult } from '../types';
+import { ErrorOrigin, Exception, isA, isError, isResults, isText, Result, Results, toResult, toString } from '../types';
 import express from 'express';
 import { HttpStatus, isResponse, Response, rest } from '../http';
 import { choose } from '../utils';
 import { VerbOptions } from '../resources';
 import { isAuthError } from './AuthError';
-
-type CustomError = { error: ErrorOrigin; options?: VerbOptions };
 
 const toResponse = (status: HttpStatus, errors: Result[] = []): Response => ({
   status,
@@ -16,36 +14,41 @@ const toBody = (error: ErrorOrigin, options: VerbOptions = {}): Response => {
   return choose<Response, any>(error)
     .case(
       o => isAuthError(o),
-      o => toResponse(HttpStatus.Forbidden, [toResult(o.message)])
+      o => toResponse(HttpStatus.Forbidden, [toResult(o.message)]),
     )
     .case(
       o => Exception.DoesNotExist.equals(o),
-      o => toResponse(options?.onNotFound ?? HttpStatus.NotFound, [toResult(o.message)])
+      o => toResponse(options?.onNotFound ?? HttpStatus.NotFound, [toResult(o.message)]),
     )
     .case(
       // This service breaks with an error
       o => isError(o),
-      o => toResponse(HttpStatus.InternalServerError, [toResult(o.message)])
+      o => toResponse(HttpStatus.InternalServerError, [toResult(o.message)]),
     )
     .case(
       // This service fails
       o => isResults(o),
-      (o: Results) => toResponse(options?.onError ?? HttpStatus.BadRequest, o.results)
+      (o: Results) => toResponse(options?.onError ?? HttpStatus.BadRequest, o.results),
     )
     .case(
       // Underlying service fails
       o => isResponse(o),
-      (o: Response) => toResponse(HttpStatus.InternalServerError, o.body.error?.errors)
+      (o: Response) => toResponse(HttpStatus.InternalServerError, o.body.error?.errors),
     )
     .case(
       // This service fails with a string
-      o => isString(o),
-      (o: Response) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(o)])
+      o => isText(o),
+      (o: Response) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(toString(o))]),
     )
     .else(() => toResponse(HttpStatus.InternalServerError, [toResult('Unknown error')]));
 };
 
+type CustomError = { error: ErrorOrigin; options?: VerbOptions };
+
+const toCustomError = (e?: unknown): CustomError => (isA<CustomError>(e, 'error') ? e : { error: e });
+
 export const error = (e: CustomError | Error, req: express.Request, res: express.Response, _next: express.NextFunction): void => {
-  const { status, body } = toBody((e as any)?.error ?? e, (e as any)?.options);
+  const { error, options } = toCustomError(e);
+  const { status, body } = toBody(error, options);
   res.status(status.status).json(body);
 };
