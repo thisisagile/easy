e2def2c514b8f2a68f07569352a747393a3fa6a8
aahoogendoorn <aahoogendoorn@gmail.com>
2021-03-02 23:10:58 +0100

    fix(utils): update to reject() and resolve() methods.

--- src/utils/Promise.ts
+++ src/utils/Promise.ts
@@ -1,4 +1,4 @@
 import { ErrorOrigin } from '../types';
 
-export const resolve = <S>(subject: S | PromiseLike<S>): Promise<S> => Promise.resolve(subject);
-export const reject = <S>(e: ErrorOrigin): Promise<S> => Promise.reject(e);
+export const resolve = <S = unknown>(subject: S | PromiseLike<S>): Promise<S> => Promise.resolve(subject);
+export const reject = <S = never>(e: ErrorOrigin): Promise<S> => Promise.reject(e);
