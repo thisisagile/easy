e672a915e2e35c908922b50e5b290ff8586126ae
aahoogendoorn <aahoogendoorn@gmail.com>
2021-03-03 17:31:03 +0100

    fix(types): update constructor types.

--- src/types/Constructor.ts
+++ src/types/Constructor.ts
@@ -1,6 +1,6 @@
 import { isFunction } from './Is';
 
-export type Constructor<T = unknown> = { new (...args: any[]): T };
+export type Constructor<T = unknown> = { new (...args: unknown[]): T };
 
 export const toName = (subject: unknown, postfix = ''): string => subject?.constructor.name?.replace(postfix, '').toLowerCase() ?? '';
 
@@ -8,7 +8,7 @@ export type Get<T = unknown, Param = unknown> = T | ((...params: Param[]) => T);
 export type Predicate<Param> = Get<boolean, Param>;
 
 export const ofGet = <T, Param>(g: Get<T, Param>, ...params: Param[]): T => (isFunction(g) ? g(...params) : g);
-export const ifGet = <T>(pred: Get<unknown>, valid: Get<T>, invalid: Get<T>): T => (ofGet(pred) ? ofGet(valid) : ofGet(invalid));
+export const ifGet = <T>(pred: Get, valid: Get<T>, invalid: Get<T>): T => (ofGet(pred) ? ofGet(valid) : ofGet(invalid));
 
 export type GetProperty<T, U> = keyof T | ((t: T) => U);
 export const ofProperty = <T, U>(t: T, p: GetProperty<T, U>): U => (isFunction(p) ? p(t) : ((t[p] as unknown) as U));
