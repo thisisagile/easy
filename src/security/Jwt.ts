85a6896403e534d26fbc1aeea81b801b23191d35
woutervanbakel <wouter.vb@gmail.com>
2021-03-03 14:48:19 +0100

    fix(security): Prevent exporting of jsonwebtoken types

--- src/security/Jwt.ts
+++ src/security/Jwt.ts
@@ -1,18 +1,18 @@
-import jwt from 'jsonwebtoken';
+import { decode, sign, verify } from 'jsonwebtoken';
 import { Json, Value } from '../types';
 import { rule } from '../validation';
 
 export class Jwt extends Value {
-  static sign = (token: Json): Jwt => new Jwt(jwt.sign(token, 'secret', { expiresIn: '1h' }));
+  static sign = (token: Json): Jwt => new Jwt(sign(token, 'secret', { expiresIn: '1h' }));
 
   static of = (a: { jwt: string }): Jwt => new Jwt(a.jwt);
 
-  decode = (): Json => jwt.decode(this.value) as Json;
+  decode = (): Json => decode(this.value) as Json;
 
   @rule('Token is not valid')
   verify(): boolean {
     try {
-      jwt.verify(this.value, 'secret');
+      verify(this.value, 'secret');
       return true;
     } catch (e) {
       return false;
