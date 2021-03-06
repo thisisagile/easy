04169c115ce4075ff559ae9ede42bc54ac69110a
aahoogendoorn <aahoogendoorn@gmail.com>
2021-03-06 14:23:30 +0100

    chore: formatting

--- src/types/IsA.ts
+++ src/types/IsA.ts
@@ -1,4 +1,4 @@
-import {isPrimitive} from './Is';
+import { isPrimitive } from './Is';
 
 export const isA = <T>(t?: unknown, ...properties: (keyof T)[]): t is T => !isPrimitive(t) && properties.every(p => p.toString() in (t as any));
 export const isAn = isA;
