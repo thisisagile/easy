22ddba271850f4f73a76ee5acb4b167d3dee5f58
aahoogendoorn <aahoogendoorn@gmail.com>
2021-03-06 13:07:58 +0100

    test(express): improved tests on ErrorHandler.test.ts

--- test/express/ErrorHandler.test.ts
+++ test/express/ErrorHandler.test.ts
@@ -1,12 +1,14 @@
 import { fits, mock } from '@thisisagile/easy-test';
 import { NextFunction, Request, Response } from 'express';
-import { error, Exception, HttpStatus, rest, toResults, toOriginatedError } from '../../src';
+import { Code, error, Exception, HttpStatus, rest, toResults, toOriginatedError } from '../../src';
 
 describe('ErrorHandler', () => {
   const options = { onOk: HttpStatus.Ok, onNotFound: HttpStatus.Conflict, onError: HttpStatus.ImATeapot };
   let req: Request;
   let res: Response;
   let next: NextFunction;
+
+  const withError = (code: Code, errorCount: number = 1) => fits.with({ error: fits.with({ code, errorCount }) });
 
   beforeEach(() => {
     req = ({} as unknown) as Request;
@@ -17,43 +19,43 @@ describe('ErrorHandler', () => {
   test('simple error', () => {
     error(new Error('This is wrong'), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.InternalServerError.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 500, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(500));
   });
 
   test('error undefined', () => {
     error(toOriginatedError(undefined), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.InternalServerError.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 500, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(500));
   });
 
   test('error undefined with options', () => {
     error(toOriginatedError(undefined, options), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.InternalServerError.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 500, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(500));
   });
 
   test('error string', () => {
     error(toOriginatedError('help'), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.BadRequest.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 400, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(400));
   });
 
   test('error string with options', () => {
     error(toOriginatedError('help', options), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.ImATeapot.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 418, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(418));
   });
 
   test('error Error', () => {
     error(toOriginatedError(new Error('help')), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.InternalServerError.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 500, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(500));
   });
 
   test('error Error with options', () => {
     error(toOriginatedError(new Error('help'), options), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.InternalServerError.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 500, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(500));
   });
 
   test('error Does not exist', () => {
@@ -70,22 +72,23 @@ describe('ErrorHandler', () => {
     );
   });
 
+
   test('error Does not exist with options', () => {
     error(toOriginatedError(Exception.DoesNotExist, options), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.Conflict.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 409, errorCount: 1 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(409));
   });
 
   test('error with results', () => {
     error(toOriginatedError(toResults('help', 'me', 'please')), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.BadRequest.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 400, errorCount: 3 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(400, 3));
   });
 
   test('error with results and options', () => {
     error(toOriginatedError(toResults('help', 'me'), options), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.ImATeapot.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 418, errorCount: 2 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(418, 2));
   });
 
   const resp = {
@@ -96,12 +99,12 @@ describe('ErrorHandler', () => {
   test('error with response', () => {
     error(toOriginatedError(resp), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.InternalServerError.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 500, errorCount: 2 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(500, 2));
   });
 
   test('error with response and options', () => {
     error(toOriginatedError(resp, options), req, res, next);
     expect(res.status).toHaveBeenCalledWith(HttpStatus.InternalServerError.status);
-    expect(res.json).toHaveBeenCalledWith(fits.with({ error: fits.with({ code: 500, errorCount: 2 }) }));
+    expect(res.json).toHaveBeenCalledWith(withError(500, 2));
   });
 });
