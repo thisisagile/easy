dee09ac9586619691a533b6aa83730e25b6d2b4f
aahoogendoorn <aahoogendoorn@gmail.com>
2021-03-08 12:52:14 +0100

    fix(types): adding IsMissingId exception to Exception.ts

--- src/types/Exception.ts
+++ src/types/Exception.ts
@@ -4,6 +4,7 @@ import { isDefined } from './Is';
 import { stringify } from '../utils';
 
 export class Exception extends Enum {
+  static readonly IsMissingId = new Exception('Subject is missing an id');
   static readonly DoesNotExist = new Exception('Does not exist');
   static readonly IsNotImplemented = new Exception('Is not implemented');
   static readonly IsNotValid = new Exception('Is not valid');
