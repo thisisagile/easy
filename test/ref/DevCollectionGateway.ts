704551eca8faa9f3de4bea460930f07e109472e6
aahoogendoorn <aahoogendoorn@gmail.com>
2021-03-17 20:07:13 +0100

    fix(ref): DevCollectionGateway.ts tiny update.

--- test/ref/DevCollectionGateway.ts
+++ test/ref/DevCollectionGateway.ts
@@ -1,8 +1,8 @@
-import { CollectionGateway, resolve } from '../../src';
+import { CollectionGateway, resolve, toList } from '../../src';
 import { Dev } from './Dev';
 
 export class DevCollectionGateway extends CollectionGateway {
   constructor() {
-    super(resolve(Dev.All.toJSON()));
+    super(resolve(toList(Dev.All.toJSON())));
   }
 }
