07c784cc09811170469f847ee0089b3aab998df9
woutervanbakel <wouter.vb@gmail.com>
2021-03-23 11:57:21 +0100

    test(sql): fix test naming

--- test/ref/DevService.ts
+++ test/ref/DevService.ts
@@ -1,9 +1,5 @@
 import { error, Handler, notFound, Service } from '../../src';
 
-// export class TestService extends Service {
-//   static readonly Dev = new TestService('dev');
-// }
-
 export class DevService extends Service {
   pre = (): Handler[] => [];
   post = (): Handler[] => [notFound, error];
--- test/sql/Table.test.ts
+++ test/sql/Table.test.ts
@@ -52,44 +52,44 @@ describe('Table in and out', () => {
     expect(dev.properties).toHaveLength(4);
   });
 
-  test('empty table.from with undefined is undefined', () => {
+  test('empty table.in with undefined is undefined', () => {
     const j = empty.in();
     expect(j).toMatchObject({});
   });
 
-  test('empty table.to with undefined is undefined', () => {
+  test('empty table.out with undefined is undefined', () => {
     const j = empty.out();
     expect(j).toMatchObject({});
   });
 
-  test('empty table.from is correct', () => {
+  test('empty table.in is correct', () => {
     const j = empty.in(devData.jeroen);
     expect(j).toMatchObject(devData.jeroen);
   });
 
-  test('empty table.to is correct', () => {
+  test('empty table.out is correct', () => {
     const j = empty.out(devData.jeroen);
     expect(j).toMatchObject(devData.jeroen);
   });
 
-  test('table.to is correct', () => {
+  test('table.out is correct', () => {
     const j = dev.out(Dev.Jeroen.toJSON());
     expect(j).toEqual({ Id: 1, Language: 'TypeScript', Name: 'Jeroen', CodingLevel: '3' });
   });
 
-  test('table.from is correct', () => {
+  test('table.in is correct', () => {
     const j = dev.in(devData.jeroen);
     expect(j).toEqual({ id: 54, name: 'Jeroen', level: 3, language: 'TypeScript' });
   });
 
-  test('table.from without id uses default', () => {
+  test('table.in without id uses default', () => {
     const j = dev.in(devData.withoutId);
     expect(j).toEqual({ id: 42, name: 'Sander', level: 3, language: 'TypeScript' });
   });
 
   class UuidTable extends Table {}
 
-  test('table.from without id uses default uuid if not overriden', () => {
+  test('table.in without id uses default uuid if not overridden', () => {
     const j = new UuidTable().in({});
     expect(isUuid(j.id)).toBeTruthy();
   });
