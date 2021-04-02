f941884ad47870538643551595b293c706072221
Jeroen Plug <jeroen.plug@gmail.com>
2021-04-02 11:54:52 +0200

    refactor(domain): Extract DateTime test values to a constant

--- src/domain/DateTime.ts
+++ src/domain/DateTime.ts
@@ -1,7 +1,6 @@
 import { Value } from '../types';
 import moment from 'moment';
 
-
 export class DateTime extends Value {
   constructor(v?: string | number | Date) {
     super(moment(v).isValid() ? moment(v).toISOString() : '');
--- test/domain/DateTime.test.ts
+++ test/domain/DateTime.test.ts
@@ -1,18 +1,21 @@
 import { DateTime } from '../../src';
 
 describe('DateTime', () => {
+  const testDate = {
+    iso: '2021-03-25T08:39:44.000Z',
+    epoch: 1616661584000,
+  };
 
   test('from value return correct DateTime', () => {
     jest.spyOn(Date, 'now').mockImplementation(() => 1614589200000);
-    expect(new DateTime().toJSON()).toBe('2021-03-01T09:00:00.000Z');
-    expect(new DateTime(1616661584000).toJSON()).toBe('2021-03-25T08:39:44.000Z');
-    expect(new DateTime('2021-03-25T08:39:44.000Z').toJSON()).toBe('2021-03-25T08:39:44.000Z');
-    expect(new DateTime(new Date(1616661584000)).toJSON()).toBe('2021-03-25T08:39:44.000Z');
+    expect(new DateTime(testDate.epoch).toJSON()).toBe(testDate.iso);
+    expect(new DateTime(testDate.iso).toJSON()).toBe(testDate.iso);
+    expect(new DateTime(new Date(testDate.epoch)).toJSON()).toBe(testDate.iso);
   });
 
   test('now return correct DateTime', () => {
-    jest.spyOn(Date, 'now').mockImplementation(() => 1614589200000);
-    expect(DateTime.now.toJSON()).toBe('2021-03-01T09:00:00.000Z');
+    jest.spyOn(Date, 'now').mockImplementation(() => testDate.epoch);
+    expect(new DateTime().toJSON()).toBe(testDate.iso);
+    expect(DateTime.now.toJSON()).toBe(testDate.iso);
   });
-
 });
