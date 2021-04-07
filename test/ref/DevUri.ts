8233dd4b9997593fa4f24b9700dfa2719379c6e7
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-07 11:44:22 +0200

    fix(utils): changing DevUri.ts.

--- test/ref/DevUri.ts
+++ test/ref/DevUri.ts
@@ -3,9 +3,11 @@ import { EasyUri, uri } from '../../src';
 export class DevUri extends EasyUri {
   static readonly devs = uri.segment('developers');
   static readonly language = uri.query('language');
+  static readonly level = uri.query('level');
 
-  static readonly Developers = new DevUri([DevUri.devs]);
-  static readonly Developer = new DevUri([DevUri.devs, DevUri.id]);
+  static get Developers() { return new DevUri([DevUri.devs]); }
+  static get Developer() { return new DevUri([DevUri.devs, DevUri.id, DevUri.level]); }
 
   language = (f: string): this => this.set(DevUri.language, f);
+  level = (l: number): this => this.set(DevUri.level, l);
 }
