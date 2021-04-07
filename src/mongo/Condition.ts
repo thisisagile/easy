cf61fde27d48b3a5845c24c3deebbee98f34592e
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-07 14:28:22 +0200

    fix(mongo): adding Condition.ts.

---
+++ src/mongo/Condition.ts
@@ -0,0 +1,24 @@
+import { Json, JsonValue } from '../types';
+import { convert, Convert } from '../utils';
+
+export class Condition {
+  constructor(readonly key: string, readonly operator: string, readonly value: unknown) {}
+
+  and = (...others: Condition[]): LogicalCondition => new LogicalCondition('and', [this, ...others]);
+  or = (...others: Condition[]): LogicalCondition => new LogicalCondition('or', [this, ...others]);
+
+  toJSON(): Json {
+    return { [this.key]: { [`$${this.operator}`]: this.value as any } };
+  }
+}
+
+export class LogicalCondition {
+  constructor(readonly operator: string, readonly conditions: Condition[]) {}
+
+  toJSON(): Json {
+    return { [`$${this.operator}`]: this.conditions.map(c => c.toJSON()) };
+  }
+}
+
+export const toCondition = (field: string, operator: string, value: unknown, conv: Convert = convert.default): Condition =>
+  new Condition(field, operator, conv.from(value) as JsonValue);
---
+++ test/mongo/Condition.test.ts
@@ -0,0 +1,64 @@
+import { Collection, Condition, Field } from '../../src';
+import '@thisisagile/easy-test';
+
+class developers extends Collection {
+}
+
+describe('Condition', () => {
+  const field = new Field(new developers(), 'name');
+
+  test('simple', () => {
+    expect(new Condition(field.name, 'eq', 'Sander').toJSON()).toMatchObject({ name: { $eq: 'Sander' } });
+  });
+
+  test('exists', () => {
+    expect(field.exists(true).toJSON()).toMatchObject({ name: { $exists: true } });
+    expect(field.exists(false).toJSON()).toMatchObject({ name: { $exists: false } });
+  });
+
+  test('is', () => {
+    expect(field.is(12).toJSON()).toMatchObject({ name: { $eq: 12 } });
+  });
+
+  test('ne', () => {
+    expect(field.isNot(12).toJSON()).toMatchObject({ name: { $ne: 12 } });
+  });
+
+  test('in', () => {
+    expect(field.in(12, 13, 14).toJSON()).toMatchObject({ name: { $in: [12, 13, 14] } });
+  });
+
+  test('nin', () => {
+    expect(field.notIn(12, 13, 14).toJSON()).toMatchObject({ name: { $nin: [12, 13, 14] } });
+  });
+
+  test('gt', () => {
+    expect(field.greater(12).toJSON()).toMatchObject({ name: { $gt: 12 } });
+  });
+
+  test('gte', () => {
+    expect(field.greaterEqual(12).toJSON()).toMatchObject({ name: { $gte: 12 } });
+  });
+
+  test('lt', () => {
+    expect(field.less(12).toJSON()).toMatchObject({ name: { $lt: 12 } });
+  });
+
+  test('lte', () => {
+    expect(field.lessEqual(12).toJSON()).toMatchObject({ name: { $lte: 12 } });
+  });
+
+  test('google', () => {
+    expect(field.google('Sander').toJSON()).toMatchObject({ $text: { $search: 'Sander' } });
+  });
+
+  test('and', () => {
+    expect(field.is('Sander').and(field.isNot('Jeroen'), field.isNot('Wouter')).toJSON()).toMatchObject({
+      $and: [{ name: { $eq: 'Sander' } }, { name: { $ne: 'Jeroen' } }, { name: { $ne: 'Wouter' } }],
+    });
+  });
+
+  test('or', () => {
+    expect(field.is('Sander').or(field.isNot('Jeroen')).toJSON()).toMatchObject({ $or: [{ name: { $eq: 'Sander' } }, { name: { $ne: 'Jeroen' } }] });
+  });
+});
