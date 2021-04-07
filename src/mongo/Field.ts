5fecf0de83110b4658461dca245418aa25e260fd
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-07 14:28:00 +0200

    fix(mongo): adding Field.ts.

---
+++ src/mongo/Field.ts
@@ -0,0 +1,32 @@
+import { convert, Property, PropertyOptions } from '../utils';
+import { Collection } from './Collection';
+import { Condition, toCondition } from './Condition';
+import { toArray } from '../types';
+
+export class Field extends Property {
+  constructor(readonly owner: Collection, name: string, options: PropertyOptions = {}) {
+    super(owner, name, { dflt: options?.dflt, convert: options?.convert ?? convert.default });
+  }
+
+  is = (value: unknown): Condition => this.condition('eq', value);
+
+  isNot = (value: unknown): Condition => this.condition('ne', value);
+
+  in = (...value: unknown[]): Condition => this.condition('in', toArray(value));
+
+  notIn = (...value: unknown[]): Condition => this.condition('nin', toArray(value));
+
+  exists = (does: boolean): Condition => this.condition('exists', does);
+
+  greater = (value: unknown): Condition => this.condition('gt', value);
+
+  greaterEqual = (value: unknown): Condition => this.condition('gte', value);
+
+  less = (value: unknown): Condition => this.condition('lt', value);
+
+  lessEqual = (value: unknown): Condition => this.condition('lte', value);
+
+  google = (value: unknown): Condition => toCondition('$text', 'search', value);
+
+  protected condition = (operator: string, value: unknown): Condition => toCondition(this.name, operator, value, this?.options?.convert);
+}
---
+++ test/mongo/Field.test.ts
@@ -0,0 +1,13 @@
+import { Collection, Field } from '../../src';
+import '@thisisagile/easy-test';
+
+class developers extends Collection {
+}
+
+describe('Field', () => {
+  const field = new Field(new developers(), 'name');
+
+  test('new field', () => {
+    expect(field.name).toBe('name');
+  });
+});
