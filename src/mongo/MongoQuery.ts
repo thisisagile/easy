98a160ba7a156345ea2417eb071657acb80f9786
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-07 14:29:13 +0200

    fix(mongo): removed MongoQuery.ts

--- src/mongo/MongoQuery.ts
+++
@@ -1,6 +0,0 @@
-import { Query } from '../data';
-
-export class MongoQuery implements Query {
-  constructor(readonly query: string) {}
-}
-export const mongo = (q: string): MongoQuery => new MongoQuery(q);
--- test/mongo/MongoQuery.test.ts
+++
@@ -1,8 +0,0 @@
-import { mongo, MongoQuery } from '../../src';
-
-describe('MongoQuery', () => {
-  test('new query', () => {
-    expect(mongo('query')).toBeInstanceOf(MongoQuery);
-    expect(mongo('q').query).toBe('q');
-  });
-});
