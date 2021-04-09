620c28c5b2f1f6a993b1fdd85f2d0f0247a58165
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-09 16:43:43 +0200

    fix(types): Args for Get ar now any by default.

--- src/types/Constructor.ts
+++ src/types/Constructor.ts
@@ -8,7 +8,7 @@ export const toName = (subject?: unknown, postfix = ''): string => (subject as a
 export type Get<T = any, Args = any> = T | Func<T, Args>;
 export type Predicate<Args> = Get<boolean, Args>;
 
-export const ofGet = <T, Args>(g: Get<T, Args>, ...args: Args[]): T => (isFunc<T, Args>(g) ? g(...args) : g);
+export const ofGet = <T, Args = any>(g: Get<T, Args>, ...args: Args[]): T => (isFunc<T, Args>(g) ? g(...args) : g);
 export const ifGet = <T>(pred: Get, valid: Get<T>, invalid: Get<T>): T => (ofGet(pred) ? ofGet(valid) : ofGet(invalid));
 
 export type GetProperty<T, Prop> = keyof T | Func<Prop, T>;
