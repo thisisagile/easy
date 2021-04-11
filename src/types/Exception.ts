73994cf981b7a36b96055bd5079b1a18b9d9d178
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-11 21:13:49 +0200

    fix(types): added some exceptions, with types now.

--- src/types/Exception.ts
+++ src/types/Exception.ts
@@ -3,10 +3,10 @@ import { Enum } from './Enum';
 import { isDefined } from './Is';
 
 export class Exception extends Enum {
-  static readonly CouldNotExecute = (target: Text, error: Text) => new Exception(`Could not execute on ${target} because ${error}.`);
+  static readonly CouldNotExecute = (target: Text, error: Text): Exception => new Exception(`Could not execute on ${target} because ${error}.`);
   static readonly IsMissingId = new Exception('Subject is missing an id');
   static readonly DoesNotExist = new Exception('Does not exist');
-  static readonly EnvironmentVariableNotFound = (variable: Text) => new Exception(`Environment variable ${text(variable).upper} could not be found.`);
+  static readonly EnvironmentVariableNotFound = (variable: Text): Exception => new Exception(`Environment variable ${text(variable).upper} could not be found.`);
   static readonly IsNotImplemented = new Exception('Is not implemented');
   static readonly IsNotValid = new Exception('Is not valid');
   static readonly Unknown = new Exception('Unknown error');
