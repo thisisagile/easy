f80bb246d3265193cf78509900f5ec939fa33e2f
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-11 21:25:27 +0200

    chore: formatting

--- src/types/Exception.ts
+++ src/types/Exception.ts
@@ -6,7 +6,8 @@ export class Exception extends Enum {
   static readonly CouldNotExecute = (target: Text, error: Text): Exception => new Exception(`Could not execute on ${target} because ${error}.`);
   static readonly IsMissingId = new Exception('Subject is missing an id');
   static readonly DoesNotExist = new Exception('Does not exist');
-  static readonly EnvironmentVariableNotFound = (variable: Text): Exception => new Exception(`Environment variable ${text(variable).upper} could not be found.`);
+  static readonly EnvironmentVariableNotFound = (variable: Text): Exception =>
+    new Exception(`Environment variable ${text(variable).upper} could not be found.`);
   static readonly IsNotImplemented = new Exception('Is not implemented');
   static readonly IsNotValid = new Exception('Is not valid');
   static readonly Unknown = new Exception('Unknown error');
