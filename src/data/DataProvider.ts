6362c5c42740a04086c60fe510d64aedf2f91f8f
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-11 21:30:13 +0200

    fix(data): simplified DataProvider.ts

--- src/data/DataProvider.ts
+++ src/data/DataProvider.ts
@@ -1,9 +1,1 @@
-import { Json, List } from '../types';
-import { Query } from './Query';
-
 export type DataProvider = any;
-
-export interface SqlProvider extends DataProvider {
-  query: (q: Query) => Promise<List<Json>>;
-  command: (q: Query) => Promise<number>;
-}
