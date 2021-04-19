51e49e3e19af5ddd82bdc4e49c553a3e24292988
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-19 15:33:18 +0200

    fix(types): added isConstructor() and ofConstruct()

--- src/types/Constructor.ts
+++ src/types/Constructor.ts
@@ -1,7 +1,13 @@
 import { Get } from './Get';
+import { isFunc } from './Func';
+import { isDefined } from './Is';
 
 export type Constructor<T = unknown> = { new (...args: any[]): T };
 
 export type Construct<T> = Get<T> | Constructor<T>;
+
+export const isConstructor = <T>(c?: unknown): c is Constructor<T> => (isDefined(c) && isFunc<T, unknown>(c) && c.prototype && c.prototype.constructor) === c;
+
+export const ofConstruct = <T>(c: Construct<T>, ...args: unknown[]): T => (isConstructor<T>(c) ? new c(...args) : isFunc<T, unknown>(c) ? c(...args) : c);
 
 export const toName = (subject?: unknown, postfix = ''): string => (subject as any)?.constructor?.name?.replace(postfix, '').toLowerCase() ?? '';
--- test/types/Constructor.test.ts
+++ test/types/Constructor.test.ts
@@ -1,10 +1,70 @@
 import { Dev, DevUri } from '../ref';
-import { toName } from '../../src';
+import { isConstructor, ofConstruct, toName, text } from '../../src';
 
 describe('toName', () => {
   test('check', () => {
     expect(toName(undefined)).toBe('');
     expect(toName(Dev.Naoufal)).toBe('dev');
     expect(toName(DevUri.Developers, 'Uri')).toBe('dev');
+  });
+});
+
+class Tester {
+  constructor(readonly name = 'Jeroen', readonly level = 3) {}
+}
+
+describe('isConstructor', () => {
+  test('false', () => {
+    expect(isConstructor()).toBeFalsy();
+    expect(isConstructor(3)).toBeFalsy();
+    expect(isConstructor('3')).toBeFalsy();
+    expect(isConstructor({})).toBeFalsy();
+    expect(
+      isConstructor(() => {
+        'Naoufal';
+      })
+    ).toBeFalsy();
+    expect(isConstructor(() => text('Yes'))).toBeFalsy();
+    expect(isConstructor(text)).toBeFalsy();
+    expect(isConstructor(new Tester())).toBeFalsy();
+    expect(isConstructor(new Tester())).toBeFalsy();
+  });
+
+  test('true', () => {
+    expect(isConstructor(String)).toBeTruthy();
+    expect(isConstructor(Symbol)).toBeTruthy();
+    expect(isConstructor(Tester)).toBeTruthy();
+  });
+});
+
+describe('ofConstruct', () => {
+  test('ofConstruct with instance', () => {
+    const t = ofConstruct(new Tester('Sander', 4));
+    expect(t.name).toBe('Sander');
+    expect(t.level).toBe(4);
+  });
+
+  test('ofConstruct with function', () => {
+    const t = ofConstruct(() => new Tester('Sander', 4));
+    expect(t.name).toBe('Sander');
+    expect(t.level).toBe(4);
+  });
+
+  test('ofConstruct with constructor', () => {
+    const t = ofConstruct(Tester);
+    expect(t.name).toBe('Jeroen');
+    expect(t.level).toBe(3);
+  });
+
+  test('ofConstruct with constructor and one parameter', () => {
+    const t = ofConstruct(Tester, 'Wouter');
+    expect(t.name).toBe('Wouter');
+    expect(t.level).toBe(3);
+  });
+
+  test('ofConstruct with constructor and multiple different parameters', () => {
+    const t = ofConstruct(Tester, 'Wouter', 4);
+    expect(t.name).toBe('Wouter');
+    expect(t.level).toBe(4);
   });
 });
