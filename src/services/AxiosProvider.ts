89d1ecc4c0cbc1ece45b58834624e2fe8493e3f1
Naoufal Lamarti <n.lamarti@outlook.com>
2021-04-22 14:06:17 +0200

    fix: formatting

--- src/services/AxiosProvider.ts
+++ src/services/AxiosProvider.ts
@@ -1,14 +1,5 @@
 import axios, { AxiosError, Method } from 'axios';
-import {
-  HttpStatus,
-  HttpVerb,
-  isRestResult,
-  Request,
-  RequestOptions,
-  RequestProvider,
-  Response,
-  toResponse,
-} from '../http';
+import { HttpStatus, HttpVerb, isRestResult, Request, RequestOptions, RequestProvider, Response, toResponse } from '../http';
 import { ctx, isDefined, toResult, Uri } from '../types';
 import { choose } from '../utils';
 
