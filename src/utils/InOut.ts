6eb63f5ccc0e7119a77d034c16e73f87c8608aae
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-23 11:53:20 +0200

    feat(utils): added Mapper. Will be merged with Map (has more functionality, and Map is reserved keyword).

--- src/utils/InOut.ts
+++ src/utils/InOut.ts
@@ -1,9 +1,74 @@
-import { isA, Json, JsonValue } from '../types';
+import { Get, isA, isEmpty, json, Json, JsonValue, List, meta, ofGet } from '../types';
+import { MapOptions } from './Map';
 
-type InOut = {
-  name: string,
-  in: (key: string, source?: Json) => JsonValue | undefined,
-  out: (key: string, source?: Json) => JsonValue | undefined,
+export type InOut = {
+  name: string;
+  in: (key: string, source?: Json) => JsonValue | undefined;
+  out: (key: string, source?: Json) => JsonValue | undefined;
+};
+
+export const isInOut = (io?: unknown): io is InOut => isA<InOut>(io, 'in', 'out');
+
+export class Mapper implements InOut {
+  private props?: List<[string, InOut]>;
+
+  constructor(readonly name: string = '', public options: MapOptions = { startFrom: 'scratch' }) {}
+
+  get properties(): List<[string, InOut]> {
+    return (
+      this.props ??
+      (this.props = meta(this)
+        .entries<InOut>()
+        .filter(([, v]) => isInOut(v)))
+    );
+  }
+
+  in = (key: string, from: Json = {}): Json =>
+    this.properties.reduce(
+      (a, [k, p]) =>
+        json.omit(
+          {
+            ...a,
+            [k]: p.in(k, { ...a, ...from }),
+          },
+          p.name
+        ),
+      this.options.startFrom === 'source' ? from : {}
+    );
+
+  out = (key: string, to: Json = {}): Json =>
+    this.properties.reduce(
+      (a, [k, p]) => {
+        return json.omit(json.merge(a, isEmpty(p.name) ? p.out(k, to) : { [p.name]: p.out(k, { ...a, ...to }) }), k);
+      },
+      this.options.startFrom === 'source' ? to : {}
+    );
 }
 
-const isInOut = (io?: unknown): io is InOut => isA<InOut>(io, 'in', 'out');
+export const inout = {
+  prop: (name: string): InOut => ({
+    name,
+    in: (key: string, source: Json = {}): JsonValue => source[name],
+    out: (key: string, source: Json = {}): JsonValue => source[key],
+  }),
+  ignore: (name: string): InOut => ({
+    name,
+    in: (): JsonValue | undefined => undefined,
+    out: (): JsonValue | undefined => undefined,
+  }),
+  func: (name: string, funcIn: Get<JsonValue, Json>, funcOut: Get<JsonValue, Json>): InOut => ({
+    name,
+    in: (key: string, source: Json = {}): JsonValue => ofGet(funcIn, source),
+    out: (key: string, source: Json = {}): JsonValue => ofGet(funcOut, source),
+  }),
+  add: (funcIn: Get<JsonValue, Json>): InOut => ({
+    name: '',
+    in: (key: string, source: Json = {}): JsonValue => ofGet(funcIn, source),
+    out: (): JsonValue | undefined => undefined,
+  }),
+  map: (name = '', mapper: Mapper): InOut => ({
+    name,
+    in: (key: string, source: Json = {}): JsonValue => mapper.in(key, isEmpty(name) ? source : (source[name] as Json)),
+    out: (key: string, source: Json = {}): JsonValue => mapper.out(name, isEmpty(key) ? source : (source[key] as Json)),
+  }),
+};
---
+++ test/utils/InOut.test.ts
@@ -0,0 +1,176 @@
+import { inout, Mapper } from '../../src';
+import { Json, JsonValue } from '@thisisagile/easy-test/dist/utils/Types';
+
+const site = 'www.acme.com';
+const city = 'Amsterdam';
+const CityName = 'Amsterdam';
+const country = 'NL';
+const Country = 'NL';
+
+describe('Mapper', () => {
+  test('empty mapper should return original in', () => {
+    const scratch = new Mapper().in('', { CityName, Country });
+    expect(scratch).toStrictEqual({});
+    const source = new Mapper('', { startFrom: 'source' }).in('', { CityName, Country });
+    expect(source).toStrictEqual({ CityName, Country });
+  });
+
+  test('empty mapper should return original out', () => {
+    const scratch = new Mapper().out('', { CityName, Country });
+    expect(scratch).toStrictEqual({});
+    const source = new Mapper('', { startFrom: 'source' }).out('', { CityName, Country });
+    expect(source).toStrictEqual({ CityName, Country });
+  });
+
+  class SingleMapper extends Mapper {
+    readonly city = inout.prop('CityName');
+  }
+
+  test('single should return in', () => {
+    const scratch = new SingleMapper().in('', { CityName, Country });
+    expect(scratch).toStrictEqual({ city });
+    const source = new SingleMapper('', { startFrom: 'source' }).in('', { CityName, Country });
+    expect(source).toStrictEqual({ city, Country });
+  });
+
+  test('single should return out', () => {
+    const scratch = new SingleMapper().out('', { city, Country });
+    expect(scratch).toStrictEqual({ CityName });
+    const source = new SingleMapper('', { startFrom: 'source' }).out('', { city, Country });
+    expect(source).toStrictEqual({ CityName, Country });
+  });
+
+  class IgnoreMapper extends Mapper {
+    readonly city = inout.ignore('CityName');
+    readonly country = inout.prop('Country');
+  }
+
+  test('ignore should return in', () => {
+    const scratch = new IgnoreMapper().in('', { CityName, Country });
+    expect(scratch).toStrictEqual({ country });
+    const source = new IgnoreMapper('', { startFrom: 'source' }).in('', { CityName, Country });
+    expect(source).toStrictEqual({ country });
+  });
+
+  test('ignore should return out', () => {
+    const scratch = new IgnoreMapper().out('', { city, country });
+    expect(scratch).toStrictEqual({ Country });
+    const source = new IgnoreMapper('', { startFrom: 'source' }).out('', { city, country });
+    expect(source).toStrictEqual({ Country });
+  });
+
+  const region = (a: Json): string => (a.CityName === city ? 'city' : 'farmers');
+  const listMe = (a: Json): JsonValue[] => [a.city, a.region, a.country];
+
+  class FuncMapper extends Mapper {
+    readonly city = inout.prop('CityName');
+    readonly country = inout.func('Country', 'NL', 'nl');
+    readonly region = inout.add(a => region(a));
+    readonly list = inout.add(a => listMe(a));
+  }
+
+  test('func should return in', () => {
+    const scratch = new FuncMapper().in('', { CityName });
+    expect(scratch).toStrictEqual({ city, country, region: 'city', list: ['Amsterdam', 'city', 'NL'] });
+    const source = new FuncMapper('', { startFrom: 'source' }).in('', { CityName });
+    expect(source).toStrictEqual({ city, country, region: 'city', list: ['Amsterdam', 'city', 'NL'] });
+  });
+
+  test('func should return out', () => {
+    const scratch = new FuncMapper().out('', { city, Country });
+    expect(scratch).toStrictEqual({ CityName, Country: 'nl' });
+    const source = new FuncMapper('', { startFrom: 'source' }).out('', { city, Country });
+    expect(source).toStrictEqual({ CityName, Country: 'nl' });
+  });
+
+  class DoubleColumnMapper extends Mapper {
+    readonly id = inout.prop('Id');
+    readonly classicId = inout.prop('Id');
+  }
+
+  test('double columns should return in', () => {
+    const scratch = new DoubleColumnMapper().in('', { Id: 42, Country });
+    expect(scratch).toStrictEqual({ id: 42, classicId: 42 });
+    const source = new DoubleColumnMapper('', { startFrom: 'source' }).in('', { Id: 42, Country });
+    expect(source).toStrictEqual({ id: 42, classicId: 42, Country });
+  });
+
+  test('double columns should return out', () => {
+    const scratch = new DoubleColumnMapper().out('', { id: 42, classicId: 42, Country });
+    expect(scratch).toStrictEqual({ Id: 42 });
+    const source = new DoubleColumnMapper('', { startFrom: 'source' }).out('', {
+      id: 42,
+      classicId: 42,
+      Country,
+    });
+    expect(source).toStrictEqual({ Id: 42, Country });
+  });
+
+  class CompanyMapper extends Mapper {
+    readonly title = inout.prop('Name');
+    readonly site = inout.prop('Website');
+  }
+
+  class UseMapperMapper extends Mapper {
+    readonly id = inout.prop('Id');
+    readonly company = inout.map('', new CompanyMapper(''));
+  }
+
+  test('sub maps should return in', () => {
+    const scratch = new UseMapperMapper().in('', { Id: 42, Country, Name: 'Acme', Website: site });
+    expect(scratch).toStrictEqual({ id: 42, company: { title: 'Acme', site } });
+    const source = new UseMapperMapper('', { startFrom: 'source' }).in('', {
+      Id: 42,
+      Country,
+      Name: 'Google',
+      Website: site,
+    });
+    expect(source).toStrictEqual({
+      id: 42,
+      Country,
+      Name: 'Google',
+      Website: site,
+      company: { title: 'Google', site },
+    });
+  });
+
+  test('sub maps should return out', () => {
+    const scratch = new UseMapperMapper().out('', { id: 42, company: { title: 'Acme', site } });
+    expect(scratch).toStrictEqual({ Id: 42, Name: 'Acme', Website: site });
+    const source = new UseMapperMapper('', { startFrom: 'source' }).out('', {
+      id: 42,
+      company: { title: 'Acme', site },
+    });
+    expect(source).toStrictEqual({ Id: 42, Name: 'Acme', Website: site });
+  });
+
+  class UseMapperPropertyMapper extends Mapper {
+    readonly id = inout.prop('Id');
+    readonly company = inout.map('Company', new CompanyMapper(''));
+  }
+
+  test('sub maps from sub object should return in', () => {
+    const scratch = new UseMapperPropertyMapper().in('', {
+      Id: 42,
+      Country,
+      Company: { Name: 'Acme', Website: site },
+    });
+    expect(scratch).toStrictEqual({ id: 42, company: { title: 'Acme', site } });
+    const source = new UseMapperPropertyMapper('', { startFrom: 'source' }).in('', {
+      Id: 42,
+      Country,
+      Company: { Name: 'Google', Website: site },
+    });
+    expect(source).toStrictEqual({ id: 42, Country, company: { title: 'Google', site } });
+  });
+
+  test('sub maps from sub object should return out', () => {
+    const scratch = new UseMapperPropertyMapper().out('', { id: 42, company: { title: 'Acme', site } });
+    expect(scratch).toStrictEqual({ Id: 42, Company: { Name: 'Acme', Website: site } });
+    const source = new UseMapperPropertyMapper('', { startFrom: 'source' }).out('', {
+      id: 42,
+      company: { title: 'Acme', site },
+    });
+    expect(source).toStrictEqual({ Id: 42, Company: { Name: 'Acme', Website: site } });
+  });
+});
