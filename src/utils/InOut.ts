f79368d824fd6384166275c66dae5ef7d5882253
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-23 11:22:49 +0200

    fix(utils): added isInOut.

--- src/utils/InOut.ts
+++ src/utils/InOut.ts
@@ -1,7 +1,9 @@
-import { Json, JsonValue } from '../types';
+import { isA, Json, JsonValue } from '../types';
 
 type InOut = {
   name: string,
   in: (key: string, source?: Json) => JsonValue | undefined,
   out: (key: string, source?: Json) => JsonValue | undefined,
 }
+
+const isInOut = (io?: unknown): io is InOut => isA<InOut>(io, 'in', 'out');
