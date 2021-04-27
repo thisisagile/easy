98cf7fa68eec1b9d817bf00ada05d5d1be5382bf
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-27 14:57:59 +0200

    feat(utils): added State.ts

--- src/utils/State.ts
+++ src/utils/State.ts
@@ -3,6 +3,6 @@ import { Construct, ofConstruct } from '../types';
 export class State {
   constructor(protected readonly state: any = {}) {}
 
-  protected get = <T>(key: string, alt: Construct<T>): T => this.state[key] ?? (this.state[key] = ofConstruct(alt));
-  protected set = <T>(key: string, value: Construct<T>): T => (this.state[key] = ofConstruct(value));
+  protected get = <T>(key: string, alt?: Construct<T>): T => this.state[key] ?? (this.state[key] = ofConstruct(alt));
+  protected set = <T>(key: string, value: Construct<T>): T => this.get(key, value);
 }
---
+++ test/utils/State.test.ts
@@ -0,0 +1,49 @@
+import { State } from '../../src';
+import { Dev } from '../ref';
+import { mock } from '@thisisagile/easy-test';
+
+describe('State', () => {
+
+  class MyState extends State {
+    value = () => Dev.Jeroen;
+    dev = () => this.get('dev', this.value);
+    devFunction = () => this.get('dev', () => this.value());
+    setDev2 = (d: Dev): Dev => this.set('dev2', d);
+    getDev2 = (): Dev | undefined => this.get('dev2');
+  }
+
+  let state: MyState;
+
+  beforeEach(() => {
+    state = new MyState();
+  })
+
+  test('get works', () => {
+    const res = state.dev();
+    expect(res).toBe(Dev.Jeroen);
+  });
+
+  test('get works repeatedly without calling again', () => {
+    state.value = mock.return(Dev.Wouter);
+    const res = state.dev();
+    expect(res).toBe(Dev.Wouter);
+    state.dev();
+    state.dev();
+    expect(state.value).toHaveBeenCalledTimes(1);
+  });
+
+  test('get works repeatedly with function without calling again', () => {
+    state.value = mock.return(Dev.Naoufal);
+    const res = state.devFunction();
+    expect(res).toBe(Dev.Naoufal);
+    state.devFunction();
+    state.devFunction();
+    expect(state.value).toHaveBeenCalledTimes(1);
+  });
+
+  test('set works', () => {
+    const res = state.setDev2(Dev.Sander);
+    expect(res).toBe(Dev.Sander);
+    expect(state.getDev2()).toBe(Dev.Sander);
+  })
+});
