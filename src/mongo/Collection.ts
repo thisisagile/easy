fa1ae2a61b9f5a3139dc4f111ad4c1edfd24ac9f
aahoogendoorn <aahoogendoorn@gmail.com>
2021-04-27 13:31:07 +0200

    fix(mongo): Collection.ts now inherits Mapper, instead of Map.

--- src/mongo/Collection.ts
+++ src/mongo/Collection.ts
@@ -1,15 +1,20 @@
-import { Map, MapOptions, PropertyOptions } from '../utils';
+import { MapOptions, Mapper, maps, PropertyOptions } from '../utils';
 import { Database } from '../data';
 import { Json, toUuid } from '../types';
 import { Field } from './Field';
 import { Condition, LogicalCondition, toCondition } from './Condition';
 
-export class Collection extends Map<Field> {
+export class Collection extends Mapper {
+  protected readonly map = {
+    ...maps,
+    field: <T = unknown>(name: string, options?: PropertyOptions<T>): Field => new Field(this, name, options),
+  };
+
   constructor(readonly db: Database = Database.Default, options: MapOptions = { startFrom: 'source' }) {
     super(options);
   }
 
-  prop = <T = unknown>(name: string, options?: PropertyOptions<T>): Field => new Field(this, name, options);
+  prop = <T = unknown>(name: string, options?: PropertyOptions<T>): Field => this.map.field(name, options);
 
   readonly id = this.prop('id', { dflt: toUuid });
 
