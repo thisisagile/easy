75b85065a6fe5ee1f870da99e504afeb9be22b92
woutervanbakel <wouter.vb@gmail.com>
2021-05-31 08:49:03 +0200

    fix(utils): Add ifNotEmpty to If

--- packages/easy/src/utils/If.ts
+++ packages/easy/src/utils/If.ts
@@ -1,3 +1,4 @@
-import { Construct, isDefined, ofConstruct } from '../types';
+import { Construct, isDefined, isNotEmpty, ofConstruct } from '../types';
 
 export const ifDefined = <T>(o: unknown, f: Construct<T>, alt?: Construct<T>): T | undefined => isDefined(o) ? ofConstruct(f, o) : ofConstruct(alt, o);
+export const ifNotEmpty = <T>(o: unknown, f: Construct<T>, alt?: Construct<T>): T | undefined => isNotEmpty(o) ? ofConstruct(f, o) : ofConstruct(alt, o);
--- packages/easy/test/utils/If.test.ts
+++ packages/easy/test/utils/If.test.ts
@@ -1,27 +1,62 @@
 import { mock } from '@thisisagile/easy-test';
-import { ifDefined } from '../../src';
+import { ifDefined, ifNotEmpty } from '../../src';
 
-describe('If', () => {
+describe('IfDefined', () => {
+  let f: jest.Mock;
+  let alt: jest.Mock;
+
+  beforeEach(() => {
+    f  = mock.return('f');
+    alt = mock.return('alt');
+  });
 
   test('call f when defined.', () => {
-    const f  = mock.return('f');
-    const alt = mock.return('alt');
     expect(ifDefined('defined', () => f(), () => alt())).toBe('f');
     expect(f).toBeCalled();
     expect(alt).not.toBeCalled();
   });
 
   test('call alt when not defined.', () => {
-    const f = mock.return('f');
-    const alt = mock.return('alt');
     expect(ifDefined(undefined, () => f(), () => alt())).toBe('alt');
     expect(f).not.toBeCalled();
     expect(alt).toBeCalled();
   });
 
   test('return undefined when alt is not given.', () => {
-    const f = mock.return('f');
     expect(ifDefined(undefined, () => f())).toBeUndefined();
+    expect(f).not.toBeCalled();
+  });
+});
+
+describe('IfNotEmpty', () => {
+  let f: jest.Mock;
+  let alt: jest.Mock;
+
+  beforeEach(() => {
+    f  = mock.return('f');
+    alt = mock.return('alt');
+  });
+
+  test('call f when not empty.', () => {
+    expect(ifNotEmpty([{}, {}], () => f(), () => alt())).toBe('f');
+    expect(f).toBeCalled();
+    expect(alt).not.toBeCalled();
+  });
+
+  test('call alt on empty array.', () => {
+    expect(ifNotEmpty([], () => f(), () => alt())).toBe('alt');
+    expect(f).not.toBeCalled();
+    expect(alt).toBeCalled();
+  });
+
+  test('call alt on empty string.', () => {
+    expect(ifNotEmpty('', () => f(), () => alt())).toBe('alt');
+    expect(f).not.toBeCalled();
+    expect(alt).toBeCalled();
+  });
+
+  test('return undefined when alt is not given.', () => {
+    expect(ifNotEmpty([], () => f())).toBeUndefined();
     expect(f).not.toBeCalled();
   });
 });
