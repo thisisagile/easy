d72c14f36977be01a641c184cf9ab108caf73aab
aahoogendoorn <aahoogendoorn@gmail.com>
2021-06-08 09:23:47 +0200

    fix(http): removed code smell.

--- packages/easy/src/http/Verb.ts
+++ packages/easy/src/http/Verb.ts
@@ -11,20 +11,20 @@ export const toVerbOptions = (options?: VerbOptions): Required<VerbOptions> => (
   type: options?.type ?? ContentType.Json,
 });
 
-const verb = <T>(verb: HttpVerb, options?: VerbOptions): PropertyDecorator => (subject: unknown, property: string | symbol): void => {
+const toVerb = <T>(verb: HttpVerb, options?: VerbOptions): PropertyDecorator => (subject: unknown, property: string | symbol): void => {
   meta(subject).property(property).set('verb', { verb, options });
 };
 
-export const get = (options?: VerbOptions): PropertyDecorator => verb(HttpVerb.Get, options);
+export const get = (options?: VerbOptions): PropertyDecorator => toVerb(HttpVerb.Get, options);
 
-export const search = (options?: VerbOptions): PropertyDecorator => verb(HttpVerb.Get, { onNotFound: HttpStatus.Ok, ...options });
+export const search = (options?: VerbOptions): PropertyDecorator => toVerb(HttpVerb.Get, { onNotFound: HttpStatus.Ok, ...options });
 
-export const put = (options?: VerbOptions): PropertyDecorator => verb(HttpVerb.Put, options);
+export const put = (options?: VerbOptions): PropertyDecorator => toVerb(HttpVerb.Put, options);
 
-export const patch = (options?: VerbOptions): PropertyDecorator => verb(HttpVerb.Patch, options);
+export const patch = (options?: VerbOptions): PropertyDecorator => toVerb(HttpVerb.Patch, options);
 
-export const post = (options?: VerbOptions): PropertyDecorator => verb(HttpVerb.Post, { onOk: HttpStatus.Created, ...options });
+export const post = (options?: VerbOptions): PropertyDecorator => toVerb(HttpVerb.Post, { onOk: HttpStatus.Created, ...options });
 
-export const del = (options?: VerbOptions): PropertyDecorator => verb(HttpVerb.Delete, { onOk: HttpStatus.NoContent, ...options });
+export const del = (options?: VerbOptions): PropertyDecorator => toVerb(HttpVerb.Delete, { onOk: HttpStatus.NoContent, ...options });
 
-export const stream = (options?: VerbOptions): PropertyDecorator => verb(HttpVerb.Get, { type: ContentType.Stream, ...options });
+export const stream = (options?: VerbOptions): PropertyDecorator => toVerb(HttpVerb.Get, { type: ContentType.Stream, ...options });
