2d2c09b08f70bb7af0c141171e0ef33b80bc4176
aahoogendoorn <aahoogendoorn@gmail.com>
2021-06-08 14:18:19 +0200

    text(types): added more tests for ofMessage().

--- packages/easy/test/types/Message.test.ts
+++ packages/easy/test/types/Message.test.ts
@@ -1,8 +1,15 @@
 import '@thisisagile/easy-test';
-import { ofMessage } from '../../dist';
+import { Message, ofMessage } from '../../src';
+import { Dev } from '../ref';
 
 describe('ofMessage', () => {
   test('from string', () => {
     expect(ofMessage('Sander')).toBe('Sander');
   });
+
+  test('from function', () => {
+    const greeting = (dev: Dev): Message<Dev> => () => dev.name;
+    expect(ofMessage(greeting(Dev.Sander))).toBe('Sander');
+  });
+
 });
