199731863e40d7a1e9fc35f60e327f43be2ac2fe
aahoogendoorn <aahoogendoorn@gmail.com>
2021-06-22 14:06:14 +0200

    fix(process): update to Scope signature.

--- packages/easy/src/process/Scope.ts
+++ packages/easy/src/process/Scope.ts
@@ -1,11 +1,11 @@
-import { Enum, Id } from '../types';
+import { Enum, Text, text } from '../types';
 
 export class Scope extends Enum {
-  protected constructor(readonly name: string, readonly id: Id) {
-    super(name, id);
-  }
-
-  static readonly Basic = new Scope('Basic', 'basic');
+  static readonly Basic = new Scope('Basic');
   static readonly Auth = new Scope('Authorization', 'auth');
   static readonly Admin = new Scope('Administration', 'admin');
+
+  protected constructor(readonly name: string, id: Text = text(name).kebab) {
+    super(name, id.toString());
+  }
 }
