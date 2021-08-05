00aba15251ce31e6022e34dc14661556b9c72b6b
robgeurden <35384293+robgeurden@users.noreply.github.com>
2021-08-05 17:02:53 +0200

    fix: used the exported match instead of creating a new one

--- packages/easy-test-web/src/matchers/toBeAt.ts
+++ packages/easy-test-web/src/matchers/toBeAt.ts
@@ -1,12 +1,12 @@
 import CustomMatcherResult = jest.CustomMatcherResult;
-import { Match } from '@thisisagile/easy-test';
+import { match } from '@thisisagile/easy-test';
 import { Id, UseCase } from '@thisisagile/easy';
 import { toUrl, Tester } from '../Tester';
 
 const ucUrl = (t: Tester, uc?: UseCase, id? : Id): string => toUrl(uc as UseCase, t.domain, t.port, id)
 
 export const toBeAt = (tester?: Tester, uc?: UseCase, id?: Id): CustomMatcherResult =>{
-  return new Match<Tester>(tester as Tester)
+  return match<Tester>(tester as Tester)
     .undefined(t => t, 'Tester is undefined')
     .undefined(() => uc, 'uc is undefined')
     .not(
@@ -29,3 +29,4 @@ declare global {
     }
   }
 }
+
