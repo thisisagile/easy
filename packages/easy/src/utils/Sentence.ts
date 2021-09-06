d6cb007ac9d72dcaa685f3a80fc12a7359a3e3a3
aahoogendoorn <aahoogendoorn@gmail.com>
2021-09-06 14:44:40 +0200

    test(utils): added more tests on Sentence.ts

--- packages/easy/src/utils/Sentence.ts
+++ packages/easy/src/utils/Sentence.ts
@@ -13,7 +13,7 @@ class Topic extends Sentence {
   it = new Sentence('it', this);
   anything = new Sentence('anything', this);
   id = (id: Id) => new Sentence(`id '${id}'`, this);
-  your = (subject?: unknown) => new Sentence(`your '${subject ?? 'item'}'`, this);
+  your = (subject?: unknown) => new Sentence(`your ${subject ?? 'item'}`, this);
   a = (subject?: unknown) => new Sentence(subject ? `a ${subject}` : 'an item', this);
   an = (subject?: unknown) => new Sentence(`an ${subject ?? 'item'}`, this);
   any = (subjects?: unknown) => new Sentence(`any ${subjects ?? 'items'}`, this);
--- packages/easy/test/utils/Sentence.test.ts
+++ packages/easy/test/utils/Sentence.test.ts
@@ -7,7 +7,13 @@ describe('We', () => {
   test('check it', () => {
     expect(we.could.not.update.id(42)).toMatchText('We could not update id \'42\'.');
     expect(we.did.not.find.any('devs')).toMatchText('We did not find any devs.');
+    expect(we.did.add.your('dev')).toMatchText('We did add your dev.');
+    expect(we.did.fetch.your()).toMatchText('We did fetch your item.');
     expect(we.did.add.a('dev')).toMatchText('We did add a dev.');
+    expect(we.did.fetch.a()).toMatchText('We did fetch an item.');
     expect(we.did.remove.an('item')).toMatchText('We did remove an item.');
+    expect(we.did.remove.an()).toMatchText('We did remove an item.');
+    expect(we.did.not.find.any('items')).toMatchText('We did not find any items.');
+    expect(we.did.not.find.any()).toMatchText('We did not find any items.');
   });
 });
