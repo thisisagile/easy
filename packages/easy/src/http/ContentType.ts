7c7105f7ca686ee5adf00f75d563d7456f575e76
woutervanbakel <wouter.vb@gmail.com>
2021-09-06 14:01:45 +0200

    fix(easy): return Json when encoding for ContentType Json, instead of string
    BREAKING CHANGE: return Json when encoding for ContentType Json, instead of string

--- packages/easy/src/http/ContentType.ts
+++ packages/easy/src/http/ContentType.ts
@@ -3,7 +3,7 @@ import formUrlEncoded from 'form-urlencoded';
 
 export class ContentType extends Enum {
   static Form = new ContentType('form', 'application/x-www-form-urlencoded', b => formUrlEncoded(b));
-  static Json = new ContentType('json', 'application/json', b => JSON.stringify(b));
+  static Json = new ContentType('json', 'application/json', b => b);
   static Stream = new ContentType('stream', 'application/octet-stream');
   static Text = new ContentType('text', 'text/plain');
   static Xml = new ContentType('xml', 'application/xml');
--- packages/easy/test/http/ContentType.test.ts
+++ packages/easy/test/http/ContentType.test.ts
@@ -1,12 +1,13 @@
 import { ContentType, json } from '../../src';
 import { Dev } from '../ref';
+import '@thisisagile/easy-test';
 
 describe('ContentType', () => {
   const j = json.omit(Dev.Wouter, 'created', 'lastModified');
 
   test('Json encode.', () => {
-    const expected = '{"id":4,"name":"Wouter","language":"TypeScript","level":3}';
-    expect(ContentType.Json.encode(j)).toBe(expected);
+    const expected = {"id":4,"name":"Wouter","language":"TypeScript","level":3};
+    expect(ContentType.Json.encode(j)).toMatchJson(expected);
   });
 
   test('Check encoding of form.', () => {
