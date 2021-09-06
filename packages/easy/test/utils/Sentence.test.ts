4a3c577443ad5dea709b3d23619da0b0fa82922b
aahoogendoorn <aahoogendoorn@gmail.com>
2021-09-06 15:21:18 +0200

    test(utils): added more tests on Sentence.ts

--- packages/easy/test/utils/Sentence.test.ts
+++ packages/easy/test/utils/Sentence.test.ts
@@ -1,9 +1,7 @@
 import '@thisisagile/easy-test';
 import { we } from '../../src';
 
-
 describe('We', () => {
-
   test('check it', () => {
     expect(we.could.not.update.id(42)).toMatchText('We could not update id \'42\'.');
     expect(we.did.not.find.any('devs')).toMatchText('We did not find any devs.');
