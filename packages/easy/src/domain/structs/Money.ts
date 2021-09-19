fc5226cb00053ba1a9a8652c6ecd984aa88c8f30
aahoogendoorn <aahoogendoorn@gmail.com>
2021-09-20 00:04:52 +0200

    feat(domain): added add() to Money.ts

--- packages/easy/src/domain/structs/Money.ts
+++ packages/easy/src/domain/structs/Money.ts
@@ -4,5 +4,10 @@ import { Currency } from '../enums';
 
 export class Money extends Struct {
   @required() readonly currency: Currency = Currency.byId(this.state.currency, Currency.EUR);
-  @required() readonly amount = this.state.amount ?? 0;
+  @required() readonly amount: number = this.state.amount ?? 0;
+
+  add = (amount: number): Money => money(this.currency, this.amount + amount);
 }
+
+export const money = (currency: Currency, amount: number): Money => new Money({ currency, amount });
+
--- packages/easy/test/domain/structs/Money.test.ts
+++ packages/easy/test/domain/structs/Money.test.ts
@@ -3,6 +3,8 @@ import { Currency, Money } from '../../../src';
 
 describe('Money', () => {
 
+  const m = new Money({currency: Currency.EUR.id, amount: 42 });
+
   test('default', () => {
     const m = new Money();
     expect(m.currency).toBe(Currency.EUR);
@@ -13,5 +15,11 @@ describe('Money', () => {
     const m = new Money({currency: Currency.USD.id, amount: 42 });
     expect(m.currency).toBe(Currency.USD);
     expect(m.amount).toBe(42);
+  });
+
+  test('add', () => {
+    const m2 = m.add(42);
+    expect(m2.currency).toBe(m.currency);
+    expect(m2.amount).toBe(84);
   });
 });
