feb443b8a8739c6efae7c1489c77e2005d2be80d
robgeurden <35384293+robgeurden@users.noreply.github.com>
2021-10-15 20:58:07 +0200

    feat(tag): add new tag property descriptors, containing searchable property tag

---
+++ packages/easy/src/types/Tag.ts
@@ -0,0 +1,7 @@
+import { meta } from './Meta';
+
+export const tag = (name: string): PropertyDecorator => (subject: unknown, property: string | symbol): void => {
+  meta(subject).property(property).set(name, property);
+};
+
+export const searchable = (): PropertyDecorator => tag(searchable.name);
---
+++ packages/easy/test/types/Tag.test.ts
@@ -0,0 +1,25 @@
+import { meta, required, searchable, Struct } from '../../src';
+import { fits } from '@thisisagile/easy-test';
+
+describe('Tag', () => {
+  class Tester extends Struct {
+    @searchable() readonly first = this.state.first;
+    @searchable() @required() readonly second = this.state.second;
+    @searchable() readonly third = this.state.third;
+    readonly fourth = this.state.fourth;
+  }
+
+  let tester: Tester;
+  beforeEach(() => {
+    tester = new Tester({ first: 'Rob', second: '' });
+  });
+
+  test('tags work', () => {
+    const props = meta(tester).properties(searchable.name);
+    expect(props).toHaveLength(3);
+    expect(props).toContainEqual(fits.with({property: 'first'}));
+    expect(props).toContainEqual(fits.with({property: 'second'}));
+    expect(props).toContainEqual(fits.with({property: 'third'}));
+  })
+});
+
