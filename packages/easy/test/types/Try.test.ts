f1084594e08d7acfd0ccee70e9a14aa009e455b7
aahoogendoorn <aahoogendoorn@gmail.com>
2021-10-17 15:37:57 +0200

    feat(types): adding map() to Try.ts.

--- packages/easy/test/types/Try.test.ts
+++ packages/easy/test/types/Try.test.ts
@@ -1,27 +1,39 @@
 import '@thisisagile/easy-test';
 import { Construct, ofConstruct } from '../../src';
 import { Dev } from '../ref';
+import { asString } from '@thisisagile/easy-test/dist/utils/Utils';
 
 
 abstract class Try<T = unknown> {
-  protected constructor(protected readonly val?: T) {
-  }
 
-  get value(): T | undefined {
-    return this.val;
-  }
+  abstract get value(): T;
+  abstract get error(): Error;
 
-  static of = <T>(c: Construct<T | Try<T>>): Try<T> => {
+  static of = <T>(c: Construct<T | Try<T>>, ...args: unknown[]): Try<T> => {
     try {
-      const out = ofConstruct(c);
+      const out = ofConstruct(c, ...args);
       return new Success(out instanceof Try ? out.value : out);
     } catch (e) {
       return new Failure(e as Error);
     }
   };
+
+  abstract map<U>(f: (value: T) => U | Try<U>): Try<U>;
 }
 
 class Success<T> extends Try<T> {
+
+  constructor(readonly value: T) {
+    super();
+  }
+
+  get error(): Error {
+    throw new Error("No error found");
+  }
+
+  map<U>(f: (value: T) => U | Try<U>): Try<U> {
+    return toTry<U>(() => f(this.value));
+  };
 }
 
 class Failure<E = Error> extends Try<E> {
@@ -29,12 +41,16 @@ class Failure<E = Error> extends Try<E> {
     super();
   }
 
-  get value(): E | undefined {
+  get value(): E  {
     throw this.error;
   }
+
+  map<U>(f: (value: E) => U | Try<U>): Try<U> {
+    return new Failure<U>(this.error);
+  };
 }
 
-const toTry = <T>(c: Construct<T>) => Try.of<T>(c);
+const toTry = <T>(c: Construct<T | Try<T>>, ...args: unknown[]) => Try.of<T>(c, ...args);
 
 describe('Try', () => {
 
@@ -44,11 +60,16 @@ describe('Try', () => {
     }
   }
 
+  const devToId = (d: Dev): string => asString(d.id);
+
   const successes = [Dev, Dev.Sander, () => Dev.Jeroen, toTry(Dev), toTry(Dev.Rob), toTry(() => Dev.Jeroen)];
 
+  const devToError = (d: Dev) => {
+    throw new Error(`Dev ${d} goes wrong`);
+  };
   const divByZero = (n = 0) => {
     throw new Error(`Divide ${n} by zero`);
-  }
+  };
   const errors = [ConstructError, toTry(ConstructError), divByZero, toTry(divByZero), toTry(() => divByZero(3))];
 
   test.each(successes)('of success', (s) => {
@@ -65,5 +86,21 @@ describe('Try', () => {
 
   test.each(errors)('value error', (s) => {
     expect(() => toTry(s).value).toThrow();
+  });
+
+  test.each(successes)('map success to success', (s) => {
+    expect(toTry(s).map(d => devToId(d))).toBeInstanceOf(Success);
+  });
+
+  test.each(successes)('map success to success value is a string', (s) => {
+    expect(typeof toTry(s).map(d => devToId(d)).value).toBe('string');
+  });
+
+  test.each(successes)('map success to failure should fail', (s) => {
+    expect(toTry(s).map(d => devToError(d))).toBeInstanceOf(Failure);
+  });
+
+  test.each(errors)('map error', (s) => {
+    expect(toTry(s).map(e => e)).toBeInstanceOf(Failure);
   });
 });
