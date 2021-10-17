a7fcc2ffde6277c9954d80b1ee3561218a41ef11
aahoogendoorn <aahoogendoorn@gmail.com>
2021-10-17 14:44:59 +0200

    feat(types): adding Success and Failure

--- packages/easy/test/types/Try.test.ts
+++ packages/easy/test/types/Try.test.ts
@@ -1,38 +1,56 @@
 import '@thisisagile/easy-test';
-import { Construct, ofConstruct } from '../../src';
+import { Get, ofGet } from '../../src';
 import { Dev } from '../ref';
 
 
 class Try<T = unknown> {
-  private constructor(public value?: T) {
+  protected constructor(protected readonly val?: T) {
   }
 
-  static of = <T>(c: Construct<T>): Try<T> => {
+  get value(): T | undefined {
+    return this.val;
+  }
+
+  static of = <T>(c: Get<T>): Try<T> => {
     try {
-      return new Try(ofConstruct(c));
+      return new Success(ofGet(c));
+    } catch (e) {
+      return new Failure(e as Error);
     }
-    catch {
-      return new Try();
-    }
+  };
+}
+
+class Success<T> extends Try<T> {
+}
+
+class Failure<E = Error> extends Try<E> {
+  constructor(readonly error: Error) {
+    super();
   }
 }
 
-const toTry = <T>(c: Construct<T>) => Try.of<T>(c);
+const toTry = <T>(c: Get<T>) => Try.of<T>(c);
 
 describe('Try', () => {
 
-  const successes = [Dev, Dev.Sander, () => Dev.Jeroen];
-  const errors = [Error, new Error('Sorry'), () => new Error('Sorry again'), (n = 0) => {throw new Error(`Divide ${n} by zero`);}];
+  const successes = [Dev.Sander, () => Dev.Jeroen];
+  const errors = [(n = 0) => {
+    throw new Error(`Divide ${n} by zero`);
+  }];
 
-  test.each(successes)('of', (s) => {
-    expect(toTry(s)).toBeInstanceOf(Try);
+  test.each(successes)('of success', (s) => {
+    expect(toTry(s)).toBeInstanceOf(Success);
   });
 
-  test.each(errors)('of', (s) => {
-    expect(toTry(s)).toBeInstanceOf(Try);
+  test.each(errors)('of error', (s) => {
+    expect(toTry(s)).toBeInstanceOf(Failure);
   });
 
-  test.each(successes)('of', (s) => {
+  test.each(successes)('value success', (s) => {
     expect(toTry(s).value).toBeInstanceOf(Dev);
+  });
+
+  test.each(errors)('value error', (s) => {
+    expect(toTry(s).value).toBeUndefined();
   });
 });
