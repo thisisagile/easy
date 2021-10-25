0d6f026b4599fabf06c53d26c7b7d98ce7f2c403
Jeroen Plug <jeroen.plug@ibood.com>
2021-10-25 15:29:20 +0200

    test(easy-express): Add tests to SecurityHandler

--- packages/easy-express/test/express/SecurityHandler.test.ts
+++ packages/easy-express/test/express/SecurityHandler.test.ts
@@ -1,14 +1,21 @@
 import passport from 'passport';
-import { authError, checkScope, checkUseCase, security } from '../../src';
+import { authError, checkScope, checkToken, checkUseCase, security } from '../../src';
 import { Request, Response } from 'express';
-import { HttpStatus, Scope, UseCase } from '@thisisagile/easy';
+import { ctx, HttpStatus, Scope, UseCase } from '@thisisagile/easy';
+import { mock } from '@thisisagile/easy-test';
 
-describe('Checks', () => {
+describe('SecurityHandler decorators', () => {
   const cb = jest.fn();
 
   beforeEach(() => {
     cb.mockReset();
   });
+
+  test('checkToken', () => {
+    const authenticateSpy = jest.spyOn(passport, 'authenticate');
+    checkToken();
+    expect(authenticateSpy).toHaveBeenCalledWith('jwt', { session: false, failWithError: true });
+  })
 
   test('checkScope', () => {
     const c = checkScope(Scope.Basic);
@@ -35,13 +42,16 @@ describe('Checks', () => {
   });
 });
 
-describe('SecurityHandler', () => {
+describe('SecurityHandler middleware', () => {
+  const key = "secretKey";
+  let useSpy: jest.SpyInstance;
+
   beforeEach(() => {
     process.env.TOKEN_PUBLIC_KEY = 'token';
+    useSpy = jest.spyOn(passport, 'use');
   });
 
   test('security middleware with default settings', () => {
-    const useSpy = jest.spyOn(passport, 'use');
     const initializeSpy = jest.spyOn(passport, 'initialize');
 
     useSpy.mockImplementationOnce(((s: any) => {
@@ -62,9 +72,7 @@ describe('SecurityHandler', () => {
   });
 
   test('security middleware with custom options', () => {
-    const useSpy = jest.spyOn(passport, 'use');
-
-    const options = {
+    const jwtStrategyOptions = {
       audience: 'audience',
       issuer: 'issuer',
       algorithms: undefined,
@@ -72,31 +80,82 @@ describe('SecurityHandler', () => {
 
     useSpy.mockImplementationOnce(((s: any) => {
       expect(s._verifOpts).toStrictEqual({
-        ...options,
+        ...jwtStrategyOptions,
         ignoreExpiration: false,
       });
     }) as any);
 
-    security({ jwtStrategyOptions: options });
+    security({ jwtStrategyOptions });
 
     expect(useSpy).toHaveBeenCalled();
   });
 
   test('security middleware with secretOrKey', () => {
-    const useSpy = jest.spyOn(passport, 'use');
-
-    const options = {
-      secretOrKey: 'mykey',
-    };
+    const jwtStrategyOptions = { secretOrKey: key };
 
     useSpy.mockImplementationOnce(((s: any) => {
       s._secretOrKeyProvider(null, null, (err: any, secretOrKey?: string | Buffer) => {
-        expect(secretOrKey).toBe(options.secretOrKey);
-      })
+        expect(secretOrKey).toBe(key);
+      });
     }) as any);
 
-    security({ jwtStrategyOptions: options });
+    security({ jwtStrategyOptions });
 
+    expect(useSpy).toHaveBeenCalled();
+  });
+
+  test('security middleware with secretOrKeyProvider', () => {
+    const keyProvider = mock.resolve(key);
+    const jwtStrategyOptions = { secretOrKeyProvider: keyProvider };
+
+    useSpy.mockImplementationOnce(((s: any) => {
+      s._secretOrKeyProvider(null, null, (err: any, secretOrKey?: string | Buffer) => {
+        expect(secretOrKey).toBe(key);
+      });
+    }) as any);
+
+    security({ jwtStrategyOptions });
+
+    expect(useSpy).toHaveBeenCalled();
+  });
+
+  test('security middleware with failing secretOrKeyProvider', () => {
+    const keyProvider = mock.reject("someError");
+    const jwtStrategyOptions = { secretOrKeyProvider: keyProvider };
+
+    useSpy.mockImplementationOnce(((s: any) => {
+      s._secretOrKeyProvider(null, null, (err: any, secretOrKey?: string | Buffer) => {
+        expect(err).toBe("someError");
+      });
+    }) as any);
+
+    security({ jwtStrategyOptions });
+
+    expect(useSpy).toHaveBeenCalled();
+  });
+
+  test('JWT strategy sets context', () => {
+    const payload = { key: "value" };
+    const done = jest.fn();
+
+    useSpy.mockImplementationOnce(((s: any) => {
+      s._verify({ headers: { authorization: `Bearer ${key}`} }, payload, done);
+    }) as any);
+
+    security();
+    expect(ctx.request.token).toBe(payload);
+    expect(ctx.request.jwt).toBe(key);
+    expect(done).toHaveBeenCalledWith(null, payload);
+    expect(useSpy).toHaveBeenCalled();
+  });
+
+  test('JWT strategy defaults jwt to empty string', () => {
+    useSpy.mockImplementationOnce(((s: any) => {
+      s._verify({ headers: {} }, {}, jest.fn());
+    }) as any);
+
+    security();
+    expect(ctx.request.jwt).toBe("");
     expect(useSpy).toHaveBeenCalled();
   });
 });
