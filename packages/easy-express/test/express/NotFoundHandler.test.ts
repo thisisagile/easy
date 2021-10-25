5d25287de6fcb37cb73fdc04a26558e90e4e94c5
Jeroen Plug <jeroen.plug@ibood.com>
2021-10-25 15:49:03 +0200

    test(express): Test NotFoundHandler

---
+++ packages/easy-express/test/express/NotFoundHandler.test.ts
@@ -0,0 +1,17 @@
+import { mock } from '@thisisagile/easy-test';
+import { NextFunction, Request, Response } from 'express';
+import { Exception, toOriginatedError } from '@thisisagile/easy';
+import { notFound } from '../../src';
+
+describe('NotFoundHandler', () => {
+  let next: NextFunction;
+
+  beforeEach(() => {
+    next = mock.return();
+  });
+
+  test('Call next with DoesNotExist error', () => {
+    notFound({} as Request, {} as Response, next);
+    expect(next).toHaveBeenCalledWith(toOriginatedError(Exception.DoesNotExist))
+  });
+});
