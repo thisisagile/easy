b80b69b99a8ca0d7e40d5b3b671b42c5c8a8b4b2
aahoogendoorn <aahoogendoorn@gmail.com>
2021-11-12 15:45:20 +0100

    fix(types): clean-up TableGateway.ts.

--- packages/easy/src/types/Gateway.ts
+++ packages/easy/src/types/Gateway.ts
@@ -2,37 +2,38 @@ import { List } from './List';
 import { Json, JsonValue } from './Json';
 import { Id } from './Id';
 import { Exception } from './Exception';
+import { reject } from '../utils';
 
 export abstract class Gateway {
   all(): Promise<List<Json>> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   }
 
   byId(id: Id): Promise<Json | undefined> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   };
 
   by(key: string, value: JsonValue): Promise<List<Json>> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   };
 
   search(q: JsonValue): Promise<List<Json>> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   };
 
   exists(id: Id): Promise<boolean> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   };
 
   add(item: Json): Promise<Json> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   };
 
   update(item: Json): Promise<Json> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   };
 
   remove(id: Id): Promise<boolean> {
-    throw Exception.IsNotImplemented;
+    return reject(Exception.IsNotImplemented);
   };
 }
