4d8c405aefb82815dc2cf686d8c07af6e1ca24b6
aahoogendoorn <aahoogendoorn@gmail.com>
2021-11-12 14:18:02 +0100

    fix(types): Gateway.ts is now an abstract class, so when you want to create a gateway and use it in a default repo, you don't have to implement all methods, just the ones you need. The others just throw Exception.NotImplemented.

--- packages/easy/src/types/Gateway.ts
+++ packages/easy/src/types/Gateway.ts
@@ -1,15 +1,50 @@
 import { List } from './List';
 import { Json, JsonValue } from './Json';
 import { Id } from './Id';
+import { Exception } from './Exception';
 
-export interface Gateway {
-  all: () => Promise<List<Json>>;
-  byId: (id: Id) => Promise<Json | undefined>;
-  by: (key: string, value: JsonValue) => Promise<List<Json>>;
-  search: (q: JsonValue) => Promise<List<Json>>;
-  exists: (id: Id) => Promise<boolean>;
+// export interface Gateway {
+//   all: () => Promise<List<Json>>;
+//   byId: (id: Id) => Promise<Json | undefined>;
+//   by: (key: string, value: JsonValue) => Promise<List<Json>>;
+//   search: (q: JsonValue) => Promise<List<Json>>;
+//   exists: (id: Id) => Promise<boolean>;
+//
+//   add: (item: Json) => Promise<Json>;
+//   update: (item: Json) => Promise<Json>;
+//   remove: (id: Id) => Promise<boolean>;
+// }
 
-  add: (item: Json) => Promise<Json>;
-  update: (item: Json) => Promise<Json>;
-  remove: (id: Id) => Promise<boolean>;
+export abstract class Gateway {
+  all(): Promise<List<Json>> {
+    throw Exception.IsNotImplemented;
+  }
+
+  byId(id: Id): Promise<Json | undefined> {
+    throw Exception.IsNotImplemented;
+  };
+
+  by(key: string, value: JsonValue): Promise<List<Json>> {
+    throw Exception.IsNotImplemented;
+  };
+
+  search(q: JsonValue): Promise<List<Json>> {
+    throw Exception.IsNotImplemented;
+  };
+
+  exists(id: Id): Promise<boolean> {
+    throw Exception.IsNotImplemented;
+  };
+
+  add(item: Json): Promise<Json> {
+    throw Exception.IsNotImplemented;
+  };
+
+  update(item: Json): Promise<Json> {
+    throw Exception.IsNotImplemented;
+  };
+
+  remove(id: Id): Promise<boolean> {
+    throw Exception.IsNotImplemented;
+  };
 }
