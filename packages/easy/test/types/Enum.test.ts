78ff3a73c9fefa1904085fa5336fe85d879a5d04
woutervanbakel <wouter.vb@gmail.com>
2021-11-18 16:37:38 +0100

    test(easy): extend unit test from Enum

--- packages/easy/test/types/Enum.test.ts
+++ packages/easy/test/types/Enum.test.ts
@@ -23,15 +23,12 @@ describe('Enum', () => {
   test('byIds', () => {
     const ids = ['javascript', 'typescript', 'java'];
     const idsWithWrongOnes = ['javascript', 'typescript', 'java', 'php', 'english'];
+    const sameIds =  ['typescript', 'typescript', 'typescript'];
     expect(Language.byIds()).toHaveLength(0);
     expect(Language.byIds(ids)).toHaveLength(3);
     expect(Language.byIds(idsWithWrongOnes)).toHaveLength(3);
+    expect(Language.byIds(sameIds)).toHaveLength(1);
     expect(Language.byIds(ids)[0]).toBeInstanceOf(Language);
-  });
-
-  test('byIds same ids', () => {
-    const ids = ['typescript', 'typescript', 'typescript'];
-    expect(Language.byIds(ids)).toHaveLength(3);
   });
 
   test('all', () => {
