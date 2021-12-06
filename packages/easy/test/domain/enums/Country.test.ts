b7956b462923ef966e641764d7aeee20bc08d624
aahoogendoorn <aahoogendoorn@gmail.com>
2021-12-06 10:33:54 +0100

    feat(domain): added Country.ts enum.

---
+++ packages/easy/test/domain/enums/Country.test.ts
@@ -0,0 +1,31 @@
+import '@thisisagile/easy-test';
+import { toList } from '@thisisagile/easy';
+import { Country } from '../../../src';
+
+describe('Country', () => {
+
+  test('id and name matches.', () => {
+    expect(Country.NL.id).toBe('NL');
+    expect(Country.NL.name).toMatchText('Netherlands');
+  });
+
+  test('short matches.', () => {
+    expect(Country.NL).toMatchText('NL');
+  });
+
+  test('byId is not case sensitive and accepts both dashes as well as underscores.', () => {
+    expect(Country.byId('nl')).toBe(Country.NL);
+    expect(Country.byId('NL')).toBe(Country.NL);
+  });
+
+  test('byIds.', () => {
+    expect(Country.byIds<Country>(['nl'])).toMatchText(toList<Country>(Country.NL));
+  });
+
+  test('equals.', () => {
+    expect(Country.NL.equals(Country.NL)).toBeTruthy();
+    // expect(Country.NL.equals('nl')).toBeTruthy();
+    expect(Country.NL.equals('NL')).toBeTruthy();
+    expect(Country.NL.equals('BE')).toBeFalsy();
+  });
+});
