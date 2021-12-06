99404249498dcd7d74d614827792314938f90f11
woutervanbakel <wouter.vb@gmail.com>
2021-12-06 15:32:46 +0100

    fix(types): isTrue also accepts a function that returns a boolean

--- packages/easy/src/types/Get.ts
+++ packages/easy/src/types/Get.ts
@@ -1,7 +1,7 @@
 import { Func, isFunc } from './Func';
 
 export type Get<T = any, Args = any> = T | Func<T, Args>;
-export type Predicate<Args> = Get<boolean, Args>;
+export type Predicate<Args = unknown> = Get<boolean, Args>;
 export const ofGet = <T, Args = any>(g: Get<T, Args>, ...args: Args[]): T => (isFunc<T, Args>(g) ? g(...args) : g);
 export const ifGet = <T>(pred: Get, valid: Get<T>, invalid: Get<T>): T => (ofGet(pred) ? ofGet(valid) : ofGet(invalid));
 export type GetProperty<T, Prop> = keyof T | Func<Prop, T>;
