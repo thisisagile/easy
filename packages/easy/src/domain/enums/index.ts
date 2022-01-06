2300b27f8614840da92286837454ef219ee102e1
woutervanbakel <wouter.vb@gmail.com>
2022-01-06 10:11:48 +0100

    fix(easy): Add Environment Enum

--- packages/easy/src/domain/enums/index.ts
+++ packages/easy/src/domain/enums/index.ts
@@ -1,3 +1,4 @@
 export * from './Country';
 export * from './Currency';
+export * from './Environment';
 export * from './Locale';
---
+++ packages/easy/test/domain/enums/Environment.test.ts
@@ -0,0 +1,9 @@
+import '@thisisagile/easy-test';
+import { Environment } from "@thisisagile/easy";
+
+describe('Environment', () => {
+  test('default construction.', () => {
+    expect(Environment.Dev.id).toBe('dev')
+    expect(Environment.Acc.name).toBe('Acceptance')
+  });
+});
