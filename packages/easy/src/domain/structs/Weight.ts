2c636f47b931e3397c5df8304c235b4484833ad9
robgeurden <rob.geurden@ibood.com>
2022-02-01 21:26:13 +0100

    feat(domain): Added greater than equals method on weight

--- packages/easy/src/domain/structs/Weight.ts
+++ packages/easy/src/domain/structs/Weight.ts
@@ -5,4 +5,7 @@ import { Struct } from '../Struct';
 export class Weight extends Struct {
   @required() readonly value: number = this.state.value;
   readonly uow: UnitOfWeight = UnitOfWeight.byId(this.state.uow, UnitOfWeight.G);
+  sizeInG = (): number => this.value * this.uow.gMultiplier;
+
+  gte = (w: Weight): boolean => this.sizeInG() >= w.sizeInG();
 }
--- packages/easy/test/domain/structs/Weight.test.ts
+++ packages/easy/test/domain/structs/Weight.test.ts
@@ -14,7 +14,6 @@ describe('Weight', () => {
       uow: UnitOfWeight.KG
     });
     expect(w).toBeValid();
-    // eslint-disable-next-line
     expect(w.value).toBe(100);
     expect(w.uow).toBe(UnitOfWeight.KG);
   });
@@ -22,8 +21,24 @@ describe('Weight', () => {
   test('Weight without UOW', () => {
     const w: Weight = new Weight({value: 200 });
     expect(w).toBeValid();
-    // eslint-disable-next-line
     expect(w.value).toBe(200);
     expect(w.uow).toBe(UnitOfWeight.G);
   });
+
+  test('SizeInG works', () => {
+    const w: Weight = new Weight({value: 200, uow: UnitOfWeight.KG });
+    expect(w).toBeValid();
+    expect(w.value).toBe(200);
+    expect(w.uow).toBe(UnitOfWeight.KG);
+  });
+
+  test('gte works', () => {
+    const heavy: Weight = new Weight({value: 200, uow: UnitOfWeight.KG });
+    const light: Weight = new Weight({value: 1, uow: UnitOfWeight.KG });
+    expect(heavy.gte(light)).toBeTruthy()
+    expect(light.gte(heavy)).toBeFalsy()
+    expect(light.gte(light)).toBeTruthy()
+  });
+
+
 });
