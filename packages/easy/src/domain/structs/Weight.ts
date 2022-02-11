e3301bfe56015e9359512e5dc05ebea2cef193c1
robgeurden <rob.geurden@ibood.com>
2022-02-11 14:41:49 +0100

    feat(domain): added lte and between to weight

--- packages/easy/src/domain/structs/Weight.ts
+++ packages/easy/src/domain/structs/Weight.ts
@@ -8,4 +8,11 @@ export class Weight extends Struct {
   sizeInG = (): number => this.value * this.uow.gMultiplier;
 
   gte = (w: Weight): boolean => this.sizeInG() >= w.sizeInG();
+  // gt = (w: Weight): boolean => this.sizeInG() > w.sizeInG();
+  lte = (w: Weight): boolean => this.sizeInG() <= w.sizeInG();
+  // lt = (w: Weight): boolean => this.sizeInG() < w.sizeInG();
+
+  between = (lower: Weight, upper= weight(Number.MAX_VALUE, this.uow)) => this.gte(lower) && this.lte(upper)
 }
+
+export const weight = (value: number, uow? :UnitOfWeight): Weight => new Weight({value, uow})
