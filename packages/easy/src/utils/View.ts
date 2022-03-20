a5db81563bafa11f4b2738fa1610313dabf1b032
aahoogendoorn <aahoogendoorn@gmail.com>
2022-03-20 14:33:31 +0100

    fix(utils): building up view, now with right dependency (7/)

--- packages/easy/src/utils/View.ts
+++ packages/easy/src/utils/View.ts
@@ -1,14 +1,13 @@
-import { isFunction, isObject, tryTo } from '../types';
-import { asJson } from '@thisisagile/easy-test/dist/utils/Utils';
+import { asJson, isFunction, isObject, tryTo } from '../types';
 
 type Func<T = unknown> = (a: any) => T;
+
 export type Viewer = { in?: { key: string, f?: Func }};
 
 export type InOut = { in?: Func | View, col?: string };
 export const isInOut = (v: unknown): v is InOut => isObject(v) && (isFunction(v.in) || v.in instanceof View);
 
 type Views = { [key: string]: string | Func | InOut };
-
 
 export class View {
   constructor(readonly views: Viewer[] = [], readonly startsFrom: 'scratch' | 'source' = 'scratch') {
@@ -17,4 +16,4 @@ export class View {
   from = (source: unknown): unknown => tryTo(asJson(source)).map(s => this.views.reduce((a: any, m) => ({ ...a, ...{ [m.in?.key ?? '']: m.in?.f && m.in.f(s) } }), this.startsFrom === 'scratch' ? {} : s)).value;
 }
 
-export const view = (map: Views, start?: 'scratch' | 'source'): View => new View([], start);
+export const view = (views: Views, startsFrom?: 'scratch' | 'source'): View => new View([], startsFrom);
