f0bc5e4888194edfdd678655ae8fbc583e016577
aahoogendoorn <aahoogendoorn@gmail.com>
2022-03-20 14:28:06 +0100

    fix(utils): building up view, now only one direction (6/)

--- packages/easy/src/utils/View.ts
+++ packages/easy/src/utils/View.ts
@@ -2,19 +2,19 @@ import { isFunction, isObject, tryTo } from '../types';
 import { asJson } from '@thisisagile/easy-test/dist/utils/Utils';
 
 type Func<T = unknown> = (a: any) => T;
-export type Viewer = { in?: { key: string, f?: Func }, out?: { key: string, f?: Func } };
+export type Viewer = { in?: { key: string, f?: Func }};
 
-export type InOut = { in?: Func | View, out?: Func | View, col?: string };
-export const isInOut = (v: unknown): v is InOut => isObject(v) && (isFunction(v.in) || v.in instanceof View || isFunction(v.out) || v.out instanceof View);
+export type InOut = { in?: Func | View, col?: string };
+export const isInOut = (v: unknown): v is InOut => isObject(v) && (isFunction(v.in) || v.in instanceof View);
 
 type Views = { [key: string]: string | Func | InOut };
 
 
 export class View {
-  constructor(readonly views: Viewer[] = [], readonly from: 'scratch' | 'source' = 'scratch') {
+  constructor(readonly views: Viewer[] = [], readonly startsFrom: 'scratch' | 'source' = 'scratch') {
   }
 
-  in = (source: unknown): unknown => tryTo(asJson(source)).map(s => this.views.reduce((a: any, m) => ({ ...a, ...{ [m.in?.key ?? '']: m.in?.f && m.in.f(s) } }), this.from === 'scratch' ? {} : s)).value;
+  from = (source: unknown): unknown => tryTo(asJson(source)).map(s => this.views.reduce((a: any, m) => ({ ...a, ...{ [m.in?.key ?? '']: m.in?.f && m.in.f(s) } }), this.startsFrom === 'scratch' ? {} : s)).value;
 }
 
-export const view = (map: Views, from?: 'scratch' | 'source'): View => new View([], from);
+export const view = (map: Views, start?: 'scratch' | 'source'): View => new View([], start);
--- packages/easy/test/utils/View.test.ts
+++ packages/easy/test/utils/View.test.ts
@@ -1,5 +1,6 @@
 import '@thisisagile/easy-test';
 import { isInOut, View, view } from '../../src';
+import { Dev } from '../ref';
 
 
 describe('View', () => {
@@ -7,26 +8,26 @@ describe('View', () => {
   test('construct default view', () => {
     const v = new View();
     expect(v.views).toHaveLength(0);
-    expect(v.from).toBe('scratch');
+    expect(v.startsFrom).toBe('scratch');
   });
 
   test('construct non-default view', () => {
     const v = new View([{}], 'source');
     expect(v.views).toHaveLength(1);
-    expect(v.from).toBe('source');
+    expect(v.startsFrom).toBe('source');
   });
 
   test('construct from view()', () => {
     const v = view({}, 'source');
     expect(v.views).toHaveLength(0);
-    expect(v.from).toBe('source');
+    expect(v.startsFrom).toBe('source');
   });
 
 
   test('construct with actual view', () => {
     const persons = view({ first: 'FirstName' });
     expect(persons.views).toHaveLength(0);
-    expect(persons.from).toBe('scratch');
+    expect(persons.startsFrom).toBe('scratch');
   });
 
   test('from scratch and from source', () => {
@@ -36,8 +37,9 @@ describe('View', () => {
     };
     const fromScratch = view({}, 'scratch');
     const fromSource = view({}, 'source');
-    expect(fromScratch.in(source)).toStrictEqual({});
-    expect(fromSource.in(source)).toStrictEqual(source);
+    expect(fromScratch.from(source)).toStrictEqual({});
+    expect(fromSource.from(source)).toStrictEqual(source);
+    expect(fromSource.from(Dev.Wouter)).toStrictEqual(Dev.Wouter.toJSON());
   });
 
   test('isInOut', () => {
@@ -47,10 +49,8 @@ describe('View', () => {
     expect(isInOut({in: {}})).toBeFalsy();
     expect(isInOut({out: {}})).toBeFalsy();
     expect(isInOut({in: () => ''})).toBeTruthy();
-    expect(isInOut({out: () => ''})).toBeTruthy();
     expect(isInOut({in: v})).toBeTruthy();
-    expect(isInOut({out: v})).toBeTruthy();
-    expect(isInOut({in: v, out: v, col: 'name'})).toBeTruthy();
+    expect(isInOut({in: v, col: 'name'})).toBeTruthy();
   })
 
 });
