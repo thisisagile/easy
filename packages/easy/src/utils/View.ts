ac163d107434719f716c88197abc4053e8ff9c60
aahoogendoorn <aahoogendoorn@gmail.com>
2022-03-20 17:20:00 +0100

    fix(utils): building up view, adding InOut to toViewers (12/)

--- packages/easy/src/utils/View.ts
+++ packages/easy/src/utils/View.ts
@@ -1,27 +1,25 @@
-import { asJson, isFunction, isObject, isString, meta, tryTo } from '../types';
+import { asJson, isDefined, isFunction, isObject, isString, meta, tryTo } from '../types';
 import { traverse } from './Traverse';
 import { choose } from './Case';
 
 type Func<T = unknown> = (a: any) => T;
 
 export type InOut = { in?: Func | View, col?: string };
-export const isInOut = (v: unknown): v is InOut => isObject(v) && (isFunction(v.in) || v.in instanceof View);
+export const isInOut = (v: unknown): v is InOut => isObject(v) && (isDefined(v.col) || isFunction(v.in) || v.in instanceof View);
 
 export type Views = { [key: string]: string | Func | InOut };
 export type Viewer = { in?: { key: string, f?: Func } };
 
 const toFunc = (a: unknown, key: string, io: InOut): Func =>
-  a => traverse(a, io.col);
-// choose<Func, InOut>(io)
-//   .case(i => isDefined(i.col) && !isDefined(i.in), (i: InOut) => (a: any) => traverse(a, i.col))
-//   .else(() => 'Boet');
+  choose<Func, InOut>(io)
+    .case(i => isDefined(i.col) && !isDefined(i.in), (i: InOut) => (a: any) => traverse(a, i.col))
+    .else(() => undefined);
 
 const toViewer = (key: string, value: unknown): Viewer =>
   choose<Viewer>(value)
     .type(isString, s => toViewer(key, (a: any) => traverse(a, s)))
     .type(isFunction, f => toViewer(key, { in: { key, f } }))
-    .type(isInOut, io => toViewer(key, (a: any) => traverse(a, io.col)))
-    .case(v => (v as any).col, v => toViewer(key, (a: any) => traverse(a, (v as any).col)))
+    .type(isInOut, io => toViewer(key, (a: any) => toFunc(a, key, io)(a)))
     .else(m => m as Viewer);
 
 export const toViewers = (views: Views): Viewer[] =>
