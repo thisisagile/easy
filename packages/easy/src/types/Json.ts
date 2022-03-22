988a93f3dd1ea8952252fca32def824e629cc476
aahoogendoorn <aahoogendoorn@gmail.com>
2022-03-22 08:42:16 +0100

    feat(types): adding delete to json, and adding set to json, that handles both delete and add.

--- packages/easy/src/types/Json.ts
+++ packages/easy/src/types/Json.ts
@@ -1,5 +1,5 @@
 import { isA } from './IsA';
-import { isDefined, isObject } from './Is';
+import { isDefined, isEmpty, isObject } from './Is';
 import { Get, ofGet } from './Get';
 
 export type JsonValue = string | number | boolean | null | Json | JsonValue[];
@@ -10,14 +10,24 @@ export const isJson = (subject?: unknown): subject is { toJSON: () => Json } => 
 export const json = {
   parse: (subject: unknown): Json => JSON.parse(JSON.stringify(subject ?? {})),
   merge: (...subjects: unknown[]): Json => json.parse(subjects.map(s => asJson(s, s => json.parse(s))).reduce((js, j) => ({ ...js, ...j }), {})),
-  omit: (subject: unknown, ...keys: string[]): Json =>
-    keys.reduce((js, k) => {
-      delete js[k];
-      return js;
-    }, json.parse(subject)),
-  add: (subject: Json, key: string, value?: unknown): Json => isDefined(value) ? ({ ...subject, ...{ [key]: value as Json } }) : subject,
+  delete: (subject: unknown, key: string): Json => {
+    delete (subject as any)[key];
+    return subject as Json;
+  },
+  set: (subject: unknown, key = '', value?: unknown): Json => isEmpty(key) ? subject : (isDefined(value) ? { ...(subject as any), ...{ [key]: value as Json } } : json.delete(subject, key)),
+  omit: (subject: unknown, ...keys: string[]): Json => keys.reduce((js, k) => json.delete(js, k), json.parse(subject)),
 };
 
 export const toJson = json.merge;
 
 export const asJson = (j?: unknown, alt: Get<Json> = {}): Json => (isJson(j) ? j.toJSON() : isObject(j) ? (j as Json) : ofGet(alt, j));
+
+class Any {
+  constructor(readonly value: unknown = {}) {
+  }
+
+  delete = (key: string): Any => new Any(json.delete(this.value, key));
+  set = (key: string, value?: unknown): Any => new Any(json.set(this.value, key, value));
+}
+
+export const any = (value: unknown): Any => new Any(value);
