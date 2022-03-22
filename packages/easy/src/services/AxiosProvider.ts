92ee1c4f94621e50c8e32e1bf6bc8d91a9a32552
aahoogendoorn <aahoogendoorn@gmail.com>
2022-03-22 10:20:10 +0100

    fix(service): introducing type guards in axios

--- packages/easy/src/services/AxiosProvider.ts
+++ packages/easy/src/services/AxiosProvider.ts
@@ -1,19 +1,15 @@
-import axios, { AxiosError, Method } from 'axios';
+import axios, { AxiosError, AxiosResponse, Method } from 'axios';
 import { HttpStatus, HttpVerb, isRestResult, Request, RequestOptions, RequestProvider, Response, toResponse } from '../http';
 import { ctx, isDefined, isEmpty, toResult, Uri } from '../types';
 import { choose } from '../utils';
 
+const isResponse = (a: unknown): a is {response: AxiosResponse} => isDefined((a as any)?.response);
+const isRequest = (a: unknown): a is {request: any} => isDefined((a as any)?.request);
+
 const asResponse = (uri: Uri, verb: HttpVerb, error: AxiosError): Response =>
   choose<Response, AxiosError>(error)
-    .case(
-      e => isDefined(e.response),
-      (e: any) =>
-        toResponse(e.response.status, isRestResult(e.response.data) ? e.response.data : toResult(e.response.statusText, uri.path, uri), e.response.headers)
-    )
-    .case(
-      e => isDefined(e.request),
-      (e: any) => toResponse(e.request.status, toResult(e.request.message, uri.path, uri))
-    )
+    .type(isResponse, r => toResponse(r.response.status, isRestResult(r.response.data) ? r.response.data : toResult(r.response.statusText, uri.path, uri), r.response.headers))
+    .type(isRequest, r => toResponse(r.request.status, toResult(r.request.message, uri.path, uri)))
     .else(e => toResponse(HttpStatus.InternalServerError, toResult(e.message, uri.path, uri)));
 
 export class AxiosProvider implements RequestProvider {
