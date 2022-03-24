9a355416b5c5b319efe1c8041b1fc597cdd13ce3
aahoogendoorn <aahoogendoorn@gmail.com>
2022-03-24 11:54:24 +0100

    fix(express): updated ErrorHandler.ts with type guards.

--- packages/easy-express/src/express/ErrorHandler.ts
+++ packages/easy-express/src/express/ErrorHandler.ts
@@ -28,29 +28,20 @@ const toResponse = (status: HttpStatus, errors: Result[] = []): Response => ({
 
 const toBody = ({ origin, options }: OriginatedError): Response => {
   return choose<Response, any>(origin)
-    .case(
-      o => isAuthError(o),
-      o => toResponse(toHttpStatus(o.status), [toResult(o.message)]),
-    )
+    .type(isAuthError, ae => toResponse(toHttpStatus(ae.status), [toResult(ae.message)]))
     .case(
       o => Exception.DoesNotExist.equals(o),
       (o: Exception) => toResponse(options?.onNotFound ?? HttpStatus.NotFound, [toResult(o.reason ?? o.message)]),
     )
-      // This service breaks with an error
+    // This service breaks with an error
     .type(isError, e => toResponse(HttpStatus.InternalServerError, [toResult(e.message)]))
     // This service fails
     .type(isResults, r => toResponse(options?.onError ?? HttpStatus.BadRequest, r.results))
     // Underlying service fails
     .type(isResponse, r => toResponse(HttpStatus.InternalServerError, r.body.error?.errors))
-    .case(
-      o => isException(o),
-      (o: Exception) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(o.reason ?? o.message)]),
-    )
-    .case(
-      // This service fails with a string
-      o => isText(o),
-      (o: Response) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(asString(o))]),
-    )
+    .type(isException, e => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(e.reason ?? e.message)]))
+    // This service fails with a string
+    .type(isText, t => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(asString(t))]))
     .else(() => toResponse(HttpStatus.InternalServerError, [toResult('Unknown error')]));
 };
 
