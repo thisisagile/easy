a7e4a688a280ed24c7bc7a2ec8dc23ecced8c4ef
aahoogendoorn <aahoogendoorn@gmail.com>
2022-03-24 11:37:25 +0100

    fix(express): migrating use of choose update on ErrorHandler.ts. (2/)

--- packages/easy-express/src/express/ErrorHandler.ts
+++ packages/easy-express/src/express/ErrorHandler.ts
@@ -15,7 +15,6 @@ import {
   Response,
   rest,
   Result,
-  Results,
   toHttpStatus,
   toOriginatedError,
   toResult,
@@ -31,33 +30,29 @@ const toBody = ({ origin, options }: OriginatedError): Response => {
   return choose<Response, any>(origin)
     .case(
       o => isAuthError(o),
-      o => toResponse(toHttpStatus(o.status), [toResult(o.message)])
+      o => toResponse(toHttpStatus(o.status), [toResult(o.message)]),
     )
     .case(
       o => Exception.DoesNotExist.equals(o),
-      (o: Exception) => toResponse(options?.onNotFound ?? HttpStatus.NotFound, [toResult(o.reason ?? o.message)])
+      (o: Exception) => toResponse(options?.onNotFound ?? HttpStatus.NotFound, [toResult(o.reason ?? o.message)]),
     )
     .case(
       // This service breaks with an error
       o => isError(o),
-      (o: Error) => toResponse(HttpStatus.InternalServerError, [toResult(o.message)])
+      (o: Error) => toResponse(HttpStatus.InternalServerError, [toResult(o.message)]),
     )
-    .case(
-      // This service fails
-      o => isResults(o),
-      (o: Results) => toResponse(options?.onError ?? HttpStatus.BadRequest, o.results)
-    )
-      // Underlying service fails
-    .type(isResponse, r => toResponse(HttpStatus.InternalServerError, r.body.error?.errors)
-    )
+    // This service fails
+    .type(isResults, r => toResponse(options?.onError ?? HttpStatus.BadRequest, r.results))
+    // Underlying service fails
+    .type(isResponse, r => toResponse(HttpStatus.InternalServerError, r.body.error?.errors))
     .case(
       o => isException(o),
-      (o: Exception) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(o.reason ?? o.message)])
+      (o: Exception) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(o.reason ?? o.message)]),
     )
     .case(
       // This service fails with a string
       o => isText(o),
-      (o: Response) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(asString(o))])
+      (o: Response) => toResponse(options?.onError ?? HttpStatus.BadRequest, [toResult(asString(o))]),
     )
     .else(() => toResponse(HttpStatus.InternalServerError, [toResult('Unknown error')]));
 };
@@ -67,9 +62,8 @@ export const error = (e: Error, req: express.Request, res: express.Response, _ne
     .map(oe => toBody(oe))
     .accept(r => (ctx.request.lastError = r.status.isServerError ? r.body.error?.errors[0]?.message : undefined))
     .accept(r => res.status(r.status.status).json(r.body))
-    .recover(o =>
-    {
+    .recover(o => {
       console.error('ErrorHandler:', o);
-     return  res.status(HttpStatus.InternalServerError.status).json(toResponse(HttpStatus.InternalServerError, [toResult(HttpStatus.InternalServerError.name)]).body) as any
+      return res.status(HttpStatus.InternalServerError.status).json(toResponse(HttpStatus.InternalServerError, [toResult(HttpStatus.InternalServerError.name)]).body) as any;
     });
 };
