4e496e092367f13664baaa62a4b3b36aa403c3f0
aahoogendoorn <aahoogendoorn@gmail.com>
2022-04-14 17:08:25 +0200

    fix(services): added transformError to Api.

--- packages/easy/src/services/Api.ts
+++ packages/easy/src/services/Api.ts
@@ -5,20 +5,20 @@ import { AxiosProvider } from './AxiosProvider';
 export class Api {
   constructor(readonly provider: RequestProvider = new AxiosProvider()) {}
 
-  get = (uri: Uri, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
-    this.provider.execute({ uri, verb: HttpVerb.Get, transform, options });
+  get = (uri: Uri, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any, transformError = (r: any) => r): Promise<Response> =>
+    this.provider.execute({ uri, verb: HttpVerb.Get, transform, transformError, options });
 
-  post = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
-    this.provider.execute({ uri, verb: HttpVerb.Post, body, transform, options });
+  post = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any, transformError = (r: any) => r): Promise<Response> =>
+    this.provider.execute({ uri, verb: HttpVerb.Post, body, transform, transformError, options });
 
-  put = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
-    this.provider.execute({ uri, verb: HttpVerb.Put, body, transform, options });
+  put = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any, transformError = (r: any) => r): Promise<Response> =>
+    this.provider.execute({ uri, verb: HttpVerb.Put, body, transform, transformError, options });
 
-  patch = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any): Promise<Response> =>
-    this.provider.execute({ uri, verb: HttpVerb.Patch, body, transform, options });
+  patch = (uri: Uri, body?: unknown, options: RequestOptions = RequestOptions.Json, transform?: (r: any) => any, transformError = (r: any) => r): Promise<Response> =>
+    this.provider.execute({ uri, verb: HttpVerb.Patch, body, transform, transformError, options });
 
-  delete = (uri: Uri, options: RequestOptions = RequestOptions.Json, transform?: (b: any) => any): Promise<Response> =>
-    this.provider.execute({ uri, verb: HttpVerb.Delete, transform, options });
+  delete = (uri: Uri, options: RequestOptions = RequestOptions.Json, transform?: (b: any) => any, transformError = (r: any) => r): Promise<Response> =>
+    this.provider.execute({ uri, verb: HttpVerb.Delete, transform, transformError, options });
 }
 
 export const api: Api = new Api();
