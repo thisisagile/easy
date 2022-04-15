947520247e80d12f3b85e45ca1b9a2c69af6e434
aahoogendoorn <aahoogendoorn@gmail.com>
2022-04-15 11:43:12 +0200

    feat(services): optimized MappedRouteGateway.ts

--- packages/easy/src/services/MappedRouteGateway.ts
+++ packages/easy/src/services/MappedRouteGateway.ts
@@ -1,23 +1,20 @@
 import { Api } from './Api';
-import { Func, Id, Json, JsonValue, List, Uri } from '../types';
+import { Func, Json, List, Uri } from '../types';
 import { Mapper } from '../utils';
 import { RouteGateway } from './RouteGateway';
+import { RequestOptions } from '../http';
 
 export class MappedRouteGateway extends RouteGateway {
   constructor(readonly route: Func<Uri>, readonly routeId: Func<Uri>, readonly map = new Mapper(), readonly api: Api = new Api()) {
     super(route, routeId, api);
   }
 
-  all(): Promise<List<Json>> {
-    return super.all().then(is => is.map(i => this.map.in(i)));
+  get(uri: Uri, options?: RequestOptions): Promise<List<Json>> {
+    return super.get(uri, options).then(is => is.map(i => this.map.in(i)));
   }
 
-  byId(id: Id): Promise<Json | undefined> {
-    return super.byId(id).then(i => this.map.in(i));
-  }
-
-  search(q: JsonValue): Promise<List<Json>> {
-    return super.search(q).then(is => is.map(i => this.map.in(i)));
+  getOne(uri: Uri, options?: RequestOptions): Promise<Json | undefined> {
+    return super.get(uri, options).then(is => is.first()).then(i => this.map.in(i));
   }
 
   add(item: Json): Promise<Json> {
