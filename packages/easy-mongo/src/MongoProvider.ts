ac1564e7acbb8a6c586213e93c9a6baf222b6e40
woutervanbakel <wouter.vb@gmail.com>
2022-04-15 09:24:38 +0200

    feat(easy-mongo): Update to Mongo Node driver v4.x
    See migration guideline https://github.com/mongodb/node-mongodb-native/blob/4.0/docs/CHANGES_4.0.0.md#removed-deprecations
    
    BREAKING CHANGE: Update to Mongo Node driver v4.x

--- packages/easy-mongo/src/MongoProvider.ts
+++ packages/easy-mongo/src/MongoProvider.ts
@@ -16,7 +16,7 @@ import {
   toList,
   when,
 } from '@thisisagile/easy';
-import { Collection as MongoCollection, FilterQuery as MongoFilterQuery, MongoClient } from 'mongodb';
+import { Collection as MongoCollection, Filter as MongoFilter, MongoClient } from 'mongodb';
 import { Collection } from './Collection';
 import { toMongoType } from './Utils';
 
@@ -31,42 +31,31 @@ export type FindOptions = {
   sort?: SortCondition[];
 };
 
-export type FilterQuery<T> = MongoFilterQuery<T>;
+export type Filter<T> = MongoFilter<T>;
 
 export class MongoProvider {
   constructor(readonly coll: Collection, private client?: Promise<MongoClient>) {}
 
+  static client(db: Database): Promise<MongoClient> {
+    return when(db.options?.cluster)
+      .not.isDefined.reject(Exception.IsNotValid.because('Missing cluster in database options.'))
+      .then(u => new MongoClient(u, { auth: { username: asString(db.options?.user), password: asString(db.options?.password) } }));
+  }
+
   cluster(): Promise<MongoClient> {
     return Promise.resolve()
-      .then(() => this.client ?? (this.client = MongoProvider.connect(this.coll.db)))
+      .then(() => this.client ?? (this.client = MongoProvider.client(this.coll.db)))
       .catch(e => {
         this.client = undefined;
         return reject(e);
       });
   }
 
-  reconnect(client: MongoClient): Promise<MongoClient> {
-    this.client = client.isConnected() ? Promise.resolve(client) : undefined;
-    return this.cluster();
-  }
-
-  static connect(db: Database): Promise<MongoClient> {
-    return when(db.options?.cluster)
-      .not.isDefined.reject(new Exception('Missing cluster in database options.'))
-      .then(u =>
-        new MongoClient(u, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          auth: { user: asString(db.options?.user), password: asString(db.options?.password) },
-        }).connect()
-      );
-  }
-
-  toMongoJson(query: Condition | LogicalCondition | FilterQuery<any>): Json {
+  toMongoJson(query: Condition | LogicalCondition | Filter<any>): Json {
     return toMongoType(asJson(query));
   }
 
-  find(query: Condition | LogicalCondition | FilterQuery<any>, options: FindOptions = { limit: 250 }): Promise<List<Json>> {
+  find(query: Condition | LogicalCondition | Filter<any>, options: FindOptions = { limit: 250 }): Promise<List<Json>> {
     return this.collection()
       .then(c => c.find(this.toMongoJson(query), { ...options, limit: options.limit ?? 250, sort: this.coll.sort(...(options.sort ?? [])) as any }))
       .then(res => res.toArray())
@@ -81,14 +70,14 @@ export class MongoProvider {
   byId(id: Id): Promise<Json> {
     return this.collection()
       .then(c => c.findOne(this.toMongoJson({ id: asString(id) })))
-      .then(i => omitId(i));
+      .then(i => omitId(i as Json));
   }
 
   by(key: string, value: JsonValue, options?: FindOptions): Promise<List<Json>> {
     return this.find({ [key]: asString(value) }, options);
   }
 
-  group(qs: FilterQuery<any>[]): Promise<Json[]> {
+  group(qs: Filter<any>[]): Promise<Json[]> {
     return this.collection()
       .then(c => c.aggregate(qs.map(q => this.toMongoJson(q))))
       .then(res => res.toArray());
@@ -96,10 +85,10 @@ export class MongoProvider {
 
   aggregate = this.group;
 
-  add(o: Json): Promise<Json> {
+  add(item: Json): Promise<Json> {
     return this.collection()
-      .then(c => c.insertOne(omitId(o)))
-      .then(i => omitId(i.ops[0]));
+      .then(c => c.insertOne(omitId(item)))
+      .then(() => omitId(item));
   }
 
   update(item: Json): Promise<Json> {
@@ -111,10 +100,10 @@ export class MongoProvider {
   remove(id: Id): Promise<boolean> {
     return this.collection()
       .then(c => c.deleteOne(this.toMongoJson({ id })))
-      .then(d => d.result.ok === 1);
+      .then(d => d.acknowledged);
   }
 
-  count(query?: Condition | LogicalCondition | FilterQuery<any>): Promise<number> {
+  count(query?: Condition | LogicalCondition | Filter<any>): Promise<number> {
     return this.collection().then(c => c.countDocuments(this.toMongoJson(query ?? {})));
   }
 
@@ -122,7 +111,7 @@ export class MongoProvider {
     return this.collection().then(c => c.createIndex(field, { unique, writeConcern: { w: 1 } }));
   }
 
-  createPartialIndex(field: string | any, filter: Condition | LogicalCondition | FilterQuery<any>, unique = true): Promise<string> {
+  createPartialIndex(field: string | any, filter: Condition | LogicalCondition | Filter<any>, unique = true): Promise<string> {
     return this.collection().then(c => c.createIndex(field, { partialFilterExpression: toMongoType(asJson(filter)), unique, writeConcern: { w: 1 } }));
   }
 
@@ -133,7 +122,7 @@ export class MongoProvider {
 
   collection(): Promise<MongoCollection> {
     return this.cluster()
-      .then(c => this.reconnect(c))
+      .then(c => c.connect())
       .then(c => c.db(this.coll.db.name))
       .then(db => db.collection(asString(this.coll)));
   }
