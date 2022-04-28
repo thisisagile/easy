19e33309afcb40cbfd4d1aa5f0f741d4cc9d6116
aahoogendoorn <aahoogendoorn@gmail.com>
2022-04-28 10:12:27 +0200

    test(types): adding unit tests (2/)

--- packages/easy/test/types/Gateway.test.ts
+++ packages/easy/test/types/Gateway.test.ts
@@ -12,4 +12,8 @@ describe('Gateway', () => {
   test('all', () => {
     return expect(gateway.all()).rejects.toBe(Exception.IsNotImplemented);
   });
+
+  test('byId', () => {
+    return expect(gateway.byId(42)).rejects.toBe(Exception.IsNotImplemented);
+  });
 });
