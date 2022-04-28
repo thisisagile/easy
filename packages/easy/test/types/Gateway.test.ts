1bc3527700617ce4a0c785944145e5dd415bf3c5
aahoogendoorn <aahoogendoorn@gmail.com>
2022-04-28 10:16:23 +0200

    test(types): adding unit tests (3/)

--- packages/easy/test/types/Gateway.test.ts
+++ packages/easy/test/types/Gateway.test.ts
@@ -16,4 +16,33 @@ describe('Gateway', () => {
   test('byId', () => {
     return expect(gateway.byId(42)).rejects.toBe(Exception.IsNotImplemented);
   });
+
+  test('by', () => {
+    return expect(gateway.by('name', 42)).rejects.toBe(Exception.IsNotImplemented);
+  });
+
+  test('byIds', () => {
+    return expect(gateway.byIds(41, 42)).rejects.toBe(Exception.IsNotImplemented);
+  });
+
+  test('search', () => {
+    return expect(gateway.search({ name: 41 })).rejects.toBe(Exception.IsNotImplemented);
+  });
+
+  test('exists', () => {
+    return expect(gateway.exists(42)).rejects.toBe(Exception.IsNotImplemented);
+  });
+
+  test('add', () => {
+    return expect(gateway.add({ name: 41 })).rejects.toBe(Exception.IsNotImplemented);
+  });
+
+  test('update', () => {
+    return expect(gateway.update({ name: 41 })).rejects.toBe(Exception.IsNotImplemented);
+  });
+
+  test('remove', () => {
+    return expect(gateway.remove(42)).rejects.toBe(Exception.IsNotImplemented);
+  });
+
 });
