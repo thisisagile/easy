57f3cf24745d7a4a19230621f7f603b6b12db7bb
aahoogendoorn <aahoogendoorn@gmail.com>
2022-04-30 10:36:21 +0200

    fix(types): minor detail.

--- packages/easy/src/types/Get.ts
+++ packages/easy/src/types/Get.ts
@@ -1,6 +1,6 @@
 import { Func, isFunc } from './Func';
 
-export type Get<T = any, Args = any> = T | Func<T, Args>;
+export type Get<T = any, Args = any> = Func<T, Args> | T;
 export type Predicate<Args = unknown> = Get<boolean, Args>;
 export const ofGet = <T, Args = any>(g: Get<T, Args>, ...args: Args[]): T => (isFunc<T, Args>(g) ? g(...args) : g);
 export const ifGet = <T>(pred: Get, valid: Get<T>, invalid: Get<T>): T => (ofGet(pred) ? ofGet(valid) : ofGet(invalid));
