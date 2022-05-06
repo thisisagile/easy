9b8170ecc1abd5217473d795a11aa1b9d40cca48
aahoogendoorn <aahoogendoorn@gmail.com>
2022-05-06 19:21:16 +0200

    fix(types): Created PageOptions type.

--- packages/easy/src/types/PageList.ts
+++ packages/easy/src/types/PageList.ts
@@ -1,9 +1,13 @@
 import { List, toList } from './List';
 
-export type PageList<T> = List<T> & { skip?: number, take?: number, total?: number };
+export type PageOptions = { take?: number, skip?: number, total?: number };
 
-export const toPageList = <T>(items?: T[], total?: number): PageList<T> => {
-  const list = toList<T>(...items ?? []);
-  (list as any).total = total;
+export type PageList<T> = List<T> & PageOptions;
+
+export const toPageList = <T>(items?: T[], options: PageOptions = {take: 250, skip: 0}): PageList<T> => {
+  const list = toList<T>(...items ?? []) as any;
+  list.take = options.take;
+  list.skip = options.skip;
+  list.total = options.total;
   return list;
 };
--- packages/easy/test/types/PageList.test.ts
+++ packages/easy/test/types/PageList.test.ts
@@ -5,39 +5,50 @@ import { Dev } from '../ref';
 describe('PageList', () => {
 
   test('toPageList empty', () => {
-    const tl = toPageList();
-    expect(tl).toBeDefined();
-    expect(tl).toHaveLength(0);
-    expect(tl.total).toBeUndefined();
+    const pl = toPageList();
+    expect(pl).toBeDefined();
+    expect(pl).toHaveLength(0);
+    expect(pl.take).toBe(250);
+    expect(pl.skip).toBe(0);
+    expect(pl.total).toBeUndefined();
+  });
+
+  test('toPageList with full options', () => {
+    const pl = toPageList([], {take: 5, skip: 1, total: 42});
+    expect(pl).toBeDefined();
+    expect(pl).toHaveLength(0);
+    expect(pl.take).toBe(5);
+    expect(pl.skip).toBe(1);
+    expect(pl.total).toBe(42);
   });
 
   test('toPageList empty list', () => {
-    const tl = toPageList([]);
-    expect(tl).toHaveLength(0);
-    expect(tl.total).toBeUndefined();
+    const pl = toPageList([]);
+    expect(pl).toHaveLength(0);
+    expect(pl.total).toBeUndefined();
   });
 
   test('toPageList undefined and total', () => {
-    const tl = toPageList(undefined, 42);
-    expect(tl).toHaveLength(0);
-    expect(tl.total).toBe(42);
+    const pl = toPageList(undefined, { total: 42 });
+    expect(pl).toHaveLength(0);
+    expect(pl.total).toBe(42);
   });
 
   test('toPageList empty list and total', () => {
-    const tl = toPageList([], 42);
-    expect(tl).toHaveLength(0);
-    expect(tl.total).toBe(42);
+    const pl = toPageList([], { total: 42 });
+    expect(pl).toHaveLength(0);
+    expect(pl.total).toBe(42);
   });
 
   test('toPageList list', () => {
-    const tl = toPageList(Dev.All);
-    expect(tl).toHaveLength(Dev.All.length);
-    expect(tl.total).toBeUndefined();
+    const pl = toPageList(Dev.All);
+    expect(pl).toHaveLength(Dev.All.length);
+    expect(pl.total).toBeUndefined();
   });
 
   test('toPageList list and total', () => {
-    const tl = toPageList(Dev.All, 42);
-    expect(tl).toHaveLength(Dev.All.length);
-    expect(tl.total).toBe(42);
+    const pl = toPageList(Dev.All, { total: 42 });
+    expect(pl).toHaveLength(Dev.All.length);
+    expect(pl.total).toBe(42);
   });
 });
