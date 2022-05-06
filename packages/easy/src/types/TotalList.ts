93a4415922e88cbfa558671add4bbe10f339b317
aahoogendoorn <aahoogendoorn@gmail.com>
2022-05-06 17:47:10 +0200

    fix(types): renamed TotalledList.ts to TotalList.ts (2/).

--- packages/easy/src/types/TotalList.ts
+++ packages/easy/src/types/TotalList.ts
@@ -1,9 +1,8 @@
 import { List, toList } from './List';
-import { ArrayLike } from './Array';
 
 export type TotalList<T> = List<T> & { total?: number };
 
-export const toTotalList = <T>(items?: ArrayLike<T>, total?: number): TotalList<T> => {
+export const toTotalList = <T>(items?: T[], total?: number): TotalList<T> => {
   const list = toList<T>(...items ?? []);
   (list as any).total = total;
   return list;
--- packages/easy/test/types/TotalList.test.ts
+++ packages/easy/test/types/TotalList.test.ts
@@ -2,40 +2,40 @@ import '@thisisagile/easy-test';
 import { toTotalList } from '../../src';
 import { Dev } from '../ref';
 
-describe('TotalledList', () => {
+describe('TotalList', () => {
 
-  test('toTotalledList empty', () => {
+  test('toTotalList empty', () => {
     const tl = toTotalList();
     expect(tl).toBeDefined();
     expect(tl).toHaveLength(0);
     expect(tl.total).toBeUndefined();
   });
 
-  test('toTotalledList empty list', () => {
+  test('toTotalList empty list', () => {
     const tl = toTotalList([]);
     expect(tl).toHaveLength(0);
     expect(tl.total).toBeUndefined();
   });
 
-  test('toTotalledList undefined and total', () => {
+  test('toTotalList undefined and total', () => {
     const tl = toTotalList(undefined, 42);
     expect(tl).toHaveLength(0);
     expect(tl.total).toBe(42);
   });
 
-  test('toTotalledList empty list and total', () => {
+  test('toTotalList empty list and total', () => {
     const tl = toTotalList([], 42);
     expect(tl).toHaveLength(0);
     expect(tl.total).toBe(42);
   });
 
-  test('toTotalledList list', () => {
+  test('toTotalList list', () => {
     const tl = toTotalList(Dev.All);
     expect(tl).toHaveLength(Dev.All.length);
     expect(tl.total).toBeUndefined();
   });
 
-  test('toTotalledList list and total', () => {
+  test('toTotalList list and total', () => {
     const tl = toTotalList(Dev.All, 42);
     expect(tl).toHaveLength(Dev.All.length);
     expect(tl.total).toBe(42);
