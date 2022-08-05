2c1b4679ee9a4805a88008e27a8eb20108126581
Jeroen Plug <jeroen.plug@gmail.com>
2022-08-05 14:05:00 +0200

    fix(easy): Update traverse method to prevent build issues with nextjs

--- packages/easy/src/utils/Traverse.ts
+++ packages/easy/src/utils/Traverse.ts
@@ -1,4 +1,5 @@
 export const traverse = (subject: unknown = {}, property = ''): unknown => {
-  const [p, ...props] = property.split('.');
+  const props = property.split('.');
+  const p = props.shift() as string;
   return props.length === 0 ? (subject as any)[p] : traverse((subject as any)[p], props.join('.'));
 };
