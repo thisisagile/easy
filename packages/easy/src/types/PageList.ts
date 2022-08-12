3bbdcec82071582289f545a9e7346efa01672308
aahoogendoorn <aahoogendoorn@gmail.com>
2022-08-12 13:33:34 +0200

    fix(types): adding Filter to PageList

--- packages/easy/src/types/PageList.ts
+++ packages/easy/src/types/PageList.ts
@@ -4,13 +4,15 @@ import { isA } from './IsA';
 
 export type Sort = { key: string; value: -1 | 1 };
 
-export type PageOptions = { take?: number; skip?: number; sort?: Sort[] };
+export type FilterValue = { label: string, value: unknown };
+export type Filter = { label: string, field: string, values: FilterValue[] };
 
-export type PageList<T> = List<T> & Omit<PageOptions, 'sort'> & { total?: number };
+export type PageOptions = { take?: number; skip?: number; sort?: Sort[]; filter?: Filter[] };
+export type PageList<T> = List<T> & Omit<PageOptions, 'sort' | 'filter'> & { total?: number };
 
 export const isPageList = <T>(l?: T[]): l is PageList<T> => isList<T>(l) && isA(l, 'total');
 
-export const toPageList = <T>(items?: T[], options?: Omit<PageOptions, 'sort'> & { total?: number }): PageList<T> => {
+export const toPageList = <T>(items?: T[], options?: Omit<PageOptions, 'sort' | 'filter'> & { total?: number }): PageList<T> => {
   const list = toList<T>(...(items ?? [])) as any;
   list.take = options?.take ?? 250;
   list.skip = options?.skip ?? 0;
@@ -21,5 +23,5 @@ export const toPageList = <T>(items?: T[], options?: Omit<PageOptions, 'sort'> &
 export const asPageList = <T, U>(c: Construct<T>, items = toPageList<U>()): PageList<T> =>
   toPageList<T>(
     items.map(i => ofConstruct(c, i)),
-    items
+    items,
   );
