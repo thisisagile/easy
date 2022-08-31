0c4987bc1397177ac12596048c8912493a3d731f
woutervanbakel <wouter.vb@gmail.com>
2022-08-31 12:44:01 +0200

    feat(easy): Extend CacheControl with more options and make it extendable

--- packages/easy/src/http/CacheControl.ts
+++ packages/easy/src/http/CacheControl.ts
@@ -1,29 +1,76 @@
 import { isDefined } from '../types';
 
 export class CacheControl {
-  private constructor(private age: number = 1, private stale?: number, readonly enabled = true) {}
+  protected constructor(readonly enabled = true) {}
 
-  static disabled = () => new CacheControl(0, 0, false);
+  protected _maxAge?: number;
+  protected _sharedMaxAge?: number;
+  protected _noCache?: boolean;
+  protected _mustRevalidate?: boolean;
+  protected _private?: boolean;
+  protected _public?: boolean;
+  protected _immutable?: boolean;
+  protected _staleWhileRevalidate?: number;
+
+  static disabled = () => new CacheControl(false);
   static OneSecond = () => new CacheControl().maxAge(1).staleWhileRevalidate(1);
   static fiveSeconds = () => new CacheControl().maxAge(5).staleWhileRevalidate(5);
   static tenSeconds = () => new CacheControl().maxAge(10).staleWhileRevalidate(10);
   static thirtySeconds = () => new CacheControl().maxAge(30).staleWhileRevalidate(30);
   static sixtySeconds = () => new CacheControl().maxAge(60).staleWhileRevalidate(60);
-  static custom = (maxAge: number, staleWhileRevalidate?: number) => new CacheControl().maxAge(maxAge).staleWhileRevalidate(staleWhileRevalidate);
+  static custom = (maxAge?: number, staleWhileRevalidate?: number) => new CacheControl().maxAge(maxAge).staleWhileRevalidate(staleWhileRevalidate);
 
-  maxAge = (a: number): this => {
-    this.age = Math.abs(a);
+  readonly maxAge = (a?: number): this => {
+    this._maxAge = a;
     return this;
   };
 
-  staleWhileRevalidate = (s?: number): this => {
-    this.stale = isDefined(s) ? Math.abs(s) : undefined;
+  readonly sharedMaxAge = (a?: number): this => {
+    this._sharedMaxAge = a;
+    return this;
+  };
+
+  readonly noCache = (a?: boolean): this => {
+    this._noCache = a;
+    return this;
+  };
+
+  readonly mustRevalidate = (a?: boolean): this => {
+    this._mustRevalidate = a;
+    return this;
+  };
+
+  readonly private = (a?: boolean): this => {
+    this._private = a;
+    return this;
+  };
+
+  readonly public = (a?: boolean): this => {
+    this._public = a;
+    return this;
+  };
+
+  readonly immutable = (a?: boolean): this => {
+    this._immutable = a;
+    return this;
+  };
+
+  readonly staleWhileRevalidate = (a?: number): this => {
+    this._staleWhileRevalidate = a;
     return this;
   };
 
   value = (): string => {
-    const swr = isDefined(this.stale) ? `, stale-while-revalidate=${this.stale}` : '';
-    return this.enabled ? `max-age=${this.age}${swr}` : '';
+    const directives: string[] = [];
+    isDefined(this._maxAge) && directives.push(`max-age=${this._maxAge}`);
+    isDefined(this._sharedMaxAge) && directives.push(`s-maxage=${this._sharedMaxAge}`);
+    this._noCache === true && directives.push(`no-cache`);
+    this._mustRevalidate === true && directives.push(`must-revalidate`);
+    this._private === true && directives.push(`private`);
+    this._public === true && directives.push(`public`);
+    this._immutable === true && directives.push(`immutable`);
+    isDefined(this._staleWhileRevalidate) && directives.push(`stale-while-revalidate=${this._staleWhileRevalidate}`);
+    return directives.join(',');
   };
 
   name = 'Cache-Control';
--- packages/easy/test/http/CacheControl.test.ts
+++ packages/easy/test/http/CacheControl.test.ts
@@ -3,20 +3,17 @@ import '@thisisagile/easy-test';
 
 describe('CacheControl', () => {
   test('CacheControl custom', () => {
-    expect(CacheControl.custom(-3).value()).toBe('max-age=3');
     expect(CacheControl.custom(5).value()).toBe('max-age=5');
-    expect(CacheControl.custom(5).value()).toBe('max-age=5');
-    expect(CacheControl.custom(-1, -3).value()).toBe('max-age=1, stale-while-revalidate=3');
-    expect(CacheControl.custom(2, 3).value()).toBe('max-age=2, stale-while-revalidate=3');
+    expect(CacheControl.custom(2, 3).value()).toBe('max-age=2,stale-while-revalidate=3');
     expect(CacheControl.custom(2, 3).maxAge(1).staleWhileRevalidate().value()).toBe('max-age=1');
   });
 
   test('CacheControl non custom', () => {
-    expect(CacheControl.OneSecond().value()).toBe('max-age=1, stale-while-revalidate=1');
-    expect(CacheControl.fiveSeconds().value()).toBe('max-age=5, stale-while-revalidate=5');
-    expect(CacheControl.tenSeconds().value()).toBe('max-age=10, stale-while-revalidate=10');
-    expect(CacheControl.thirtySeconds().value()).toBe('max-age=30, stale-while-revalidate=30');
-    expect(CacheControl.sixtySeconds().value()).toBe('max-age=60, stale-while-revalidate=60');
+    expect(CacheControl.OneSecond().value()).toBe('max-age=1,stale-while-revalidate=1');
+    expect(CacheControl.fiveSeconds().value()).toBe('max-age=5,stale-while-revalidate=5');
+    expect(CacheControl.tenSeconds().value()).toBe('max-age=10,stale-while-revalidate=10');
+    expect(CacheControl.thirtySeconds().value()).toBe('max-age=30,stale-while-revalidate=30');
+    expect(CacheControl.sixtySeconds().value()).toBe('max-age=60,stale-while-revalidate=60');
     expect(CacheControl.disabled().value()).toBe('');
   });
 
@@ -26,6 +23,19 @@ describe('CacheControl', () => {
     expect(CacheControl.tenSeconds().enabled).toBeTruthy();
     expect(CacheControl.custom(2, 3).enabled).toBeTruthy();
     expect(CacheControl.disabled().enabled).toBeFalsy();
+  });
+
+  test('directives', () => {
+    expect(CacheControl.custom().sharedMaxAge(42).value()).toBe('s-maxage=42');
+    expect(CacheControl.custom().noCache(true).value()).toBe('no-cache');
+    expect(CacheControl.custom().mustRevalidate(true).value()).toBe('must-revalidate');
+    expect(CacheControl.custom().private(true).value()).toBe('private');
+    expect(CacheControl.custom().public(true).value()).toBe('public');
+    expect(CacheControl.custom().immutable(true).value()).toBe('immutable');
+  });
+
+  test('default value is empty', () => {
+    expect(CacheControl.custom().value()).toBe('');
   });
 
   test('name', () => {
