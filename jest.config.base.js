27d011aee95d1bd961b747ac7584d1a77e3f398c
woutervanbakel <wouter.vb@gmail.com>
2022-09-26 12:10:45 +0200

    fix: Update to the latest major of Jest and related packages

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,15 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   roots: ['<rootDir>/test'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
   reporters: ['default', 'jest-junit'],
   testResultsProcessor: 'jest-sonar-reporter',
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'cobertura', 'text-summary'],
   setupFilesAfterEnv: ['../../test/init.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
 };
