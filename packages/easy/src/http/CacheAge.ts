bbb2c41022400fbeac025e35722aa05c1420cf90
aahoogendoorn <aahoogendoorn@gmail.com>
2022-11-03 09:16:16 +0100

    fix(http): adding CacheAge (2/), toSeconds

--- packages/easy/src/http/CacheAge.ts
+++ packages/easy/src/http/CacheAge.ts
@@ -28,3 +28,27 @@ export const toMilliseconds = (ca: CacheAge): number =>
         )
         .else(ca as number);
 
+export const toSeconds = (ca: CacheAge): number =>
+    choose(asString(ca))
+        .case(
+            c => c.endsWith('ms'),
+            c => ageNumber(c) / 1000
+        )
+        .case(
+            c => c.endsWith('s'),
+            c => ageNumber(c)
+        )
+        .case(
+            c => c.endsWith('m'),
+            c => ageNumber(c) * 60
+        )
+        .case(
+            c => c.endsWith('h'),
+            c => ageNumber(c) * 3600
+        )
+        .case(
+            c => c.endsWith('d'),
+            c => ageNumber(c) * 24 * 3600
+        )
+        .else(ca as number);
+
--- packages/easy/test/http/CacheAge.test.ts
+++ packages/easy/test/http/CacheAge.test.ts
@@ -1,4 +1,4 @@
-import {asNumber, CacheAge, toMilliseconds} from '../../src';
+import {asNumber, CacheAge, toMilliseconds, toSeconds} from '../../src';
 import '@thisisagile/easy-test';
 
 describe('CacheAge', () => {
@@ -12,5 +12,16 @@ describe('CacheAge', () => {
         expect(toMilliseconds('3m')).toBe(180000);
         expect(toMilliseconds('2h')).toBe(7200000);
         expect(toMilliseconds('1d')).toBe(86400000);
+    });
+
+    test('toSeconds works', () => {
+        expect(asNumber('10s')).toBe(10);
+        expect(toSeconds(0)).toBe(0);
+        expect(toSeconds(10)).toBe(10);
+        expect(toSeconds('1000ms')).toBe(1);
+        expect(toSeconds('10s')).toBe(10);
+        expect(toSeconds('3m')).toBe(180);
+        expect(toSeconds('2h')).toBe(7200);
+        expect(toSeconds('1d')).toBe(86400);
     });
 });
