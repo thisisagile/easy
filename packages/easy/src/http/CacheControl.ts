9c4ba6bf4f85edc07abc21a07e54ee977d27b0a9
aahoogendoorn <aahoogendoorn@gmail.com>
2022-11-05 12:48:48 +0100

    fix(types): minifying CacheControl.ts (2/)

--- packages/easy/src/http/CacheControl.ts
+++ packages/easy/src/http/CacheControl.ts
@@ -1,18 +1,9 @@
-import {isNumber, meta} from '../types';
+import {isNumber, meta, on} from '../types';
 import {cacheAge, CacheAge} from "./CacheAge";
 import {ifDefined} from "../utils";
 
 export class CacheControl {
     name = 'Cache-Control';
-
-    // protected _maxAge?: number;
-    // protected _sharedMaxAge?: number;
-    // protected _noCache?: boolean;
-    // protected _mustRevalidate?: boolean;
-    // protected _private?: boolean;
-    // protected _public?: boolean;
-    // protected _immutable?: boolean;
-    // protected _staleWhileRevalidate?: number;
 
     protected constructor(readonly enabled = true, private directives: Record<string, boolean | number | undefined> = {}) {
     }
@@ -31,65 +22,25 @@ export class CacheControl {
 
     static custom = (maxAge?: CacheAge, staleWhileRevalidate?: CacheAge) => new CacheControl().maxAge(maxAge).staleWhileRevalidate(staleWhileRevalidate);
 
-    readonly maxAge = (ca?: CacheAge): this => {
-        // this._maxAge = ca ? cacheAge.toSeconds(ca) : ca;
-        this.directives['max-age'] = ca && cacheAge.toSeconds(ca);
-        return this;
-    };
+    readonly maxAge = (ca?: CacheAge): this => on(this, t => t.directives['max-age'] = ca && cacheAge.toSeconds(ca));
 
-    readonly sharedMaxAge = (ca?: CacheAge): this => {
-        // this._sharedMaxAge = ca ? cacheAge.toSeconds(ca) : ca;
-        this.directives['s-maxage'] = ca && cacheAge.toSeconds(ca);
-        return this;
-    };
+    readonly sharedMaxAge = (ca?: CacheAge): this => on(this, t=> t.directives['s-maxage'] = ca && cacheAge.toSeconds(ca));
 
-    readonly noCache = (a?: boolean): this => {
-        // this._noCache = a;
-        this.directives['no-cache'] = a;
-        return this;
-    };
+    readonly noCache = (a?: boolean): this => on(this, t => t.directives['no-cache'] = a);
 
-    readonly mustRevalidate = (a?: boolean): this => {
-        // this._mustRevalidate = a;
-        this.directives['must-revalidate'] = a;
-        return this;
-    };
+    readonly mustRevalidate = (a?: boolean): this => on(this, t => t.directives['must-revalidate'] = a);
 
-    readonly private = (a?: boolean): this => {
-        // this._private = a;
-        this.directives['private'] = a;
-        return this;
-    };
+    readonly private = (a?: boolean): this => on(this, t => t.directives['private'] = a);
 
-    readonly public = (a?: boolean): this => {
-        // this._public = a;
-        this.directives['public'] = a;
-        return this;
-    };
+    readonly public = (a?: boolean): this => on(this, t => t.directives['public'] = a);
 
-    readonly immutable = (a?: boolean): this => {
-        // this._immutable = a;
-        this.directives['immutable'] = a;
-        return this;
-    };
+    readonly immutable = (a?: boolean): this => on(this, t => t.directives['immutable'] = a);
 
-    readonly staleWhileRevalidate = (ca?: CacheAge): this => {
-        // this._staleWhileRevalidate = ca ? cacheAge.toSeconds(ca) : ca;
-        this.directives['stale-while-revalidate'] = ca && cacheAge.toSeconds(ca);
-        return this;
-    };
+    readonly staleWhileRevalidate = (ca?: CacheAge): this => on(this, t => t.directives['stale-while-revalidate'] = ca && cacheAge.toSeconds(ca));
 
-    value = (): string => {
-        // const directives: string[] = [];
-        // isDefined(this._maxAge) && directives.push(`max-age=${this._maxAge}`);
-        // isDefined(this._sharedMaxAge) && directives.push(`s-maxage=${this._sharedMaxAge}`);
-        // this._noCache === true && directives.push(`no-cache`);
-        // this._mustRevalidate === true && directives.push(`must-revalidate`);
-        // this._private === true && directives.push(`private`);
-        // this._public === true && directives.push(`public`);
-        // this._immutable === true && directives.push(`immutable`);
-        // isDefined(this._staleWhileRevalidate) && directives.push(`stale-while-revalidate=${this._staleWhileRevalidate}`);
-        // return directives.join(',');
+    value = (): string => this.toString();
+
+    toString(): string {
         return meta(this.directives).entries().mapDefined(([k, v]) => ifDefined(v, isNumber(v) ? `${k}=${v}` : k)).join(',');
-    };
+    }
 }
