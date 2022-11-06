df7555c84364ed73cbb05408d5163908aeecd5bb
aahoogendoorn <aahoogendoorn@gmail.com>
2022-11-06 11:29:51 +0100

    fix(types): updating RouteGateway.ts with generic options type.

--- packages/easy/src/services/RouteGateway.ts
+++ packages/easy/src/services/RouteGateway.ts
@@ -1,81 +1,83 @@
-import { Api } from './Api';
-import { Filter, Func, Gateway, Id, Json, JsonValue, PageList, PageOptions, toPageList, Uri } from '../types';
-import { HttpStatus, RequestOptions, toPageOptions } from '../http';
+import {Api} from './Api';
+import {FetchOptions, Filter, Func, Gateway, Id, Json, JsonValue, PageList, toPageList, Uri} from '../types';
+import {HttpStatus, RequestOptions, toPageOptions} from '../http';
 
-export class RouteGateway extends Gateway {
-  constructor(readonly route: Func<Uri>, readonly routeId: Func<Uri>, readonly api: Api = new Api()) {
-    super();
-  }
+export type RouteOptions = RequestOptions | FetchOptions;
 
-  get(uri: Uri, options?: RequestOptions | PageOptions): Promise<PageList<Json>> {
-    return this.api.get(uri, options).then(r => toPageList<Json>(r.body.data?.items, toPageOptions(options) && {
-      total: r.body.data?.totalItems,
-      filters: r.body.data?.meta?.filters as Filter[],
-    }));
-  }
+export class RouteGateway extends Gateway<RouteOptions> {
+    constructor(readonly route: Func<Uri>, readonly routeId: Func<Uri>, readonly api: Api = new Api()) {
+        super();
+    }
 
-  getOne(uri: Uri, options?: RequestOptions): Promise<Json | undefined> {
-    return this.get(uri, options).then(r => r.first());
-  }
+    get(uri: Uri, options?: RouteOptions): Promise<PageList<Json>> {
+        return this.api.get(uri, options).then(r => toPageList<Json>(r.body.data?.items, toPageOptions(options) && {
+            total: r.body.data?.totalItems,
+            filters: r.body.data?.meta?.filters as Filter[],
+        }));
+    }
 
-  all(options?: PageOptions): Promise<PageList<Json>> {
-    return this.get(this.route(), options);
-  }
+    getOne(uri: Uri, options?: RouteOptions): Promise<Json | undefined> {
+        return this.get(uri, options).then(r => r.first());
+    }
 
-  byId(id: Id): Promise<Json | undefined> {
-    return this.getOne(this.routeId().id(id));
-  }
+    all(options?: RouteOptions): Promise<PageList<Json>> {
+        return this.get(this.route(), options);
+    }
 
-  search(q: JsonValue, options?: PageOptions): Promise<PageList<Json>> {
-    return this.get(this.route().query(q), options);
-  }
+    byId(id: Id, options?: RouteOptions): Promise<Json | undefined> {
+        return this.getOne(this.routeId().id(id));
+    }
 
-  exists(id: Id): Promise<boolean> {
-    return this.get(this.routeId().id(id))
-      .then(r => r.length === 1)
-      .catch(r => (HttpStatus.NotFound.equals(r.status) ? false : Promise.reject(r)));
-  }
+    search(q: JsonValue, options?: RouteOptions): Promise<PageList<Json>> {
+        return this.get(this.route().query(q), options);
+    }
 
-  post(uri: Uri, item?: Json): Promise<Json> {
-    return this.api.post(uri, item).then(r => r.body.data?.items.first() ?? {});
-  }
+    exists(id: Id, options?: RouteOptions): Promise<boolean> {
+        return this.get(this.routeId().id(id))
+            .then(r => r.length === 1)
+            .catch(r => (HttpStatus.NotFound.equals(r.status) ? false : Promise.reject(r)));
+    }
 
-  postSearch(uri: Uri, options?: RequestOptions | PageOptions): Promise<PageList<Json>> {
-    return this.api.post(uri, options).then(r => toPageList<Json>(r.body.data?.items, toPageOptions(options) && {
-      total: r.body.data?.totalItems,
-      filters: r.body.data?.meta?.filters as Filter[],
-    }));
-  }
+    add(item: Json, options?: RouteOptions): Promise<Json> {
+        return this.post(this.route(), item);
+    }
 
-  add(item: Json): Promise<Json> {
-    return this.post(this.route(), item);
-  }
+    filter(options?: RouteOptions): Promise<PageList<Json>> {
+        return this.postSearch(this.route(), options);
+    }
 
-  filter(options?: RequestOptions | PageOptions): Promise<PageList<Json>> {
-    return this.postSearch(this.route(), options);
-  }
+    update(item: Json, options?: RouteOptions): Promise<Json> {
+        return this.patch(this.routeId().id(item.id), item);
+    }
 
-  update(item: Json): Promise<Json> {
-    return this.patch(this.routeId().id(item.id), item);
-  }
+    upsert(item: Json, options?: RouteOptions): Promise<Json> {
+        return this.put(this.routeId().id(item.id), item);
+    }
 
-  patch(uri: Uri, item: Json): Promise<Json> {
-    return this.api.patch(uri, item).then(r => r.body.data?.items.first() ?? {});
-  }
+    remove(id: Id, options?: RouteOptions): Promise<boolean> {
+        return this.delete(this.routeId().id(id));
+    }
 
-  upsert(item: Json): Promise<Json> {
-    return this.put(this.routeId().id(item.id), item);
-  }
+    post(uri: Uri, item?: Json): Promise<Json> {
+        return this.api.post(uri, item).then(r => r.body.data?.items.first() ?? {});
+    }
 
-  put(uri: Uri, item: Json): Promise<Json> {
-    return this.api.put(uri, item).then(r => r.body.data?.items.first() ?? {});
-  }
+    postSearch(uri: Uri, options?: RequestOptions | FetchOptions): Promise<PageList<Json>> {
+        return this.api.post(uri, options).then(r => toPageList<Json>(r.body.data?.items, toPageOptions(options) && {
+            total: r.body.data?.totalItems,
+            filters: r.body.data?.meta?.filters as Filter[],
+        }));
+    }
 
-  remove(id: Id): Promise<boolean> {
-    return this.delete(this.routeId().id(id));
-  }
+    patch(uri: Uri, item: Json): Promise<Json> {
+        return this.api.patch(uri, item).then(r => r.body.data?.items.first() ?? {});
+    }
 
-  delete(uri: Uri): Promise<boolean> {
-    return this.api.delete(uri).then(() => true);
-  }
+    put(uri: Uri, item: Json): Promise<Json> {
+        return this.api.put(uri, item).then(r => r.body.data?.items.first() ?? {});
+    }
+
+    delete(uri: Uri, options?: RouteOptions): Promise<boolean> {
+        return this.api.delete(uri).then(() => true);
+    }
 }
