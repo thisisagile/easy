7b238cdb95e7324f62f4babe852ad085beb2b406
aahoogendoorn <aahoogendoorn@gmail.com>
2022-11-08 17:41:03 +0100

    fix(domain): refactoring repositories (5/)

---
+++ packages/easy/test/ref/DevTypo.ts
@@ -0,0 +1,27 @@
+import { asString, isA, RouteGateway, Typo, View, view, views } from '../../src';
+import { DevUri } from './DevUri';
+import { Dev } from './Dev';
+
+const { keep } = views;
+
+export type DevType = { name: string; level: string; certificates: [] };
+
+export const isDevType = (d?: unknown): d is DevType => isA<DevType>(d, 'name', 'level', 'certificates');
+
+export const developers = view<DevType>({
+  name: keep,
+  level: d => asString(d.level),
+  certificates: (d: Dev) => d.certificates.map(c => c.id),
+});
+
+export class DevTypo extends Typo<DevType> {
+  constructor(
+    readonly developers: View,
+    readonly devs = new RouteGateway(
+      () => DevUri.Developers,
+      () => DevUri.Developer
+    )
+  ) {
+    super(developers, devs);
+  }
+}
