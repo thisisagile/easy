6c9daa418dfaba0b7e8301f205983c8f38071448
aahoogendoorn <aahoogendoorn@gmail.com>
2022-11-08 17:47:04 +0100

    fix(types): Updating cache.

--- packages/easy/src/types/Cache.ts
+++ packages/easy/src/types/Cache.ts
@@ -1,6 +1,7 @@
 import { Func } from './Func';
 import { CacheAge } from './CacheAge';
 import { Construct } from './Constructor';
+import { meta } from './Meta';
 
 export interface Store<To = any, From = any> {
   execute: (fetch: From, f: Func<Promise<To>, From>) => Promise<To>;
@@ -10,3 +11,14 @@ export type CacheOptions = {
   expiresIn?: CacheAge;
   store?: Construct<Store>;
 };
+
+export const toCacheOptions = (co: Partial<CacheOptions>): CacheOptions => ({
+  expiresIn: co.expiresIn ?? '5m',
+  store: co.store,
+});
+
+export const cache =
+  (options: Partial<CacheOptions>): ClassDecorator =>
+  (subject: unknown): void => {
+    meta(subject).set('cache', toCacheOptions(options));
+  };
