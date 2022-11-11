7f36a6c3d51995f660d7179f762c49f38dfd558d
aahoogendoorn <aahoogendoorn@gmail.com>
2022-11-11 19:10:43 +0100

    fix: removed sepearate NoRequestStore

--- packages/easy/src/http/NoRequestStore.ts
+++
@@ -1,9 +0,0 @@
-import { Func, Store } from '../types';
-import { Request } from './Request';
-import { Response } from './Response';
-
-export class NoRequestStore implements Store<Response, Request> {
-  execute(req: Request, f: Func<Promise<Response>, Request>): Promise<Response> {
-    return f(req);
-  }
-}
