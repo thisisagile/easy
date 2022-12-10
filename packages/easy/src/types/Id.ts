a78c5f9c980e12082fdb90bcb9bb3573a2278254
aahoogendoorn <aahoogendoorn@gmail.com>
2022-12-10 13:13:11 +0100

    fix(types): added HasId type.

--- packages/easy/src/types/Id.ts
+++ packages/easy/src/types/Id.ts
@@ -3,5 +3,6 @@ import { toUuid } from './Uuid';
 export type Id = string | number;
 export type Key = Id;
 export type Code = Id;
+export type HasId = { id: Id };
 
 export const toId = (): Id => toUuid();
