73cd22bc6c9a7fb99e7883aa84e6734adb3c6ef6
aahoogendoorn <aahoogendoorn@gmail.com>
2023-01-03 18:29:55 +0100

    fix(types): Added Primitive. (3/)

--- packages/easy/test/types/Primitive.test.ts
+++ packages/easy/test/types/Primitive.test.ts
@@ -2,16 +2,18 @@ import { isPrimitive } from '../../src';
 import { Dev } from '../ref';
 
 describe('isPrimitive', () => {
-  test('Check', () => {
+  const valids = [null, '', 'Hi', 123, true, false, undefined];
+  const invalids = [{}, Dev, Dev.Eugen, () => true, []];
+
+  test('Check with nothing', () => {
     expect(isPrimitive()).toBeTruthy();
-    expect(isPrimitive(null)).toBeTruthy();
-    expect(isPrimitive('')).toBeTruthy();
-    expect(isPrimitive({})).toBeFalsy();
-    expect(isPrimitive(123)).toBeTruthy();
-    expect(isPrimitive(false)).toBeTruthy();
-    expect(isPrimitive(Dev)).toBeFalsy();
-    expect(isPrimitive(Dev.Wouter)).toBeFalsy();
-    expect(isPrimitive(() => true)).toBeFalsy();
-    expect(isPrimitive([])).toBeFalsy();
+  });
+
+  test.each(valids)('%s is a valid primitive', v => {
+    expect(isPrimitive(v)).toBeTruthy();
+  });
+
+  test.each(invalids)('%s is not a valid primitive', v => {
+    expect(isPrimitive(v)).toBeFalsy();
   });
 });
