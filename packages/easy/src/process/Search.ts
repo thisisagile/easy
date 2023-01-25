2f54187de67d3332d73166ea8eebe16092f34517
aahoogendoorn <aahoogendoorn@gmail.com>
2023-01-25 22:29:51 +0100

    fix(types): Added Options to the generic parameters of a Search.ts.

--- packages/easy/src/process/Search.ts
+++ packages/easy/src/process/Search.ts
@@ -1,22 +1,21 @@
-import { choose, Id, JsonValue, Key, PageList, PageOptions, toList } from '../types';
+import { choose, FetchOptions, Id, JsonValue, Key, PageList, Repository, toList } from '../types';
 import { resolve } from '../utils';
 import { Req } from '../resources';
-import { Repository } from '../types';
 
-export class Search<T> {
-  constructor(protected repo: Repository<T>) {}
+export class Search<T, Options = FetchOptions> {
+  constructor(protected repo: Repository<T, Options>) {}
 
-  all = (options?: PageOptions): Promise<PageList<T>> => this.repo.all(options);
+  all = (options?: Options): Promise<PageList<T>> => this.repo.all(options);
 
   byId = (id: Id): Promise<T> => this.repo.byId(id);
 
   byIds = (...ids: Id[]): Promise<PageList<T>> => this.repo.byIds(...ids);
 
-  byKey = (key: Key, options?: PageOptions): Promise<PageList<T>> => this.repo.byKey(key, options);
+  byKey = (key: Key, options?: Options): Promise<PageList<T>> => this.repo.byKey(key, options);
 
-  query = ({ query, skip, take }: Req): Promise<PageList<T>> => this.search(query, { skip, take });
+  query = ({ query, skip, take }: Req): Promise<PageList<T>> => this.search(query, { skip, take } as Options);
 
-  search = (query: JsonValue, options?: PageOptions): Promise<PageList<T>> =>
+  search = (query: JsonValue, options?: Options): Promise<PageList<T>> =>
     choose(query)
       .is.not.empty(
         q => q,
@@ -24,7 +23,7 @@ export class Search<T> {
       )
       .else(() => resolve(toList<T>()));
 
-  filter = (options?: PageOptions): Promise<PageList<T>> => this.repo.filter(options);
+  filter = (options?: Options): Promise<PageList<T>> => this.repo.filter(options);
 
   exists = (id: Id): Promise<boolean> => this.repo.exists(id);
 }
