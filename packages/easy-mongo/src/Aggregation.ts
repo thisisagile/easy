798d16ee3aa097a4849839113c80b84089fbba57
aahoogendoorn <aahoogendoorn@gmail.com>
2023-03-08 18:04:32 +0100

    fix(mongo): adding a more simple aggregation function for MongoDB aggregation pipelines (now first date parsing function called date).

--- packages/easy-mongo/src/Aggregation.ts
+++ packages/easy-mongo/src/Aggregation.ts
@@ -1,4 +1,4 @@
-import { Id, ifDefined, isPresent, Optional } from '@thisisagile/easy';
+import { ifDefined, isPresent, Optional } from '@thisisagile/easy';
 import { Filter, FindOptions } from './MongoProvider';
 
 export const asc = 1;
@@ -6,17 +6,19 @@ export const desc = -1;
 
 export const aggregation = {
   match: (f: Filter) => ({ $match: f }),
-  id: (id: Id) => aggregation.match({ id }),
-  eq: (key: string, value: Id | Filter) => aggregation.match({ [key]: value }),
-  gt: (key: string, value: Id) => ({ [key]: { $gt: value } }),
-  gte: (key: string, value: Id) => ({ [key]: { $gte: value } }),
-  lt: (key: string, value: Id) => ({ [key]: { $lt: value } }),
-  lte: (key: string, value: Id) => ({ [key]: { $lte: value } }),
+  id: (id: Filter) => aggregation.match({ id }),
+  eq: (key: string, value: Filter) => aggregation.match({ [key]: value }),
+  gt: (key: string, value: Filter) => ({ [key]: { $gt: value } }),
+  gte: (key: string, value: Filter) => ({ [key]: { $gte: value } }),
+  lt: (key: string, value: Filter) => ({ [key]: { $lt: value } }),
+  lte: (key: string, value: Filter) => ({ [key]: { $lte: value } }),
   sum: (to: string, from: string = to) => ({ [to]: { $sum: `$${from}` } }),
-  group: (id: Id, g: Filter) => ({ $group: { _id: id, ...g } }),
+  count: (to = 'count') => ({ [to]: { $count: {} } }),
+  group: (by: Filter, ...fs: Filter[]) => ({ $group: Object.assign({ _id: by }, ...fs) }),
   skip: ({ skip: $skip }: FindOptions): Optional<Filter> => ifDefined($skip, { $skip }),
   take: ({ take: $limit }: FindOptions): Optional<Filter> => ifDefined($limit, { $limit }),
   sort: ($sort: Record<string, typeof asc | typeof desc>): Optional<Filter> => (isPresent($sort) ? { $sort } : undefined),
   asc: (key: string) => aggregation.sort({ [key]: asc }),
   desc: (key: string) => aggregation.sort({ [key]: desc }),
+  date: (key: string, format = '%Y-%m-%d') => ({ $dateToString: { date: `$${key}`, format } }),
 };
--- packages/easy-mongo/test/Aggregation.test.ts
+++ packages/easy-mongo/test/Aggregation.test.ts
@@ -1,10 +1,11 @@
 import '@thisisagile/easy-test';
 import { aggregation } from '../src';
+import { toArray } from '@thisisagile/easy';
 
 describe('Aggregation', () => {
   const name = { name: 'Sander' };
   const options = { skip: 10, take: 5 };
-  const { id, eq, gt, lt, gte, lte, match, sum, group, skip, take, sort, asc, desc } = aggregation;
+  const { id, eq, gt, lt, gte, lte, date, match, sum, count, group, skip, take, sort, asc, desc } = aggregation;
 
   test('id', () => {
     expect(id(42)).toMatchObject({ $match: { id: 42 } });
@@ -21,6 +22,7 @@ describe('Aggregation', () => {
 
   test('gt', () => {
     expect(gt('prio', 10)).toMatchObject({ prio: { $gt: 10 } });
+    expect(match(gt('prio', 10))).toMatchObject({ $match: { prio: { $gt: 10 } } });
   });
 
   test('gte', () => {
@@ -40,9 +42,37 @@ describe('Aggregation', () => {
     expect(sum('total')).toMatchObject({ total: { $sum: '$total' } });
   });
 
+  test('count', () => {
+    expect(count('total')).toMatchObject({ total: { $count: {} } });
+  });
+
+  test('date', () => {
+    expect(date('lastModified.when')).toMatchObject({ $dateToString: { date: '$lastModified.when', format: '%Y-%m-%d' } });
+  });
+
+  test('dsdsdsad', () => {
+    const c = Object.assign({ _id: 'hoi' }, ...toArray([{ x: 1 }, { y: 2 }]));
+    expect(c).toMatchObject({ _id: 'hoi', x: 1, y: 2 });
+  });
+
   test('group', () => {
     const g = group('brandId', sum('total', 'count'));
     expect(g).toMatchObject({ $group: { _id: 'brandId', total: { $sum: '$count' } } });
+    const g2 = group(date('created.when'), count());
+    expect(g2).toMatchObject({
+      $group: {
+        _id: { $dateToString: { date: '$created.when', format: '%Y-%m-%d' } },
+        count: { $count: {} },
+      },
+    });
+    const g3 = group(date('created.when'), count(), sum('total'));
+    expect(g3).toMatchObject({
+      $group: {
+        _id: { $dateToString: { date: '$created.when', format: '%Y-%m-%d' } },
+        count: { $count: {} },
+        total: { $sum: '$total' },
+      },
+    });
   });
 
   test('skip', () => {
