ee17e3007cb32b533ddfca7579346a34b150fa9b
Jeroen Plug <jeroen.plug@gmail.com>
2023-04-07 15:54:28 +0200

    fix: Don't try to run jest in easy-vitest

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const projects = fs
   .readdirSync('packages/', { withFileTypes: true })
-  .filter(c => c.isDirectory())
+  .filter(c => c.isDirectory() && c.name !== 'easy-vitest')
   .map(c => ({...require(`./packages/${c.name}/jest.config.js`), rootDir: `./packages/${c.name}`}));
 
 module.exports = {
