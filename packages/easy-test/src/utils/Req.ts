510aa7e26ccd51583b9c26ec606981ce63f93c83
aahoogendoorn <aahoogendoorn@gmail.com>
2023-04-17 14:12:56 +0200

    fix(easy-test): added tests for asNumber(). (2/)

--- packages/easy-test/src/utils/Req.ts
+++ packages/easy-test/src/utils/Req.ts
@@ -1,31 +1,32 @@
-import { asNumber, ifDefined } from './Utils';
-import { Id, Json, JsonValue, Text } from './Types';
+import {asNumber, isDefined} from './Utils';
+import {Id, Json, JsonValue, Text} from './Types';
 
 export class Req {
-  constructor(readonly state: any = {}) {}
+    readonly skip = isDefined(this.query?.skip) ? asNumber(this.query?.skip) : undefined;
+    readonly take = isDefined(this.query?.take) ? asNumber(this.query?.take) : undefined;
 
-  get id(): Id {
-    return this.state.id ?? this.path.id;
-  }
+    constructor(readonly state: any = {}) {
+    }
 
-  get q(): JsonValue {
-    return this.state.q ?? this.query.q;
-  }
+    get id(): Id {
+        return this.state.id ?? this.path.id;
+    }
 
-  readonly skip = ifDefined(this.query?.skip, asNumber(this.query?.skip));
-  readonly take = ifDefined(this.query?.take, asNumber(this.query?.take));
+    get q(): JsonValue {
+        return this.state.q ?? this.query.q;
+    }
 
-  get path(): Json {
-    return this.state?.path ?? {};
-  }
+    get path(): Json {
+        return this.state?.path ?? {};
+    }
 
-  get query(): Json {
-    return this.state?.query ?? {};
-  }
+    get query(): Json {
+        return this.state?.query ?? {};
+    }
 
-  get body(): unknown {
-    return this.state.body;
-  }
+    get body(): unknown {
+        return this.state.body;
+    }
 
-  get = (key: Text): any => this?.state[key.toString()] ?? this.path[key.toString()] ?? this.query[key.toString()];
+    get = (key: Text): any => this?.state[key.toString()] ?? this.path[key.toString()] ?? this.query[key.toString()];
 }
