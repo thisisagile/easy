da05f0c65e31628c12282d50ec112aec69c096e0
aahoogendoorn <aahoogendoorn@gmail.com>
2023-04-17 14:11:32 +0200

    fix(easy-test): removing dependency on easy from easy-test.

--- packages/easy-test/src/utils/Utils.ts
+++ packages/easy-test/src/utils/Utils.ts
@@ -1,8 +1,8 @@
-import { Construct, Get, isNullish, isNumber, ofConstruct, ofGet, tryTo } from '@thisisagile/easy';
+import {Get, ofGet, tryTo} from '@thisisagile/easy';
 
-export const isDefined = (o?: unknown): boolean => !isNullish(o);
-export const ifDefined = <T>(o: unknown, f: Construct<T>, alt?: Construct<T>): T | undefined => (isDefined(o) ? ofConstruct(f, o) : ofConstruct(alt, o));
+export const isDefined = <T = unknown>(o?: T): boolean => o !== undefined && o !== null;
 
+export const isNumber = (o?: unknown): o is number => isDefined(o) && typeof o === 'number' && !Number.isNaN(o);
 export const isFunction = (o?: unknown): o is (...params: unknown[]) => unknown => isDefined(o) && typeof o === 'function';
 
 export const isA = <T>(t?: unknown, ...properties: (keyof T)[]): t is T => isDefined(t) && properties.every(p => isDefined((t as T)[p]));
@@ -12,10 +12,10 @@ export const isArray = <T = any>(o?: unknown): o is Array<T> => isDefined(o) && 
 export const isObject = (o?: unknown): o is Record<string, unknown> => o != null && (typeof o === 'object' || typeof o === 'function') && !isArray(o);
 
 export const asJson = (a?: unknown): any => ((a as any)?.toJSON ? (a as any).toJSON() : isObject(a) ? a : undefined);
-export const asString = (a?: unknown): any => (a as any)?.toString();
+export const asString = (a?: unknown): string => (a as any)?.toString();
 
 export const asNumber = (n: unknown, alt?: Get<number>): number =>
-  tryTo(() => asString(n))
-    .map(s => parseInt(s))
-    .filter(n => isNumber(n))
-    .or(ofGet(alt) ?? NaN);
+    tryTo(() => asString(n))
+        .map(s => parseInt(s))
+        .filter(n => isNumber(n))
+        .or(ofGet(alt) ?? NaN);
