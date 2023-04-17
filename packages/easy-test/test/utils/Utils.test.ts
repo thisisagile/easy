741cb2785255b36923237d018e6f106643a0c3c1
aahoogendoorn <aahoogendoorn@gmail.com>
2023-04-17 14:05:21 +0200

    fix(easy-test): added tests for asNumber().

--- packages/easy-test/test/utils/Utils.test.ts
+++ packages/easy-test/test/utils/Utils.test.ts
@@ -1,11 +1,11 @@
-import { asJson } from '../../src/utils/Utils';
+import { asJson, asNumber } from '../../src/utils/Utils';
 
 class Dev {
   constructor(readonly name: string) {}
 }
 
 describe('asJson', () => {
-  test('works', () => {
+  test('asJson', () => {
     expect(asJson()).toBeUndefined();
     expect(asJson('')).toBeUndefined();
     expect(asJson(3)).toBeUndefined();
@@ -13,4 +13,20 @@ describe('asJson', () => {
     expect(asJson({ id: 3 })).toMatchObject({ id: 3 });
     expect(asJson(new Dev('Wouter'))).toMatchObject({ name: 'Wouter' });
   });
+
+  test('asNumber', () => {
+    expect(asNumber(undefined)).toBeNaN();
+    expect(asNumber(undefined, 3)).toBe(3);
+    expect(asNumber('')).toBeNaN();
+    expect(asNumber('', 3)).toBe(3);
+    expect(asNumber({})).toBeNaN();
+    expect(asNumber({}, 3)).toBe(3);
+    expect(asNumber({name: 'Sa'})).toBeNaN();
+    expect(asNumber({name: 'Sa'}, 3)).toBe(3);
+    expect(asNumber(4)).toBe(4);
+    expect(asNumber(4, 3)).toBe(4);
+    expect(asNumber('4')).toBe(4);
+    expect(asNumber('4', 3)).toBe(4);
+  });
+
 });
