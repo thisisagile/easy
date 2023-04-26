b105be7ecf8ed7d5b21c6b27f5d650e712caec09
aahoogendoorn <aahoogendoorn@gmail.com>
2023-04-26 12:21:09 +0200

    fix(easy): updated definition of ifDefined type.

--- packages/easy/src/utils/If.ts
+++ packages/easy/src/utils/If.ts
@@ -2,6 +2,8 @@ import { Construct, isDefined, isNotEmpty, isTrue, ofConstruct, Optional } from 
 
 export const ifTrue = <T>(o: unknown, f: Construct<T>, alt?: Construct<T>): Optional<T> => (isTrue(o) ? ofConstruct(f, o) : ofConstruct(alt, o));
 export const ifFalse = <T>(o: unknown, f: Construct<T>, alt?: Construct<T>): Optional<T> => (!isTrue(o) ? ofConstruct(f, o) : ofConstruct(alt, o));
-export const ifDefined = <T>(o: unknown, f: Construct<T>, alt?: Construct<T>): Optional<T> => (isDefined(o) ? ofConstruct(f, o) : ofConstruct(alt, o));
+// export const ifDefined = <T>(o: unknown, f: Construct<T>, alt?: Construct<T>): Optional<T> => (isDefined(o) ? ofConstruct(f, o) : ofConstruct(alt, o));
+export const ifDefined = <Out, In = unknown>(o: In, f: Construct<Out, NonNullable<In>>, alt?: Construct<Out, In>): Optional<Out> => (isDefined(o) ? ofConstruct(f, o)  : ofConstruct(alt, o));
+
 export const ifNotEmpty = <T>(o: unknown, f: Construct<T> = o => o, alt?: Construct<T>): Optional<T> =>
   isNotEmpty(o) ? ofConstruct(f, o) : ofConstruct(alt, o);
