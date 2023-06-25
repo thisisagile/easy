a9dd04057edaf08db5abd86357eb03678c500ec2
aahoogendoorn <aahoogendoorn@gmail.com>
2023-06-25 15:30:20 +0200

    fix(easy-mongo): improving isIn in Stages.ts.

--- packages/easy-mongo/src/Stages.ts
+++ packages/easy-mongo/src/Stages.ts
@@ -45,7 +45,7 @@ export const stages = {
         gte: (value: Filter) => ({$gte: value}),
         lt: (value: Filter) => ({$lt: value}),
         lte: (value: Filter) => ({$lte: value}),
-        isIn: (value: string | unknown[], separator = ',') => ({$in: isString(value) ? value.split(separator) : value}),
+        isIn: (value: OneOrMore<unknown>, separator = ',') => ({$in: isString(value) ? value.split(separator) : value}),
         after: (date: unknown) => stages.match.gte(toMongoType(date)),
         before: (date: unknown) => stages.match.lt(toMongoType(date)),
         anywhere: (q: string) => ({$regex: escapeRegex(q), $options: 'i'}),
