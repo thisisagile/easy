b3151128e0327df0815ca183695e4a27847346ae
aahoogendoorn <aahoogendoorn@gmail.com>
2023-07-05 13:19:50 +0200

    fix(easy): extended log function to have a label possibly.

--- packages/easy/src/utils/Log.ts
+++ packages/easy/src/utils/Log.ts
@@ -1,4 +1,12 @@
-export const log = <T>(t: T): T => {
-  console.log(t);
-  return t;
-};
+
+export function log<T>(t: T): T;
+export function log<T>(label: string, t: T): T;
+export function log<T>(labelOrT: string | T, t?: T): T {
+  if (t) {
+    console.log(labelOrT as string, t);
+    return t;
+  } else {
+    console.log(labelOrT);
+    return labelOrT as T;
+  }
+}
