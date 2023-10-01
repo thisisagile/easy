011ddcac348962013b600165ec40425ff629db56
aahoogendoorn <aahoogendoorn@gmail.com>
2023-10-01 11:31:54 +0200

    fix(easy): now have a generic Req<T>, where the body is assumed of type T.

--- packages/easy/src/resources/Req.ts
+++ packages/easy/src/resources/Req.ts
@@ -1,10 +1,10 @@
 import { asNumber, Id, Json, JsonValue, OneOrMore, Optional, PageOptions, Text } from '../types';
 import { ifDefined } from '../utils';
 
-export class Req implements Omit<PageOptions, 'sort'> {
+export class Req<T = unknown> implements Omit<PageOptions, 'sort'> {
   readonly skip: Optional<number>;
   readonly take: Optional<number>;
-  constructor(readonly path: Json = {}, readonly query: Json = {}, readonly body: unknown, readonly headers: Record<string, OneOrMore<string>>) {
+  constructor(readonly path: Json = {}, readonly query: Json = {}, readonly body: T, readonly headers: Record<string, OneOrMore<string>>) {
     this.skip = ifDefined(query.skip, s => asNumber(s));
     this.take = ifDefined(query.take, t => asNumber(t));
   }
@@ -20,5 +20,6 @@ export class Req implements Omit<PageOptions, 'sort'> {
   get = (key: Text): any => this.path[key.toString()] ?? this.query[key.toString()];
 }
 
-export const toReq = (req: { params?: { id?: unknown }; query?: { q?: unknown }; body?: unknown; headers?: unknown }): Req =>
-  new Req(req.params as Json, req.query as Json, req.body as Json, req.headers as Record<string, OneOrMore<string>>);
+export function toReq<T = unknown>(req: { params?: { id?: unknown }; query?: { q?: unknown }; body?: T; headers?: unknown }): Req {
+  return new Req(req.params as Json, req.query as Json, req.body as Json, req.headers as Record<string, OneOrMore<string>>);
+}
