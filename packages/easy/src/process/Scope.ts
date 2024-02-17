4d24281b52cfb91696eca202fbf1d44eec606937
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2024-02-17 13:59:33 +0100

    fix: updated Scope with a for function, to allow slightly more dynamic use.

--- packages/easy/src/process/Scope.ts
+++ packages/easy/src/process/Scope.ts
@@ -1,4 +1,4 @@
-import { Enum, Text, text } from '../types';
+import { Enum, IdName, isString, kebab, Text, text } from '../types';
 
 export class Scope extends Enum {
   // static readonly Basic = new Scope('Basic');
@@ -7,5 +7,9 @@ export class Scope extends Enum {
 
   protected constructor(readonly name: string, id: Text = text(name).kebab) {
     super(name, id.toString());
+  }
+
+  for(item: string | IdName): Scope {
+    return new Scope(`${this.name} ${isString(item) ? text(item).title : item.name}`, kebab(`${this.id} ${isString(item) ? item : item.id}`));
   }
 }
--- packages/easy/test/process/Scope.test.ts
+++ packages/easy/test/process/Scope.test.ts
@@ -7,4 +7,18 @@ describe('Scope', () => {
     expect(DevScope.Manager.id).toBe('mngr');
     expect(DevScope.Manager.code).toBe('mngr');
   });
+
+  test('for with a string', () => {
+    const marketing = 'marketing';
+    expect(DevScope.Manager.for(marketing)).toBeInstanceOf(Scope);
+    expect(DevScope.Manager.for(marketing).name).toBe('Manager Marketing');
+    expect(DevScope.Manager.for(marketing).id).toBe('mngr-marketing');
+  });
+
+  test('for with an IdName', () => {
+    const marketing = { id: 'marketing', name: 'Marketing' };
+    expect(DevScope.Manager.for(marketing)).toBeInstanceOf(Scope);
+    expect(DevScope.Manager.for(marketing).name).toBe('Manager Marketing');
+    expect(DevScope.Manager.for(marketing).id).toBe('mngr-marketing');
+  });
 });
