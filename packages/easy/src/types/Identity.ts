8a4261eef4ac2d3c1fdccf31a19621216fa0c101
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2024-02-17 15:06:00 +0100

    fix: reformatted Identity.ts

--- packages/easy/src/types/Identity.ts
+++ packages/easy/src/types/Identity.ts
@@ -1,8 +1,5 @@
 import { Id } from './Id';
 
-export type Identity = {
-  id: Id;
-  user?: string;
-};
+export type Identity = { id: Id; user?: string };
 
 export type IdName = { id: Id; name: string, slug?: string };
