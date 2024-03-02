3aca732e15c3ecaa1c0b3ff81dcfdf726bd632f5
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2024-03-02 11:08:27 +0100

    feat: adding the useOnce.ts hook, that automatically triggers only once, an works both on objects as on lists. (4/)

--- packages/easy-react/test/utils/useOnce.test.tsx
+++ packages/easy-react/test/utils/useOnce.test.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import '@thisisagile/easy-test';
 import { rendersWait } from '@thisisagile/easy-test-react';
-import { resolve } from '@thisisagile/easy';
-import { useOnce } from '../../src';
+import { HasId, resolve } from '@thisisagile/easy';
+import { useOnce, UseOnceOptions } from '../../src';
 
 describe('useOnce', () => {
-  const Club = ({ f, initial }: { f: () => Promise<string>; initial?: string }) => {
-    const [name] = useOnce(f, { initial });
-    return <div data-testid={name}>Club</div>;
+  const Club = ({ f, options = {} }: { f: () => Promise<HasId>; options?: UseOnceOptions<HasId> }) => {
+    const [o] = useOnce(f, options);
+    return <div data-testid={o?.id}>Club</div>;
   };
 
   test('useOnce works.', async () => {
-    const { container, atId } = await rendersWait(<Club f={() => resolve('barcelona')} />);
+    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 42 })} />);
     expect(container).toMatchSnapshot();
-    expect(atId('barcelona')).toBeValid();
+    expect(atId('42')).toBeValid();
   });
 
   test('useOnce works with initial.', async () => {
-    const { container, atId } = await rendersWait(<Club f={() => resolve('barcelona')} initial="feyenoord" />);
+    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 43 })} options={{ initial: { id: 44 } }} />);
     expect(container).toMatchSnapshot();
-    expect(atId('barcelona')).toBeValid();
+    expect(atId('43')).toBeValid();
+  });
+
+  test('useOnce works with deps.', async () => {
+    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 44 })} options={{ deps: [{ id: 44 }] }} />);
+    expect(container).toMatchSnapshot();
+    expect(atId('44')).toBeValid();
   });
 });
