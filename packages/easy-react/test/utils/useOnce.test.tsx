c192756c23a24d912494866540ad78781fcdc6c3
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2024-03-03 12:47:33 +0100

    feat: adding the useOnce.ts hook (7/)

--- packages/easy-react/test/utils/useOnce.test.tsx
+++ packages/easy-react/test/utils/useOnce.test.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { DependencyList } from 'react';
 import '@thisisagile/easy-test';
 import { rendersWait } from '@thisisagile/easy-test-react';
 import { HasId, resolve } from '@thisisagile/easy';
-import { useOnce, UseOnceOptions } from '../../src';
+import { useOnce } from '../../src';
 
 describe('useOnce', () => {
-  const Club = ({ f, options = {} }: { f: () => Promise<HasId>; options?: UseOnceOptions<HasId> }) => {
-    const [o] = useOnce(f, options);
+  const Club = ({ f, initial, deps = [] }: { f: () => Promise<HasId>; initial?: HasId; deps?: DependencyList }) => {
+    const [o] = useOnce(f, { initial, deps });
     return <div data-testid={o?.id}>Club</div>;
   };
 
@@ -17,19 +17,19 @@ describe('useOnce', () => {
   });
 
   test('useOnce works with initial.', async () => {
-    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 43 })} options={{ initial: { id: 44 } }} />);
+    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 43 })} initial={{ id: 44 }} />);
     expect(container).toMatchSnapshot();
     expect(atId('43')).toBeValid();
   });
 
   test('useOnce works with deps.', async () => {
-    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 44 })} options={{ deps: [{ id: 44 }] }} />);
+    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 44 })} deps={[{ id: 44 }]} />);
     expect(container).toMatchSnapshot();
     expect(atId('44')).toBeValid();
   });
 
   test('useOnce works with deps and initial.', async () => {
-    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 44 })} options={{ initial: { id: 56 }, deps: [{ id: 44 }] }} />);
+    const { container, atId } = await rendersWait(<Club f={() => resolve({ id: 44 })} initial={{ id: 56 }} deps={[{ id: 44 }]} />);
     expect(container).toMatchSnapshot();
     expect(atId('44')).toBeValid();
   });
