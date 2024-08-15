14551a3844294f379fe8b5635b5dcdde84af59bc
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2024-08-15 14:42:29 +0200

    fix: added TypedKeys<T, U> type

--- packages/easy/src/types/Types.ts
+++ packages/easy/src/types/Types.ts
@@ -3,3 +3,7 @@ export type DontInfer<T> = [T][T extends any ? 0 : never];
 export type Optional<T> = T | undefined;
 
 export type PartialRecord<K extends keyof any, T> = { [P in K]?: T };
+
+export type TypedKeys<T, U> = {
+  [K in keyof T]: T[K] extends U ? K : never;
+}[keyof T];
