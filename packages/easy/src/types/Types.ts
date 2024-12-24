9186b04b404c0236d336c58c4e7f690030af3ec3
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2024-12-24 10:20:31 +0100

    fix: added FromKeys type, that returns a type based on the type of the array of strings or numbers you pass in.

--- packages/easy/src/types/Types.ts
+++ packages/easy/src/types/Types.ts
@@ -3,3 +3,7 @@ export type DontInfer<T> = [T][T extends any ? 0 : never];
 export type Optional<T> = T | undefined;
 
 export type PartialRecord<K extends keyof any, T> = { [P in K]?: T };
+
+export type FromKeys<T extends readonly (string | number | symbol)[], V = boolean> = {
+  [key in T[number]]: V;
+};
