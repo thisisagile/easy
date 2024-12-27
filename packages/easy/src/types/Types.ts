22c7e0ede46ee3051c632ac5ebf03b0bb3942d48
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2024-12-27 16:46:04 +0100

    fix: added FromObject

--- packages/easy/src/types/Types.ts
+++ packages/easy/src/types/Types.ts
@@ -4,6 +4,6 @@ export type Optional<T> = T | undefined;
 
 export type PartialRecord<K extends keyof any, T> = { [P in K]?: T };
 
-export type FromObject<K, V> = { [key in keyof K]?: V };
+export type FromObject<K, V = boolean> = { [key in keyof K]?: V };
 
 export type FromKeys<T extends readonly (string | number | symbol)[], V = boolean> = FromObject<Record<T[number], unknown>, V>;
