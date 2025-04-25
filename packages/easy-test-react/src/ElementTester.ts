778e6f63d2d8d89721cf30d85fa6a1dc5199f1fb
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2025-04-25 16:57:47 +0200

    fix: added keyDown and pressEnter to ElementTester

--- packages/easy-test-react/src/ElementTester.ts
+++ packages/easy-test-react/src/ElementTester.ts
@@ -22,8 +22,10 @@ export class ElementTester {
 
   click = (): this | undefined => (this.element() && fireEvent.click(this.element()) ? this : undefined);
   awaitClick = (): Promise<boolean> => waitFor(() => fireEvent.click(this.element()));
+  keyDown = (key: string): this | undefined => (this.element() && fireEvent.keyDown(this.element(), { key }) ? this : undefined);
   mouseDown = (index?: number): this | undefined =>
     this.element() && fireEvent.mouseDown(isDefined(index) ? this.element().children[index] : this.element()) ? this : undefined;
+  pressEnter = (): this | undefined => this.keyDown('Enter');
   type = (value: string): boolean => fireEvent.change(this.element(), { target: { value } });
   wait = (): Promise<Element> => waitFor(this.element);
   waitForRemove = (): Promise<void> => waitForElementToBeRemoved(this.element);
