ee044836c46497467479c11743599c0b8a02200e
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2025-04-29 09:57:17 +0200

    fix: added keyDown and pressEnter to ElementTester /2

--- packages/easy-test-react/test/ElementTester.test.tsx
+++ packages/easy-test-react/test/ElementTester.test.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { mock } from '@thisisagile/easy-test';
+import { fireEvent, screen } from '@testing-library/react';
+import { ElementTester, renders, Tester } from '../src';
 
-const getByText = mock.return(<div />);
-jest.mock('@testing-library/react', () => ({ ...jest.requireActual('@testing-library/react'), screen: { getByText } }));
-import { fireEvent } from '@testing-library/react';
-import { ElementTester, Tester, renders } from '../src';
+jest.mock('@testing-library/react', () => ({
+  ...jest.requireActual('@testing-library/react'),
+  screen: { getByText: jest.fn().mockReturnValue(<div />) },
+}));
 
 describe('ElementTester', () => {
   const a = <div />;
-  const e = mock.empty<Element & { value: string }>({ value: '42' });
+  const e = mock.empty<Element & { value: string }>({ value: '43' });
   let et: ElementTester;
 
   beforeEach(() => {
@@ -16,7 +18,7 @@ describe('ElementTester', () => {
   });
 
   test('get value', () => {
-    expect(new ElementTester(() => e).value).toBe('42');
+    expect(new ElementTester(() => e).value).toBe('43');
   });
 
   test('is valid', () => {
@@ -36,21 +38,21 @@ describe('ElementTester', () => {
   test('click fires click event', () => {
     fireEvent.click = mock.return(true);
     expect(et.click()).toBe(et);
-    expect(getByText).toHaveBeenCalled();
+    expect(screen.getByText).toHaveBeenCalled();
     expect(fireEvent.click).toHaveBeenCalledWith(a);
   });
 
   test('click fires click event but fails', () => {
     fireEvent.click = mock.return(false);
     expect(et.click()).not.toBeValid();
-    expect(getByText).toHaveBeenCalled();
+    expect(screen.getByText).toHaveBeenCalled();
     expect(fireEvent.click).toHaveBeenCalledWith(a);
   });
 
   test('mouseDown fires mousedown event', () => {
     fireEvent.mouseDown = mock.return(true);
     expect(et.mouseDown()).toBe(et);
-    expect(getByText).toHaveBeenCalled();
+    expect(screen.getByText).toHaveBeenCalled();
     expect(fireEvent.mouseDown).toHaveBeenCalledWith(a);
   });
 
@@ -65,7 +67,7 @@ describe('ElementTester', () => {
   test('mouseDown fires mouseDown event but fails', () => {
     fireEvent.mouseDown = mock.return(false);
     expect(et.mouseDown()).not.toBeValid();
-    expect(getByText).toHaveBeenCalled();
+    expect(screen.getByText).toHaveBeenCalled();
     expect(fireEvent.mouseDown).toHaveBeenCalledWith(a);
   });
 
@@ -73,7 +75,7 @@ describe('ElementTester', () => {
     fireEvent.change = mock.return();
     const value = 'hello';
     et.type(value);
-    expect(getByText).toHaveBeenCalled();
+    expect(screen.getByText).toHaveBeenCalled();
     expect(fireEvent.change).toHaveBeenCalledWith(a, { target: { value } });
   });
 });
