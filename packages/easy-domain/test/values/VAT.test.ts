e4ff56fa654027dcdf0e20cf5c926f046de74827
mnd <mandy.monteiro@ibood.com>
2025-12-15 13:33:37 +0100

    fix(easy-domain): refactor VAT class to accept country in constructor

--- packages/easy-domain/test/values/VAT.test.ts
+++ packages/easy-domain/test/values/VAT.test.ts
@@ -1,52 +1,42 @@
-import { ctx } from '@thisisagile/easy';
-import { toVAT, VAT } from '../../src';
+import { vat, VAT } from '../../src';
 import '@thisisagile/easy-test';
 
 describe('VAT', () => {
-  beforeEach(() => {
-    (ctx as any).request = { shopCode: 'NL' };
-  });
-
-  afterEach(() => {
-    (ctx as any).request = undefined;
-  });
 
   test('creates a VAT value', () => {
-    const vat = toVAT('NL123456789B01');
-    expect(vat).toBeInstanceOf(VAT);
+    const v = vat('NL 123456...789B01', 'NL');
+    expect(v).toBeInstanceOf(VAT);
   });
 
   test('normalizes dots and trims input', () => {
-    const vat = toVAT('  NL12.34.56.78.9B01  ');
-    expect(vat.value).toBe('NL123456789B01');
+    const v = vat('  NL12.34.56.78.9B01  ', 'NL');
+    expect(v.value).toBe('NL123456789B01');
   });
 
-  test('is valid for a correct VAT and shopCode', () => {
-    const vat = toVAT('NL123456789B01');
-    expect(vat.isValid).toBeTruthy();
-  });
-
-  test('is invalid without shopCode in ctx', () => {
-    (ctx as any).request = {};
-    const vat = toVAT('NL123456789B01');
-    expect(vat.isValid).toBeFalsy();
+  test('is valid for a correct VAT and country', () => {
+    const v = vat('NL123456789B01', 'NL');
+    expect(v.isValid).toBeTruthy();
   });
 
   test('is invalid for an incorrect VAT', () => {
-    const vat = toVAT('NL123');
-    expect(vat.isValid).toBeFalsy();
+    const v = vat('NL123', 'NL');
+    expect(v.isValid).toBeFalsy();
   });
 
   test('is invalid for empty input', () => {
-    const vat = toVAT('');
-    expect(vat.isValid).toBeFalsy();
+    const v = vat('', 'NL');
+    expect(v.isValid).toBeFalsy();
   });
 
-  test('respects shopCode country', () => {
-    (ctx as any).request = { shopCode: 'BE' };
-    const vatNL = toVAT('NL 8128.93.773.B.01');
-    const vatBE = toVAT('BE 0895.244.474');
+  test('respects country code', () => {
+    const vatNL = vat('NL 8128.93.773.B.01', 'BE');
+    const vatBE = vat('BE 0895.244.474', 'BE');
     expect(vatNL.isValid).toBeFalsy();
     expect(vatBE.isValid).toBeTruthy();
+  });
+
+  test('defaults to NL if country not provided', () => {
+    const v = vat('NL123456789B01');
+    expect(v.isValid).toBeTruthy();
   });
 });
