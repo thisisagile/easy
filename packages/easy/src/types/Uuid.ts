bb5e71cb9577689c2ec52b44444525785e4e1d28
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2025-12-18 22:06:57 +0100

    fix: added includesUuid() function to Uuid utilities and corresponding tests

--- packages/easy/src/types/Uuid.ts
+++ packages/easy/src/types/Uuid.ts
@@ -1,10 +1,12 @@
 import { v4 as uuid } from 'uuid';
 import { isString } from './Is';
-
-const isValidUuid = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i);
+import { asString } from './Text';
 
 export type Uuid = string;
+export const toUuid = (): Uuid => uuid();
 
+const isValidUuid = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i);
 export const isUuid = (id?: unknown): id is Uuid => isString(id) && isValidUuid.test(id);
 
-export const toUuid = (): Uuid => uuid();
+const includeUuid = new RegExp(/\b[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\b/i);
+export const includesUuid = (value?: unknown): boolean => includeUuid.test(asString(value));
--- packages/easy/test/types/Uuid.test.ts
+++ packages/easy/test/types/Uuid.test.ts
@@ -1,9 +1,18 @@
-import { isUuid, toUuid } from '../../src';
+import { includesUuid, isUuid, toUuid } from '../../src';
 
 describe('Uuid', () => {
   test('isUuid', () => {
     expect(isUuid()).toBeFalsy();
     expect(isUuid(42)).toBeFalsy();
     expect(isUuid(toUuid())).toBeTruthy();
+  });
+
+  test('includesUuid', () => {
+    expect(includesUuid()).toBeFalsy();
+    expect(includesUuid(undefined)).toBeFalsy();
+    expect(includesUuid(42)).toBeFalsy();
+    expect(includesUuid('No UUID here')).toBeFalsy();
+    expect(includesUuid(toUuid())).toBeTruthy();
+    expect(includesUuid(`Yes this has a ${toUuid()}`)).toBeTruthy();
   });
 });
