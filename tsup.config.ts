c1518ac4b2a35e79d23f1ec0f4afa34c6ea54136
woutervanbakel <wouter.vb@gmail.com>
2026-03-03 17:18:53 +0100

    Revert "fix: Bundle ESM output to fix Node 24 module resolution"
    This reverts commit 765e12ebf49de0f7f8fa8690d9ec82c4d84ae550.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,27 @@
-import { defineConfig } from 'tsup';
+import { defineConfig, Format } from 'tsup';
 import path from 'node:path';
 
-export const config = defineConfig({
-  entry: ['src/index.ts'],
+const commonConfig = {
   clean: true,
   sourcemap: true,
   tsconfig: path.resolve(__dirname, './tsconfig.json'),
   outDir: 'dist',
-  format: ['esm', 'cjs'],
-});
+  format: ['esm' as Format],
+};
+
+export const config = defineConfig([
+  {
+    entry: ['src/**/!(index).ts?(x)'],
+    ...commonConfig,
+  },
+  {
+    entry: ['src/index.ts', 'src/**/index.ts'],
+    ...commonConfig,
+    bundle: false,
+  },
+  {
+    entry: ['src/index.ts'],
+    ...commonConfig,
+    format: ['cjs'],
+  },
+]);
