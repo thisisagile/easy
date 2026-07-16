da404ca10ec8c74bd0268b7d4fb89cb0f1bcba29
Sander Hoogendoorn <sander.hoogendoorn@ibood.com>
2026-07-16 22:28:53 +0200

    feat: add `errorMessage` utility to `ErrorOrigin` for extracting error messages from various types

--- packages/easy/src/types/ErrorOrigin.ts
+++ packages/easy/src/types/ErrorOrigin.ts
@@ -1,7 +1,19 @@
-import { Text } from './Text';
-import { Exception } from './Exception';
-import { Response } from '../http/Response';
-import { Results } from './Results';
+import { asString, Text } from './Text';
+import { Exception, isException } from './Exception';
+import { isResponse, Response } from '../http/Response';
+import { isResults, Results } from './Results';
 import { Result } from './Result';
+import { choose } from './Case';
+import { isError, isString } from './Is';
+import { Optional } from './Types';
 
 export type ErrorOrigin = Text | Error | Exception | Response | Results | Result;
+
+export const errorMessage = (e: unknown): Optional<string> =>
+  choose(e)
+    .type(isResponse, r => r.body.error?.errors?.[0]?.message ?? r.body.error?.message)
+    .type(isException, ex => asString(ex.reason) || ex.message)
+    .type(isResults, rs => rs.results[0]?.message)
+    .type(isError, e => e.message)
+    .type(isString, s => s)
+    .else(() => undefined);
